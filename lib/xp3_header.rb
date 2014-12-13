d519005f2ca3115b0f9867d18290a0b199e08040
Marcin Kurczewski <mkurczew@gmail.com>
2014-12-13 12:09:58 +0100

    Added magic verification

--- lib/xp3_header.rb
+++ lib/xp3_header.rb
@@ -1,11 +1,15 @@
-# basic XP3 header (not fully reversed)
+# Basic XP3 header (not fully reversed)
 class Xp3Header
+  MAGIC = "XP3\r\n"
+
   attr_accessor :magic
   attr_accessor :version
   attr_accessor :file_table_origin
 
   def read!(file)
     @magic = file.read(5)
+    fail 'Not an XP3 archive' unless @magic == MAGIC
+
     @version = file.read(4).unpack('I')[0]
     file.seek(2, IO::SEEK_CUR)
     @file_table_origin = file.read(4).unpack('I')[0]
