1f8e8fbd52491bb0ada902907c8bde412377d1c4
Marcin Kurczewski <mkurczew@gmail.com>
2015-01-24 15:11:51 +0100

    Improved readability of C build script

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-dirs=( lib/formats/gfx/prs_converter lib/formats/gfx/spb_converter lib/formats/gfx/g00_converter lib/formats/gfx/tlg_converter lib/formats/gfx/cbg_converter )
+dirs=()
+dirs+=(lib/formats/gfx/prs_converter)
+dirs+=(lib/formats/gfx/spb_converter)
+dirs+=(lib/formats/gfx/g00_converter)
+dirs+=(lib/formats/gfx/tlg_converter)
+dirs+=(lib/formats/gfx/cbg_converter)
 
 for x in "${dirs[@]}"; do
     pushd "$x"
