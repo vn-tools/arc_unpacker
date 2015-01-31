3cadb0777082b36018758b0825dc7be91b17e619
Marcin Kurczewski <mkurczew@gmail.com>
2015-01-31 12:48:20 +0100

    Fixed compound assert macro optimizations

--- src/assert_ex.h
+++ src/assert_ex.h
@@ -4,21 +4,21 @@
 #include <stdbool.h>
 
 #ifdef ENABLE_ASSERT
-	#define assert_equali(a, b) __assert_equali(__FILE__, __LINE__, a, b)
-	#define assert_equals(a, b) __assert_equals(__FILE__, __LINE__, a, b)
-	#define assert_equalsn(a, b, n) __assert_equalsn(__FILE__, __LINE__, a, b, n)
-	#define assert_equalp(a, b) __assert_equalp(__FILE__, __LINE__, a, b)
-	#define assert_not_null(a) __assert_not_null( __FILE__, __LINE__, a)
-	#define assert_null(a) __assert_null(__FILE__, __LINE__, a)
-	#define assert_that(a) __assert_that(__FILE__, __LINE__, a)
+    #define assert_equali(a, b) __assert_equali(__FILE__, __LINE__, a, b)
+    #define assert_equals(a, b) __assert_equals(__FILE__, __LINE__, a, b)
+    #define assert_equalsn(a, b, n) __assert_equalsn(__FILE__, __LINE__, a, b, n)
+    #define assert_equalp(a, b) __assert_equalp(__FILE__, __LINE__, a, b)
+    #define assert_not_null(a) __assert_not_null(__FILE__, __LINE__, a)
+    #define assert_null(a) __assert_null(__FILE__, __LINE__, a)
+    #define assert_that(a) __assert_that(__FILE__, __LINE__, a)
 #else
-	#define assert_equali(a, b) ((void)(a);(void)(b))
-	#define assert_equals(a, b) ((void)(a);(void)(b))
-	#define assert_equalsn(a, b, n) ((void)(a);(void)(b);(void)(n))
-	#define assert_equalp(a, b) ((void)(a);(void)(b))
-	#define assert_not_null(a) ((void)(a))
-	#define assert_null(a) ((void)(a))
-	#define assert_that(a) ((void)(a))
+    #define assert_equali(a, b) (void)(a);(void)(b)
+    #define assert_equals(a, b) (void)(a);(void)(b)
+    #define assert_equalsn(a, b, n) (void)(a);(void)(b);(void)(n)
+    #define assert_equalp(a, b) (void)(a);(void)(b)
+    #define assert_not_null(a) (void)(a)
+    #define assert_null(a) (void)(a)
+    #define assert_that(a) (void)(a)
 #endif
 
 void __assert_equali(const char *file, int line, int expected, int actual);
