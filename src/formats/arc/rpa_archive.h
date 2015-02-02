a0d6745c8fd36840159d2df86c1f1a07887935a8
Marcin Kurczewski <mkurczew@gmail.com>
2015-02-03 00:07:20 +0100

    Added RPA archive support

---
+++ src/formats/arc/rpa_archive.h
@@ -0,0 +1,7 @@
+#ifndef FORMAT_ARC_RPA_ARCHIVE
+#define FROMAT_ARC_RPA_ARCHIVE
+#include "formats/archive.h"
+
+Archive *rpa_archive_create();
+
+#endif
---
+++ tests/formats/arc/rpa_archive_test.c
@@ -0,0 +1,52 @@
+#include "formats/arc/rpa_archive.h"
+#include "test_support/archive_support.h"
+
+// import cPickle
+// import zlib
+//
+// key = 0
+// def filter(x):
+//   return x ^ key
+//
+// print zlib.compress(cPickle.dumps({
+//   'abc.txt': [(filter(0x19), filter(2), '1')],
+//   'another.txt': [(filter(0x1B), filter(7), 'abc')]
+// }, cPickle.HIGHEST_PROTOCOL))
+
+void test_rpa_archive(const char *path)
+{
+    Array *expected_files = array_create();
+    VirtualFile *file1 = virtual_file_create();
+    VirtualFile *file2 = virtual_file_create();
+    virtual_file_set_name(file1, "another.txt");
+    virtual_file_set_name(file2, "abc.txt");
+    io_write_string(file1->io, "abcdefghij", 10);
+    io_write_string(file2->io, "123", 3);
+    array_set(expected_files, 0, file1);
+    array_set(expected_files, 1, file2);
+
+    Archive *archive = rpa_archive_create();
+
+    OutputFiles *output_files = unpack_to_memory(
+        path,
+        archive,
+        0,
+        NULL);
+    Array *actual_files = output_files_get_saved(output_files);
+
+    compare_files(expected_files, actual_files);
+    output_files_destroy(output_files);
+
+    virtual_file_destroy(file1);
+    virtual_file_destroy(file2);
+    archive_destroy(archive);
+    array_destroy(expected_files);
+}
+
+int main(void)
+{
+    test_rpa_archive("tests/test_files/arc/rpa/v3.rpa");
+    test_rpa_archive("tests/test_files/arc/rpa/v2.rpa");
+    test_rpa_archive("tests/test_files/arc/rpa/prefixes.rpa");
+    return 0;
+}
