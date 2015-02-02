89f0eb95860d7301d3791455d05dc0a3d44c2150
Marcin Kurczewski <mkurczew@gmail.com>
2015-02-02 20:03:33 +0100

    Removed unused global magic definitions

--- src/formats/gfx/cbg_converter.h
+++ src/formats/gfx/cbg_converter.h
@@ -2,9 +2,6 @@
 #define FORMATS_GFX_CBG_CONVERTER_H
 #include "formats/converter.h"
 
-extern const char *cbg_magic;
-extern const size_t cbg_magic_length;
-
 Converter *cbg_converter_create();
 
 #endif
--- src/formats/gfx/prs_converter.h
+++ src/formats/gfx/prs_converter.h
@@ -2,9 +2,6 @@
 #define FORMATS_GFX_PRS_CONVERTER_H
 #include "formats/converter.h"
 
-extern const char *prs_magic;
-extern const size_t prs_magic_length;
-
 Converter *prs_converter_create();
 
 #endif
