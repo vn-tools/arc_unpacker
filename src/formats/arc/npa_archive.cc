1755b9124281fe450eb1889819da7e844c680032
Marcin Kurczewski <mkurczew@gmail.com>
2015-02-08 11:21:45 +0100

    Fixed NPA unpacking

--- src/formats/arc/npa_archive.cc
+++ src/formats/arc/npa_archive.cc
@@ -14,13 +14,13 @@
 
 namespace
 {
-    const std::string npa_magic("NPA\x01\x00\x00\x00", 7);
+    const std::string magic("NPA\x01\x00\x00\x00", 7);
 
     typedef enum
     {
-        NPA_FILE_TYPE_DIRECTORY = 1,
-        NPA_FILE_TYPE_FILE = 2
-    } NpaFileType;
+        FILE_TYPE_DIRECTORY = 1,
+        FILE_TYPE_FILE = 2
+    } FileType;
 
     typedef struct
     {
@@ -32,25 +32,22 @@ namespace
         uint32_t folder_count;
         uint32_t file_count;
         uint32_t table_size;
-    } NpaHeader;
-
-    typedef struct NpaUnpackContext
-    {
-        IO &arc_io;
-        NpaHeader &header;
-        NpaFilter *filter;
-        size_t file_pos;
         size_t table_offset;
+    } Header;
 
-        NpaUnpackContext(IO &arc_io, NpaHeader header)
-            : arc_io(arc_io), header(header)
-        {
-        }
-    } NpaUnpackContext;
-
-    std::unique_ptr<NpaHeader> npa_read_header(IO &arc_io)
+    typedef struct
     {
-        std::unique_ptr<NpaHeader> header(new NpaHeader);
+        std::string name;
+        uint32_t offset;
+        uint32_t size_compressed;
+        uint32_t size_original;
+    } TableEntry;
+
+    typedef std::vector<std::unique_ptr<TableEntry>> Table;
+
+    std::unique_ptr<Header> read_header(IO &arc_io)
+    {
+        std::unique_ptr<Header> header(new Header);
         header->key1 = arc_io.read_u32_le();
         header->key2 = arc_io.read_u32_le();
         header->compressed = arc_io.read_u8();
@@ -60,116 +57,113 @@ namespace
         header->file_count = arc_io.read_u32_le();
         arc_io.skip(8);
         header->table_size = arc_io.read_u32_le();
+        header->table_offset = arc_io.tell();
         return header;
     }
 
-    void npa_decrypt_file_name(
-        std::string &name, NpaUnpackContext *unpack_context)
+    void decrypt_file_name(
+        std::string &name,
+        const Header &header,
+        const NpaFilter &filter,
+        size_t file_pos)
     {
-        uint32_t tmp = unpack_context->filter->file_name_key(
-            unpack_context->header.key1,
-            unpack_context->header.key2);
-
-        size_t char_pos;
-        for (char_pos = 0; char_pos < name.size(); char_pos++)
+        uint32_t tmp = filter.file_name_key(header.key1, header.key2);
+        for (size_t char_pos = 0; char_pos < name.size(); char_pos++)
         {
             uint32_t key = 0xfc * char_pos;
             key -= tmp >> 0x18;
             key -= tmp >> 0x10;
             key -= tmp >> 0x08;
             key -= tmp & 0xff;
-            key -= unpack_context->file_pos >> 0x18;
-            key -= unpack_context->file_pos >> 0x10;
-            key -= unpack_context->file_pos >> 0x08;
-            key -= unpack_context->file_pos;
+            key -= file_pos >> 0x18;
+            key -= file_pos >> 0x10;
+            key -= file_pos >> 0x08;
+            key -= file_pos;
             name[char_pos] += (key & 0xff);
         }
     }
 
-    std::string npa_read_file_name(NpaUnpackContext *unpack_context)
+    void decrypt_file_data(
+        std::string &data,
+        const TableEntry &table_entry,
+        const Header &header,
+        const NpaFilter &filter)
     {
-        size_t file_name_length = unpack_context->arc_io.read_u32_le();
-        std::string name = unpack_context->arc_io.read(file_name_length);
-        npa_decrypt_file_name(name, unpack_context);
-        return name;
-    }
-
-    void npa_decrypt_file_data(
-        unsigned char *data,
-        size_t data_size_compressed,
-        size_t data_size_original,
-        const std::string &original_file_name,
-        NpaUnpackContext *unpack_context)
-    {
-        const unsigned char *permutation = unpack_context->filter->permutation;
-
-        uint32_t key = unpack_context->filter->data_key;
-        size_t i;
-        for (i = 0; i < original_file_name.size(); i ++)
-            key -= (unsigned char)original_file_name[i];
-        key *= original_file_name.size();
-        key += unpack_context->header.key1 * unpack_context->header.key2;
-        key *= data_size_original;
+        uint32_t key = filter.data_key;
+        for (size_t i = 0; i < table_entry.name.size(); i ++)
+            key -= reinterpret_cast<const unsigned char&>(table_entry.name[i]);
+        key *= table_entry.name.size();
+        key += header.key1 * header.key2;
+        key *= table_entry.size_original;
         key &= 0xff;
 
-        size_t length = 0x1000 + original_file_name.size();
-        for (i = 0; i < length && i < data_size_compressed; i ++)
-            data[i] = (unsigned char)(permutation[(size_t)data[i]] - key - i);
-    }
-
-    std::string npa_read_file_data(
-        size_t size_compressed,
-        size_t size_original,
-        const std::string &original_file_name,
-        NpaUnpackContext *unpack_context)
-    {
-        std::string data = unpack_context->arc_io.read(size_compressed);
-
-        if (unpack_context->header.encrypted)
+        size_t length = 0x1000 + table_entry.name.size();
+        for (size_t i = 0; i < length && i < table_entry.size_compressed; i ++)
         {
-            npa_decrypt_file_data(
-                (unsigned char*)data.data(),
-                size_compressed,
-                size_original,
-                original_file_name,
-                unpack_context);
+            char p = filter.permutation[static_cast<unsigned char>(data[i])];
+            data[i] = static_cast<char>(p - key - i);
         }
-
-        if (unpack_context->header.compressed)
-            data = zlib_inflate(data);
-
-        return data;
     }
 
-    std::unique_ptr<VirtualFile> npa_read_file(void *_context)
+    std::unique_ptr<TableEntry> read_table_entry(
+        IO &arc_io,
+        const Header &header,
+        const NpaFilter &filter,
+        size_t file_pos)
     {
-        NpaUnpackContext *unpack_context = (NpaUnpackContext*)_context;
-        std::unique_ptr<VirtualFile> file(new VirtualFile);
+        std::unique_ptr<TableEntry> table_entry(new TableEntry);
 
-        std::string file_name = npa_read_file_name(unpack_context);
-        file->name = convert_encoding(file_name, "cp932", "utf-8");
+        table_entry->name = arc_io.read(arc_io.read_u32_le());
+        decrypt_file_name(table_entry->name, header, filter, file_pos);
 
-        NpaFileType file_type = (NpaFileType)unpack_context->arc_io.read_u8();
-        unpack_context->arc_io.skip(4);
-        uint32_t offset = unpack_context->arc_io.read_u32_le();
-        uint32_t size_compressed = unpack_context->arc_io.read_u32_le();
-        uint32_t size_original = unpack_context->arc_io.read_u32_le();
-        offset +=
-            unpack_context->table_offset + unpack_context->header.table_size;
+        FileType file_type = static_cast<FileType>(arc_io.read_u8());
+        arc_io.skip(4);
 
-        if (file_type == NPA_FILE_TYPE_DIRECTORY)
+        table_entry->offset
+            = arc_io.read_u32_le() + header.table_offset + header.table_size;
+        table_entry->size_compressed = arc_io.read_u32_le();
+        table_entry->size_original = arc_io.read_u32_le();
+
+        if (file_type == FILE_TYPE_DIRECTORY)
             return nullptr;
-
-        if (file_type != NPA_FILE_TYPE_FILE)
+        if (file_type != FILE_TYPE_FILE)
             throw std::runtime_error("Unknown file type");
 
-        size_t old_pos = unpack_context->arc_io.tell();
-        unpack_context->arc_io.seek(offset);
-        std::string file_data = npa_read_file_data(
-            size_compressed, size_original, file_name, unpack_context);
-        file->io.write(file_data);
-        unpack_context->arc_io.seek(old_pos);
+        return table_entry;
+    }
 
+    Table read_table(
+        IO &arc_io, const Header &header, const NpaFilter &filter)
+    {
+        Table table;
+        for (size_t i = 0; i < header.total_count; i ++)
+        {
+            auto table_entry = read_table_entry(arc_io, header, filter, i);
+            if (table_entry != nullptr)
+                table.push_back(std::move(table_entry));
+        }
+        return table;
+    }
+
+    std::unique_ptr<VirtualFile> read_file(
+        IO &arc_io,
+        const Header &header,
+        const NpaFilter &filter,
+        const TableEntry &table_entry)
+    {
+        std::unique_ptr<VirtualFile> file(new VirtualFile);
+        file->name = convert_encoding(table_entry.name, "cp932", "utf-8");
+
+        arc_io.seek(table_entry.offset);
+        std::string data = arc_io.read(table_entry.size_compressed);
+
+        if (header.encrypted)
+            decrypt_file_data(data, table_entry, header, filter);
+
+        if (header.compressed)
+            data = zlib_inflate(data);
+
+        file->io.write(data);
         return file;
     }
 }
@@ -220,19 +214,20 @@ void NpaArchive::parse_cli_options(ArgParser &arg_parser)
 
 void NpaArchive::unpack_internal(IO &arc_io, OutputFiles &output_files) const
 {
-    if (arc_io.read(npa_magic.size()) != npa_magic)
+    if (arc_io.read(magic.size()) != magic)
         throw std::runtime_error("Not a NPA archive");
 
     if (context->filter == nullptr)
         throw std::runtime_error("No plugin selected");
 
-    std::unique_ptr<NpaHeader> header = npa_read_header(arc_io);
-    NpaUnpackContext unpack_context(arc_io, *header);
-    unpack_context.filter = context->filter;
-    unpack_context.table_offset = arc_io.tell();
-    for (size_t file_pos = 0; file_pos < header->total_count; file_pos ++)
+    std::unique_ptr<Header> header = read_header(arc_io);
+
+    Table table = read_table(arc_io, *header, *context->filter);
+    for (size_t i = 0; i < table.size(); i ++)
     {
-        unpack_context.file_pos = file_pos;
-        output_files.save(&npa_read_file, &unpack_context);
+        output_files.save([&]() -> std::unique_ptr<VirtualFile>
+        {
+            return read_file(arc_io, *header, *context->filter, *table[i]);
+        });
     }
 }
