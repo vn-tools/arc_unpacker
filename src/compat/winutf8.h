8590251922e8f5e506afe033ec0f0ba47ab2b9b0
Marcin Kurczewski <mkurczew@gmail.com>
2015-02-12 08:55:26 +0100

    Fixed linking for GNU/Linux

--- src/compat/winutf8.h
+++ src/compat/winutf8.h
@@ -1,5 +1,8 @@
 #ifndef COMPAT_WINUTF8_H
 #define COMPAT_WINUTF8_H
+#ifndef _WIN32
+    #error Trying to include Windows-related headers for non-Windows system
+#endif
 #include <string>
 
 std::wstring string2wstring(std::string str);
