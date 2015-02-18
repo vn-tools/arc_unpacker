f3a8d379305d071f81b678014ba5bf1f440ab05b
Marcin Kurczewski <mkurczew@gmail.com>
2015-02-18 21:25:21 +0100

    Refactored BitReader to allow more strategies

--- src/bit_reader.cc
+++ src/bit_reader.cc
@@ -1,54 +1,114 @@
 #include <stdexcept>
 #include "bit_reader.h"
 
-struct BitReader::Internals
+namespace
 {
-    const uint8_t *buffer;
-    size_t buffer_size;
-    uint8_t mask;
-    uint8_t value;
+    class Reader
+    {
+    private:
+        uint8_t mask;
+        uint8_t value;
+        virtual bool eof() = 0;
+        virtual uint8_t fetch_byte() = 0;
 
-    Internals(const uint8_t *buffer, size_t buffer_size) :
-        buffer(buffer), buffer_size(buffer_size)
+    public:
+        Reader();
+        bool get(bool exception);
+        int getn(size_t n, bool exception);
+    };
+
+    Reader::Reader()
     {
         mask = 0;
         value = 0;
     }
 
-    bool get()
+    bool Reader::get(bool exception)
     {
         mask >>= 1;
         if (mask == 0x00)
         {
-            if (buffer_size == 0)
+            if (eof())
+            {
+                if (!exception)
+                    return 0;
                 throw std::runtime_error("Trying to read bits beyond EOF");
+            }
 
             mask = 0x80;
-            value = *buffer ++;
-            -- buffer_size;
+            value = fetch_byte();
         }
         return (value & mask) != 0;
     }
 
-    bool try_get()
+    int Reader::getn(size_t n, bool exception)
     {
-        mask >>= 1;
-        if (mask == 0x00)
-        {
-            if (buffer_size == 0)
-                return 0;
+        if (n > 32)
+            throw std::runtime_error("Too many bits");
 
-            mask = 0x80;
-            value = *buffer ++;
-            -- buffer_size;
+        unsigned int value = 0;
+        while (n --)
+        {
+            value <<= 1;
+            value |= static_cast<int>(get(exception));
         }
-        return (value & mask) != 0;
+        return value;
+    }
+}
+
+namespace
+{
+    class BufferBasedReader : public Reader
+    {
+    private:
+        const uint8_t *buffer;
+        size_t buffer_size;
+
+    public:
+        BufferBasedReader(const uint8_t *buffer, size_t buffer_size);
+        bool eof() override;
+        uint8_t fetch_byte() override;
+    };
+
+    BufferBasedReader::BufferBasedReader(
+        const uint8_t *buffer, size_t buffer_size)
+        : buffer(buffer), buffer_size(buffer_size)
+    {
+    }
+
+    bool BufferBasedReader::eof()
+    {
+        return buffer_size == 0;
+    }
+
+    uint8_t BufferBasedReader::fetch_byte()
+    {
+        -- buffer_size;
+        return *buffer ++;
+    }
+}
+
+struct BitReader::Internals
+{
+    std::unique_ptr<Reader> reader;
+
+    Internals(std::unique_ptr<Reader> reader) : reader(std::move(reader))
+    {
+    }
+
+    ~Internals()
+    {
     }
 };
 
+
 BitReader::BitReader(const char *buffer, size_t buffer_size)
-    : internals(new Internals(
-        reinterpret_cast<const uint8_t*>(buffer), buffer_size))
+    : internals(
+        new Internals(
+            std::unique_ptr<Reader>(
+                new BufferBasedReader(
+                    reinterpret_cast<const uint8_t*>(buffer),
+                    buffer_size))))
 {
 }
 
@@ -58,28 +118,10 @@ BitReader::~BitReader()
 
 unsigned int BitReader::get(size_t n)
 {
-    if (n > 32)
-        throw std::runtime_error("Too many bits");
-
-    unsigned int value = 0;
-    while (n --)
-    {
-        value <<= 1;
-        value |= static_cast<int>(internals->get());
-    }
-    return value;
+    return internals->reader->getn(n, true);
 }
 
 unsigned int BitReader::try_get(size_t n)
 {
-    if (n > 32)
-        throw std::runtime_error("Too many bits");
-
-    unsigned int value = 0;
-    while (n --)
-    {
-        value <<= 1;
-        value |= static_cast<int>(internals->try_get());
-    }
-    return value;
+    return internals->reader->getn(n, false);
 }
