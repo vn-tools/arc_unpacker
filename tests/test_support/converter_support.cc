2addc366cbf1a9db0a6935449114a763e98b3201
Marcin Kurczewski <mkurczew@gmail.com>
2015-05-06 23:10:34 +0200

    Improved color assertions in tests

--- tests/test_support/converter_support.cc
+++ tests/test_support/converter_support.cc
@@ -18,8 +18,8 @@ namespace
             {
                 uint32_t expected_rgba = expected_image.color_at(x, y);
                 uint32_t actual_rgba = actual_image.color_at(x, y);
-                if (expected_rgba != actual_rgba)
-                    REQUIRE(0);
+                if (expected_rgba != actual_rgba) //speed up
+                    REQUIRE(expected_rgba == actual_rgba);
             }
         }
     }
