11f2aa7424f2565df0c1cf3f8e853c1c5d99abba
Wu Zhenwei <wuzhenwei35@gmail.com>
2015-06-29 17:31:31 +0800

    Fix comment. Fix VS source group.

--- tests/formats/ivory/mbl_archive_test.cc
+++ tests/formats/ivory/mbl_archive_test.cc
@@ -8,7 +8,7 @@ static void test_mbl_archive(const std::string &path)
     std::shared_ptr<File> file1(new File);
     std::shared_ptr<File> file2(new File);
     file1->name = "abc.txt";
-    file2->name = "\xE3\x83\x86\xE3\x82\xB9\xE3\x83\x88";//テスト
+    file2->name = "\xE3\x83\x86\xE3\x82\xB9\xE3\x83\x88"; // "テスト"
     file1->io.write("abc", 3);
     file2->io.write("AAAAAAAAAAAAAAAA", 16);
     std::vector<std::shared_ptr<File>> expected_files { file1, file2 };
