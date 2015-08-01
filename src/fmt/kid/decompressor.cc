e5a8d4ad3f3896b80df1f917642126f334bea0b0
rr- <mkurczew@gmail.com>
2015-08-01 14:54:01 +0200

    fmt: protect KID decompressor from buffer overflow

--- src/fmt/kid/decompressor.cc
+++ src/fmt/kid/decompressor.cc
@@ -1,5 +1,6 @@
 #include "fmt/kid/decompressor.h"
 #include "io/buffered_io.h"
+#include "util/format.h"
 #include "util/range.h"
 
 using namespace au;
@@ -11,6 +12,11 @@ std::unique_ptr<io::IO> au::fmt::kid::decompress(
     output_io->reserve(size_original);
     while (output_io->tell() < size_original)
     {
+        if (input_io.eof())
+        {
+            throw std::runtime_error(util::format(
+                "EOF reached at %d/%d", output_io->tell(), size_original));
+        }
         u8 byte = input_io.read_u8();
         if (byte & 0x80)
         {
@@ -27,7 +33,9 @@ std::unique_ptr<io::IO> au::fmt::kid::decompress(
             {
                 int length = ((byte >> 2) & 0xF) + 2;
                 int look_behind = ((byte & 3) << 8) + input_io.read_u8() + 1;
-                int start_pos = output_io->tell() - look_behind;
+                size_t start_pos = output_io->tell() - look_behind;
+                if (start_pos > output_io->tell())
+                    start_pos = output_io->tell();
                 for (auto i : util::range(length))
                     output_io->write_u8(output_io->buffer()[start_pos + i]);
             }
