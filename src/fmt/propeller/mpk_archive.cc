1e5d2c2f2b1f782544f1f88e835b7b30275f1c45
rr- <mkurczew@gmail.com>
2015-08-02 12:54:26 +0200

    fmt: add variable key support to MPK

--- src/fmt/propeller/mpk_archive.cc
+++ src/fmt/propeller/mpk_archive.cc
@@ -38,28 +38,27 @@ static Table read_table(io::IO &arc_io)
     for (auto i : util::range(file_count))
     {
         std::unique_ptr<TableEntry> entry(new TableEntry);
-        entry->name = arc_io.read(32);
-        entry->offset = arc_io.read_u32_le();
-        entry->size = arc_io.read_u32_le();
+
+        auto name = arc_io.read(32);
+        u8 key8 = name[31];
+        u32 key32 = (key8 << 24) | (key8 << 16) | (key8 << 8) | key8;
+
+        for (auto i : util::range(32))
+            name[i] ^= key8;
+        name = util::sjis_to_utf8(name);
+        if (name[0] == '\\')
+            name = name.substr(1);
+        name.erase(name.find('\x00'));
+
+        entry->name = name;
+        entry->offset = arc_io.read_u32_le() ^ key32;
+        entry->size = arc_io.read_u32_le() ^ key32;
+
         table.push_back(std::move(entry));
     }
 
     if (file_count == 0)
         return table;
-    u8 key8 = table[0]->name[31];
-    u32 key32 = (key8 << 24) | (key8 << 16) | (key8 << 8) | key8;
-
-    for (auto &entry : table)
-    {
-        for (auto i : util::range(32))
-            entry->name[i] ^= key8;
-        entry->name = util::sjis_to_utf8(entry->name);
-        if (entry->name[0] == '\\')
-            entry->name = entry->name.substr(1);
-        entry->name.erase(entry->name.find('\x00'));
-        entry->offset ^= key32;
-        entry->size ^= key32;
-    }
 
     return table;
 }
