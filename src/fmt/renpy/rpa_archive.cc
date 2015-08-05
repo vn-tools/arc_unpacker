1172f74fed1b26fdf7d135a3cf74587d061ec48f
rr- <mkurczew@gmail.com>
2015-08-05 12:25:20 +0200

    style: replace #define with enum for RPA

--- src/fmt/renpy/rpa_archive.cc
+++ src/fmt/renpy/rpa_archive.cc
@@ -11,67 +11,74 @@
 
 #include "fmt/renpy/rpa_archive.h"
 #include "io/buffered_io.h"
+#include "util/format.h"
 #include "util/pack/zlib.h"
 #include "util/range.h"
 
 using namespace au;
 using namespace au::fmt::renpy;
 
-#define PICKLE_MARK            '('
-#define PICKLE_STOP            '.'
-#define PICKLE_POP             '0'
-#define PICKLE_POP_MARK        '1'
-#define PICKLE_DUP             '2'
-#define PICKLE_FLOAT           'F'
-#define PICKLE_INT             'I'
-#define PICKLE_BININT1         'K'
-#define PICKLE_BININT2         'M'
-#define PICKLE_BININT4         'J'
-#define PICKLE_LONG            'L'
-#define PICKLE_NONE            'N'
-#define PICKLE_PERSID          'P'
-#define PICKLE_BINPERSID       'Q'
-#define PICKLE_REDUCE          'R'
-#define PICKLE_STRING          'S'
-#define PICKLE_BINSTRING       'T'
-#define PICKLE_SHORT_BINSTRING 'U'
-#define PICKLE_UNICODE         'V'
-#define PICKLE_BINUNICODE      'X'
-#define PICKLE_APPEND          'a'
-#define PICKLE_BUILD           'b'
-#define PICKLE_GLOBAL          'c'
-#define PICKLE_DICT            'd'
-#define PICKLE_EMPTY_DICT      '}'
-#define PICKLE_APPENDS         'e'
-#define PICKLE_GET             'g'
-#define PICKLE_BINGET          'h'
-#define PICKLE_LONG_BINGET     'j'
-#define PICKLE_INST            'i'
-#define PICKLE_LIST            'l'
-#define PICKLE_EMPTY_LIST      ']'
-#define PICKLE_OBJ             'o'
-#define PICKLE_PUT             'p'
-#define PICKLE_BINPUT          'q'
-#define PICKLE_LONG_BINPUT     'r'
-#define PICKLE_SETITEM         's'
-#define PICKLE_TUPLE           't'
-#define PICKLE_EMPTY_TUPLE     ')'
-#define PICKLE_SETITEMS        'u'
-#define PICKLE_BINFLOAT        'G'
+namespace
+{
+    enum class PickleOpcode : u8
+    {
+        Mark           = '(',
+        Stop           = '.',
+        Pop            = '0',
+        PopMark        = '1',
+        Dup            = '2',
+        Float          = 'F',
+        Int            = 'I',
+        BinInt1        = 'K',
+        BinInt2        = 'M',
+        BinInt4        = 'J',
+        Long           = 'L',
+        None           = 'N',
+        PersId         = 'P',
+        BinPersId      = 'Q',
+        Reduce         = 'R',
+        String         = 'S',
+        BinString      = 'T',
+        ShortBinString = 'U',
+        Unicode        = 'V',
+        BinUnicode     = 'X',
+        Append         = 'a',
+        Build          = 'b',
+        Global         = 'c',
+        Dict           = 'd',
+        EmptyDict      = '}',
+        Appends        = 'e',
+        Get            = 'g',
+        BinGet         = 'h',
+        LongBinGet     = 'j',
+        Inst           = 'i',
+        List           = 'l',
+        EmptyList      = ']',
+        Obj            = 'o',
+        Put            = 'p',
+        BinPut         = 'q',
+        LongBinPut     = 'r',
+        SetItem        = 's',
+        Tuple          = 't',
+        EmptyTuple     = ')',
+        SetItems       = 'u',
+        BinFloat       = 'G',
 
-// Pickle protocol 2
-#define PICKLE_PROTO    '\x80'_u8
-#define PICKLE_NEWOBJ   '\x81'_u8
-#define PICKLE_EXT1     '\x82'_u8
-#define PICKLE_EXT2     '\x83'_u8
-#define PICKLE_EXT4     '\x84'_u8
-#define PICKLE_TUPLE1   '\x85'_u8
-#define PICKLE_TUPLE2   '\x86'_u8
-#define PICKLE_TUPLE3   '\x87'_u8
-#define PICKLE_NEWTRUE  '\x88'_u8
-#define PICKLE_NEWFALSE '\x89'_u8
-#define PICKLE_LONG1    '\x8A'_u8
-#define PICKLE_LONG4    '\x8B'_u8
+        // Pickle protocol 2
+        Proto          = '\x80'_u8,
+        Newobj         = '\x81'_u8,
+        Ext1           = '\x82'_u8,
+        Ext2           = '\x83'_u8,
+        Ext4           = '\x84'_u8,
+        Tuple1         = '\x85'_u8,
+        Tuple2         = '\x86'_u8,
+        Tuple3         = '\x87'_u8,
+        NewTrue        = '\x88'_u8,
+        NewFalse       = '\x89'_u8,
+        Long1          = '\x8A'_u8,
+        Long4          = '\x8B'_u8,
+    };
+}
 
 namespace
 {
@@ -107,48 +114,48 @@ static void unpickle(io::IO &table_io, UnpickleContext *context)
 {
     // Stupid unpickle "implementation" ahead: instead of twiddling with stack,
     // arrays, dictionaries and all that crap, just remember all pushed strings
-    // and integers for later interpretation.  We also take advantage of RenPy
+    // and integers for later interpretation. We also take advantage of RenPy
     // using Pickle's HIGHEST_PROTOCOL, which means there's no need to parse
     // 90% of the opcodes (such as "S" with escape stuff).
     size_t table_size = table_io.size();
     while (table_io.tell() < table_size)
     {
-        u8 c = table_io.read_u8();
+        PickleOpcode c = static_cast<PickleOpcode>(table_io.read_u8());
         switch (c)
         {
-            case PICKLE_SHORT_BINSTRING:
+            case PickleOpcode::ShortBinString:
             {
                 char size = table_io.read_u8();
                 unpickle_handle_string(table_io.read(size), context);
                 break;
             }
 
-            case PICKLE_BINUNICODE:
+            case PickleOpcode::BinUnicode:
             {
                 u32 size = table_io.read_u32_le();
                 unpickle_handle_string(table_io.read(size), context);
                 break;
             }
 
-            case PICKLE_BININT1:
+            case PickleOpcode::BinInt1:
             {
                 unpickle_handle_number(table_io.read_u8(), context);
                 break;
             }
 
-            case PICKLE_BININT2:
+            case PickleOpcode::BinInt2:
             {
                 unpickle_handle_number(table_io.read_u16_le(), context);
                 break;
             }
 
-            case PICKLE_BININT4:
+            case PickleOpcode::BinInt4:
             {
                 unpickle_handle_number(table_io.read_u32_le(), context);
                 break;
             }
 
-            case PICKLE_LONG1:
+            case PickleOpcode::Long1:
             {
                 size_t length = table_io.read_u8();
                 u32 number = 0;
@@ -164,36 +171,35 @@ static void unpickle(io::IO &table_io, UnpickleContext *context)
                 break;
             }
 
-            case PICKLE_PROTO:
+            case PickleOpcode::Proto:
                 table_io.skip(1);
                 break;
 
-            case PICKLE_BINPUT:
+            case PickleOpcode::BinPut:
                 table_io.skip(1);
                 break;
 
-            case PICKLE_LONG_BINPUT:
+            case PickleOpcode::LongBinPut:
                 table_io.skip(4);
                 break;
 
-            case PICKLE_APPEND:
-            case PICKLE_SETITEMS:
-            case PICKLE_MARK:
-            case PICKLE_EMPTY_LIST:
-            case PICKLE_EMPTY_DICT:
-            case PICKLE_TUPLE1:
-            case PICKLE_TUPLE2:
-            case PICKLE_TUPLE3:
+            case PickleOpcode::Append:
+            case PickleOpcode::SetItems:
+            case PickleOpcode::Mark:
+            case PickleOpcode::EmptyList:
+            case PickleOpcode::EmptyDict:
+            case PickleOpcode::Tuple1:
+            case PickleOpcode::Tuple2:
+            case PickleOpcode::Tuple3:
                 break;
 
-            case PICKLE_STOP:
+            case PickleOpcode::Stop:
                 return;
 
             default:
             {
-                std::string msg = "Unsupported pickle operator ";
-                msg += c;
-                throw std::runtime_error(msg);
+                throw std::runtime_error(util::format(
+                    "Unsupported pickle operator %c", static_cast<char>(c)));
             }
         }
     }
