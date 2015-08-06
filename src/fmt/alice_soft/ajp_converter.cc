cb3a8ed8cc54ec7f3cf3a6eefcb01ed30d50fe73
rr- <mkurczew@gmail.com>
2015-08-06 23:04:50 +0200

    fmt: fix AliceSoft's AJP reader
    I accidentally pushed development version that was unable to unpack
    anything. Now it should unpack opaque sprites. The next step is to apply
    the opacity masks, which will be done in separate commit.

--- src/fmt/alice_soft/ajp_converter.cc
+++ src/fmt/alice_soft/ajp_converter.cc
@@ -10,6 +10,8 @@
 
 #include <iostream>
 #include "fmt/alice_soft/ajp_converter.h"
+#include "io/buffered_io.h"
+#include "util/image.h"
 #include "util/range.h"
 
 using namespace au;
@@ -17,6 +19,20 @@ using namespace au::fmt;
 using namespace au::fmt::alice_soft;
 
 static const std::string magic = "AJP\x00"_s;
+static const std::string key =
+    "\x5d\x91\xAE\x87\x4A\x56\x41\xCD\x83\xEC\x4C\x92\xB5\xCB\x16\x34";
+
+static std::unique_ptr<io::IO> decrypt(io::IO &input_io, size_t size)
+{
+    std::unique_ptr<io::BufferedIO> output_io(new io::BufferedIO);
+    output_io->reserve(size);
+    for (auto i : util::range(key.size()))
+        if (output_io->tell() < size)
+            output_io->write_u8(input_io.read_u8() ^ key[i]);
+    output_io->write_from_io(input_io, size - output_io->tell());
+    output_io->seek(0);
+    return std::unique_ptr<io::IO>(std::move(output_io));
+}
 
 bool AjpConverter::is_recognized_internal(File &file) const
 {
@@ -26,25 +42,31 @@ bool AjpConverter::is_recognized_internal(File &file) const
 std::unique_ptr<File> AjpConverter::decode_internal(File &file) const
 {
     file.io.skip(magic.size());
-    auto version = file.io.read_u32_le();
-    auto header1_size = file.io.read_u32_le();
+    file.io.skip(4 * 2);
     auto width = file.io.read_u32_le();
     auto height = file.io.read_u32_le();
-    auto header2_size = file.io.read_u32_le();
-    auto jpeg_data_size = file.io.read_u32_le();
-    auto alpha_location = file.io.read_u32_le();
-    auto additional_data_size = file.io.read_u32_le();
-    file.io.skip(16 + 4 + 16);
+    auto jpeg_offset = file.io.read_u32_le();
+    auto jpeg_size = file.io.read_u32_le();
+    auto mask_offset = file.io.read_u32_le();
+    auto mask_size = file.io.read_u32_le();
+
+    file.io.seek(jpeg_offset);
+    auto jpeg_io = decrypt(file.io, jpeg_size);
+
+    if (mask_size)
+    {
+        file.io.seek(mask_offset);
+        auto mask_io = decrypt(file.io, mask_size);
+
+        //TODO:
+        //1. add PM converter
+        //2. pass the mask through the PM converter
+        //3. apply the mask to JPEG image
+    }
 
     std::unique_ptr<File> output_file(new File);
-    output_file->io.write("\xFF\xD8\xFF\xE0\x00\x10JFIF\x00\x01\x02\x00"_s);
-    output_file->io.write_u16_le(file.io.read_u16_le());
-    file.io.skip(-2);
-    output_file->io.write_from_io(file.io, jpeg_data_size);
-
+    output_file->io.write_from_io(*jpeg_io);
     output_file->name = file.name;
     output_file->guess_extension();
     return output_file;
-
-    //apparently, it is followed by some kind of alpha channel
 }
