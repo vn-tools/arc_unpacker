324723c0f2f4bd811fe50ef6defc264ee0a7f32c
rr- <mkurczew@gmail.com>
2015-08-11 21:37:32 +0200

    fmt: change ARC unpacker to use Table idiom

--- src/fmt/bgi/arc_archive.cc
+++ src/fmt/bgi/arc_archive.cc
@@ -17,24 +17,41 @@ using namespace au::fmt::bgi;
 
 static const bstr magic = "PackFile\x20\x20\x20\x20"_b;
 
-static std::unique_ptr<File> read_file(io::IO &arc_io, size_t file_count)
+namespace
+{
+    struct TableEntry
+    {
+        std::string name;
+        u32 offset;
+        u32 size;
+    };
+
+    using Table = std::vector<std::unique_ptr<TableEntry>>;
+}
+
+static Table read_table(io::IO &arc_io)
+{
+    auto file_count = arc_io.read_u32_le();
+    Table table;
+    auto file_data_start = arc_io.tell() + file_count * 32;
+    for (auto i : util::range(file_count))
+    {
+        std::unique_ptr<TableEntry> entry(new TableEntry);
+        entry->name = arc_io.read_to_zero(16).str();
+        entry->offset = arc_io.read_u32_le() + file_data_start;
+        entry->size = arc_io.read_u32_le();
+        arc_io.skip(8);
+        table.push_back(std::move(entry));
+    }
+    return table;
+}
+
+static std::unique_ptr<File> read_file(io::IO &arc_io, const TableEntry &entry)
 {
     std::unique_ptr<File> file(new File);
-
-    size_t old_pos = arc_io.tell();
-    file->name = arc_io.read_to_zero().str();
-    arc_io.seek(old_pos + 16);
-
-    size_t offset = arc_io.read_u32_le();
-    size_t size = arc_io.read_u32_le();
-    offset += magic.size() + 4 + file_count * 32;
-    arc_io.skip(8);
-
-    old_pos = arc_io.tell();
-    arc_io.seek(offset);
-    file->io.write_from_io(arc_io, size);
-    arc_io.seek(old_pos);
-
+    arc_io.seek(entry.offset);
+    file->io.write_from_io(arc_io, entry.size);
+    file->name = entry.name;
     return file;
 }
 
@@ -62,12 +79,7 @@ bool ArcArchive::is_recognized_internal(File &arc_file) const
 void ArcArchive::unpack_internal(File &arc_file, FileSaver &file_saver) const
 {
     arc_file.io.skip(magic.size());
-
-    size_t file_count = arc_file.io.read_u32_le();
-    for (auto i : util::range(file_count))
-    {
-        auto file = read_file(arc_file.io, file_count);
-        file->guess_extension();
-        file_saver.save(std::move(file));
-    }
+    auto table = read_table(arc_file.io);
+    for (auto &entry : table)
+        file_saver.save(read_file(arc_file.io, *entry));
 }
