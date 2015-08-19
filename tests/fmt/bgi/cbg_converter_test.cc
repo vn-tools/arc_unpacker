85cda4103665d5e885224f9c043c0b506b408c62
rr- <mkurczew@gmail.com>
2015-08-19 08:11:24 +0200

    tests: test BGI's CBG v2 image decoding

--- tests/fmt/bgi/cbg_converter_test.cc
+++ tests/fmt/bgi/cbg_converter_test.cc
@@ -5,29 +5,56 @@
 using namespace au;
 using namespace au::fmt::bgi;
 
-TEST_CASE("Decoding 24-bit CBG images works")
+TEST_CASE("Decoding 32-bit CBG1 images works")
 {
     CbgConverter converter;
     tests::assert_image_conversion(
         converter,
-        "tests/fmt/bgi/files/3",
-        "tests/fmt/bgi/files/3-out.png");
+        "tests/fmt/bgi/files/cbg-v1/ti_si_de_a1",
+        "tests/fmt/bgi/files/cbg-v1/ti_si_de_a1-out.png");
 }
 
-TEST_CASE("Decoding 8-bit CBG images works")
+TEST_CASE("Decoding 24-bit CBG1 images works")
 {
     CbgConverter converter;
     tests::assert_image_conversion(
         converter,
-        "tests/fmt/bgi/files/4",
-        "tests/fmt/bgi/files/4-out.png");
+        "tests/fmt/bgi/files/cbg-v1/3",
+        "tests/fmt/bgi/files/cbg-v1/3-out.png");
 }
 
-TEST_CASE("Decoding 32-bit CBG images works")
+TEST_CASE("Decoding 8-bit CBG1 images works")
 {
     CbgConverter converter;
     tests::assert_image_conversion(
         converter,
-        "tests/fmt/bgi/files/ti_si_de_a1",
-        "tests/fmt/bgi/files/ti_si_de_a1-out.png");
+        "tests/fmt/bgi/files/cbg-v1/4",
+        "tests/fmt/bgi/files/cbg-v1/4-out.png");
+}
+
+TEST_CASE("Decoding 32-bit CBG2 images works")
+{
+    CbgConverter converter;
+    tests::assert_image_conversion(
+        converter,
+        "tests/fmt/bgi/files/cbg-v2/ms_wn_base",
+        "tests/fmt/bgi/files/cbg-v2/ms_wn_base-out.png");
+}
+
+TEST_CASE("Decoding 24-bit CBG2 images works")
+{
+    CbgConverter converter;
+    tests::assert_image_conversion(
+        converter,
+        "tests/fmt/bgi/files/cbg-v2/l_card000",
+        "tests/fmt/bgi/files/cbg-v2/l_card000-out.png");
+}
+
+TEST_CASE("Decoding 8-bit CBG2 images works")
+{
+    CbgConverter converter;
+    tests::assert_image_conversion(
+        converter,
+        "tests/fmt/bgi/files/cbg-v2/mask04r",
+        "tests/fmt/bgi/files/cbg-v2/mask04r-out.png");
 }
