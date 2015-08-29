18cd86dc0468872a5e3881436800225bb394c236
rr- <mkurczew@gmail.com>
2015-08-29 20:08:27 +0200

    style: change SAR unpacker to use Table idiom

--- src/fmt/nscripter/sar_archive.cc
+++ src/fmt/nscripter/sar_archive.cc
@@ -21,6 +21,26 @@ namespace
         u32 offset;
         u32 size;
     };
+
+    using Table = std::vector<std::unique_ptr<TableEntry>>;
+}
+
+static Table read_table(io::IO &arc_io)
+{
+    u16 file_count = arc_io.read_u16_be();
+    u32 offset_to_files = arc_io.read_u32_be();
+
+    Table table;
+    for (auto i : util::range(file_count))
+    {
+        std::unique_ptr<TableEntry> entry(new TableEntry);
+        entry->name = arc_io.read_to_zero().str();
+        entry->offset = arc_io.read_u32_be() + offset_to_files;
+        entry->size = arc_io.read_u32_be();
+        table.push_back(std::move(entry));
+    }
+
+    return table;
 }
 
 static std::unique_ptr<File> read_file(io::IO &arc_io, const TableEntry &entry)
@@ -41,20 +61,7 @@ bool SarArchive::is_recognized_internal(File &arc_file) const
 
 void SarArchive::unpack_internal(File &arc_file, FileSaver &file_saver) const
 {
-    u16 file_count = arc_file.io.read_u16_be();
-    u32 offset_to_files = arc_file.io.read_u32_be();
-
-    std::vector<std::unique_ptr<TableEntry>> table;
-    table.reserve(file_count);
-    for (auto i : util::range(file_count))
-    {
-        std::unique_ptr<TableEntry> entry(new TableEntry);
-        entry->name = arc_file.io.read_to_zero().str();
-        entry->offset = arc_file.io.read_u32_be() + offset_to_files;
-        entry->size = arc_file.io.read_u32_be();
-        table.push_back(std::move(entry));
-    }
-
+    auto table = read_table(arc_file.io);
     for (auto &entry : table)
         file_saver.save(read_file(arc_file.io, *entry));
 }
