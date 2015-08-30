vn-tools/arc_unpacker

commit 9a466024569f1fc7622f5b32b82d9fb85be94478

fmt: simplify QLiE's ABMP10 file processing

src/fmt/qlie/abmp10_archive.cc
@@ -63,7 +63,6 @@ static std::unique_ptr<File> read_file(io::IO &arc_io)
     std::unique_ptr<File> subfile(new File);
     subfile->io.write_from_io(arc_io, len);
     subfile->name = (name == "" ? "unknown" : name) + ".dat";
-    subfile->guess_extension();
     return subfile;
 }
 
@@ -88,27 +87,22 @@ void Abmp10Archive::unpack_internal(File &arc_file, FileSaver &file_saver) const
             size_t size = arc_file.io.read_u32_le();
             arc_file.io.skip(size);
         }
-        else if (magic == magic_image10)
+        else if (magic == magic_image10 || magic == magic_image10)
         {
-            size_t image_count = arc_file.io.read_u8();
-            for (auto i : util::range(image_count))
+            size_t file_count = arc_file.io.read_u8();
+            for (auto i : util::range(file_count))
             {
                 auto subfile = read_file(arc_file.io);
                 if (subfile != nullptr)
+                {
+                    subfile->guess_extension();
                     file_saver.save(std::move(subfile));
-            }
-        }
-        else if (magic == magic_sound10)
-        {
-            size_t sound_count = arc_file.io.read_u8();
-            for (auto i : util::range(sound_count))
-            {
-                auto subfile = read_file(arc_file.io);
-                if (subfile != nullptr)
-                    file_saver.save(std::move(subfile));
+                }
             }
         }
         else
+        {
             throw std::runtime_error("Unknown section " + magic);
+        }
     }
 }

commit 8fb2e3a439a196b2da65696ad2ba3055f9857a43

fmt: fix QLiE's ABMP10 unpacking
Was broken since 9a466024569f1fc7622f5b32b82d9fb85be94478.

src/fmt/qlie/abmp10_archive.cc
@@ -83,21 +83,21 @@ void Abmp10Archive::unpack_internal(File &arc_file, FileSaver &file_saver) const
     {
         bstr magic = arc_file.io.read(16);
         if (magic == magic_data10
-        || magic == magic_data11
-        || magic == magic_data12
-        || magic == magic_data13)
+            || magic == magic_data11
+            || magic == magic_data12
+            || magic == magic_data13)
         {
             //interesting
             size_t size = arc_file.io.read_u32_le();
             arc_file.io.skip(size);
         }
-        else if (magic == magic_image10 || magic == magic_image10)
+        else if (magic == magic_image10 || magic == magic_sound10)
         {
             size_t file_count = arc_file.io.read_u8();
             for (auto i : util::range(file_count))
             {
                 auto subfile = read_file(arc_file.io);
-                if (subfile != nullptr)
+                if (subfile)
                 {
                     subfile->guess_extension();
                     file_saver.save(std::move(subfile));