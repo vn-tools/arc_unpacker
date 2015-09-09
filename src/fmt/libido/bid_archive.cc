35f5a75a7da878a5b04661f66e5da80e112c2e36
rr- <mkurczew@gmail.com>
2015-09-09 23:44:34 +0200

    fmt: support Libido's BID archives

---
+++ src/fmt/libido/bid_archive.cc
@@ -0,0 +1,69 @@
+// BID archive
+//
+// Company:   Libido
+// Engine:    -
+// Extension: .BID
+//
+// Known games:
+// - [Libido] [970613] Houkago Mania Club
+
+#include "fmt/libido/bid_archive.h"
+#include "util/range.h"
+
+using namespace au;
+using namespace au::fmt::libido;
+
+namespace
+{
+    struct TableEntry
+    {
+        std::string name;
+        size_t size;
+        size_t offset;
+    };
+
+    using Table = std::vector<std::unique_ptr<TableEntry>>;
+}
+
+static Table read_table(io::IO &arc_io)
+{
+    Table table;
+    u32 data_start = arc_io.read_u32_le();
+    arc_io.skip(4);
+    while (arc_io.tell() < data_start)
+    {
+        std::unique_ptr<TableEntry> entry(new TableEntry);
+        entry->name = arc_io.read_to_zero(16).str();
+        entry->offset = arc_io.read_u32_le() + data_start;
+        entry->size = arc_io.read_u32_le();
+        table.push_back(std::move(entry));
+    }
+    return table;
+}
+
+static std::unique_ptr<File> read_file(io::IO &arc_io, const TableEntry &entry)
+{
+    std::unique_ptr<File> file(new File);
+    arc_io.seek(entry.offset);
+    file->io.write_from_io(arc_io, entry.size);
+    file->name = entry.name;
+    return file;
+}
+
+bool BidArchive::is_recognized_internal(File &arc_file) const
+{
+    auto data_start = arc_file.io.read_u32_le();
+    arc_file.io.seek(data_start - 8);
+    auto last_file_offset = arc_file.io.read_u32_le() + data_start;
+    auto last_file_size = arc_file.io.read_u32_le();
+    return last_file_offset + last_file_size == arc_file.io.size();
+}
+
+void BidArchive::unpack_internal(File &arc_file, FileSaver &file_saver) const
+{
+    Table table = read_table(arc_file.io);
+    for (auto &entry : table)
+        file_saver.save(read_file(arc_file.io, *entry));
+}
+
+static auto dummy = fmt::Registry::add<BidArchive>("libido/bid");
---
+++ tests/fmt/libido/bid_archive_test.cc
@@ -0,0 +1,21 @@
+#include "fmt/libido/bid_archive.h"
+#include "test_support/archive_support.h"
+#include "test_support/catch.hh"
+#include "test_support/file_support.h"
+
+using namespace au;
+using namespace au::fmt::libido;
+
+TEST_CASE("Unpacking Libido's BID archives works")
+{
+    std::vector<std::shared_ptr<File>> expected_files
+    {
+        tests::stub_file("123.txt", "1234567890"_b),
+        tests::stub_file("abc.txt", "abcdefghijklmnopqrstuvwxyz"_b),
+    };
+
+    BidArchive archive;
+    auto actual_files = tests::unpack_to_memory(
+        "tests/fmt/libido/files/bid/test.BID", archive);
+    tests::compare_files(expected_files, actual_files, true);
+}
