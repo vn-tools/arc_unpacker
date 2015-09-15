5ba8490014e3c3c3dd75d438fe679c2ecbde19e0
rr- <mkurczew@gmail.com>
2015-09-15 12:37:39 +0200

    tests: remove useless class
    This class made sense when Log wasn't around and all output went
    straight to std::cout and std::cerr.

--- tests/arc_unpacker_test.cc
+++ tests/arc_unpacker_test.cc
@@ -1,49 +1,47 @@
 #include <boost/filesystem.hpp>
 #include "arc_unpacker.h"
+#include "log.h"
 #include "test_support/catch.hh"
-#include "test_support/suppress_output.h"
 
 using namespace au;
 
 TEST_CASE("Converting single files with CLI facade works")
 {
-    tests::suppress_output([&]()
-    {
-        ArcUnpacker arc_unpacker(
-            {
-                "arc_unpacker_executable",
-                "./tests/fmt/real_live/files/g00-2/AYU_03.g00",
-                "--fmt=rl/g00"
-            },
-            "0.0");
+    ArcUnpacker arc_unpacker(
+        {
+            "arc_unpacker_executable",
+            "./tests/fmt/real_live/files/g00-2/AYU_03.g00",
+            "--fmt=rl/g00"
+        },
+        "0.0");
 
-        arc_unpacker.run();
+    Log.mute();
+    arc_unpacker.run();
+    Log.unmute();
 
-        REQUIRE(boost::filesystem::is_regular_file("./AYU_03.png"));
-        boost::filesystem::remove("./AYU_03.png");
-    });
+    REQUIRE(boost::filesystem::is_regular_file("./AYU_03.png"));
+    boost::filesystem::remove("./AYU_03.png");
 }
 
 TEST_CASE("Unpacking archives with CLI facade works")
 {
-    tests::suppress_output([&]()
-    {
-        ArcUnpacker arc_unpacker(
-            {
-                "arc_unpacker_executable",
-                "./tests/fmt/kirikiri/files/xp3/xp3-v2.xp3",
-                "--fmt=krkr/xp3",
-                "--plugin=noop"
-            },
-            "0.0");
+    ArcUnpacker arc_unpacker(
+        {
+            "arc_unpacker_executable",
+            "./tests/fmt/kirikiri/files/xp3/xp3-v2.xp3",
+            "--fmt=krkr/xp3",
+            "--plugin=noop"
+        },
+        "0.0");
 
-        arc_unpacker.run();
+    Log.mute();
+    arc_unpacker.run();
+    Log.unmute();
 
-        REQUIRE(boost::filesystem::is_directory("./xp3-v2~.xp3"));
-        REQUIRE(boost::filesystem::is_regular_file("./xp3-v2~.xp3/abc.txt"));
-        REQUIRE(boost::filesystem::is_regular_file("./xp3-v2~.xp3/abc2.txt"));
-        boost::filesystem::remove("./xp3-v2~.xp3/abc2.txt");
-        boost::filesystem::remove("./xp3-v2~.xp3/abc.txt");
-        boost::filesystem::remove("./xp3-v2~.xp3");
-    });
+    REQUIRE(boost::filesystem::is_directory("./xp3-v2~.xp3"));
+    REQUIRE(boost::filesystem::is_regular_file("./xp3-v2~.xp3/abc.txt"));
+    REQUIRE(boost::filesystem::is_regular_file("./xp3-v2~.xp3/abc2.txt"));
+    boost::filesystem::remove("./xp3-v2~.xp3/abc2.txt");
+    boost::filesystem::remove("./xp3-v2~.xp3/abc.txt");
+    boost::filesystem::remove("./xp3-v2~.xp3");
 }
--- tests/file_saver_test.cc
+++ tests/file_saver_test.cc
@@ -1,11 +1,11 @@
 #include <boost/filesystem.hpp>
+#include "log.h"
 #include "file_saver.h"
 #include "test_support/catch.hh"
-#include "test_support/suppress_output.h"
 
 using namespace au;
 
-static void test(const boost::filesystem::path &path)
+static void do_test(const boost::filesystem::path &path)
 {
     FileSaverHdd file_saver(".", true);
 
@@ -13,7 +13,9 @@ static void test(const boost::filesystem::path &path)
     file->io.write("test"_b);
     file->name = path.string();
 
+    Log.mute();
     file_saver.save(file);
+    Log.unmute();
 
     REQUIRE(boost::filesystem::exists(path));
     {
@@ -39,8 +41,10 @@ static void do_test_overwriting(
     {
         REQUIRE(!boost::filesystem::exists(path));
         REQUIRE(!boost::filesystem::exists(path2));
+        Log.mute();
         file_saver1.save(file);
         file_saver2.save(file);
+        Log.unmute();
         REQUIRE(boost::filesystem::exists(path));
         REQUIRE(boost::filesystem::exists(path2) == renamed_file_exists);
         if (boost::filesystem::exists(path)) boost::filesystem::remove(path);
@@ -56,41 +60,29 @@ static void do_test_overwriting(
 
 TEST_CASE("Unicode file names work")
 {
-    tests::suppress_output([&]()
-    {
-        test("test.out");
-        test("ąćę.out");
-        test("不用意な変換.out");
-    });
+    do_test("test.out");
+    do_test("ąćę.out");
+    do_test("不用意な変換.out");
 }
 
 TEST_CASE("Two file savers overwrite the same file")
 {
-    tests::suppress_output([&]()
-    {
-        FileSaverHdd file_saver1(".", true);
-        FileSaverHdd file_saver2(".", true);
-        do_test_overwriting(file_saver1, file_saver2, false);
-    });
+    FileSaverHdd file_saver1(".", true);
+    FileSaverHdd file_saver2(".", true);
+    do_test_overwriting(file_saver1, file_saver2, false);
 }
 
 TEST_CASE("Two file savers don't overwrite the same file")
 {
-    tests::suppress_output([&]()
-    {
-        FileSaverHdd file_saver1(".", false);
-        FileSaverHdd file_saver2(".", false);
-        do_test_overwriting(file_saver1, file_saver2, true);
-    });
+    FileSaverHdd file_saver1(".", false);
+    FileSaverHdd file_saver2(".", false);
+    do_test_overwriting(file_saver1, file_saver2, true);
 }
 
 TEST_CASE("One file saver never overwrites the same file")
 {
-    tests::suppress_output([&]()
-    {
-        //even if we pass overwrite=true, files within the same archive with the
-        //same name are too valuable to be ovewritten silently
-        FileSaverHdd file_saver(".", true);
-        do_test_overwriting(file_saver, file_saver, true);
-    });
+    // even if we pass overwrite=true, files within the same archive with the
+    // same name are too valuable to be ovewritten silently
+    FileSaverHdd file_saver(".", true);
+    do_test_overwriting(file_saver, file_saver, true);
 }
