70363186fdf3601cfdb612480ce1af8b4f1b3802
rr- <mkurczew@gmail.com>
2015-09-22 21:05:06 +0200

    fmt: simplify Alice Soft's QNT pixel operations
    Turns out the expected size calculations were wrong. Rather than trying
    to fix these already overcomplicated formulas I just switched to
    io::BufferedIO, which improves readability and fixes these corner cases.

--- src/fmt/alice_soft/qnt_converter.cc
+++ src/fmt/alice_soft/qnt_converter.cc
@@ -1,9 +1,11 @@
 #include "fmt/alice_soft/qnt_converter.h"
 #include "err.h"
-#include "util/image.h"
+#include "io/buffered_io.h"
 #include "util/format.h"
+#include "util/image.h"
 #include "util/pack/zlib.h"
 #include "util/range.h"
+#include "log.h"
 
 using namespace au;
 using namespace au::fmt::alice_soft;
@@ -22,45 +24,40 @@ namespace
 
 static void deinterleave(pix::Grid &pixels, const bstr &input)
 {
-    size_t expected_size = 3;
-    expected_size *= ((pixels.width() + 1) / 2) * 2;
-    expected_size *= ((pixels.height() + 1) / 2) * 2;
-    if (input.size() != expected_size)
-        throw err::BadDataSizeError();
+    io::BufferedIO input_io(input);
 
-    const u8 *input_ptr = input.get<const u8>();
     size_t x, y;
-    for (size_t i : util::range(3))
+    for (auto i : util::range(3))
     {
         for (y = 0; y < pixels.height() - 1; y += 2)
         {
             for (x = 0; x < pixels.width() - 1; x += 2)
             {
-                pixels.at(x, y)[i] = input_ptr[0];
-                pixels.at(x, y + 1)[i] = input_ptr[1];
-                pixels.at(x + 1, y)[i] = input_ptr[2];
-                pixels.at(x + 1, y + 1)[i] = input_ptr[3];
-                input_ptr += 4;
+                pixels.at(x, y)[i] = input_io.read_u8();
+                pixels.at(x, y + 1)[i] = input_io.read_u8();
+                pixels.at(x + 1, y)[i] = input_io.read_u8();
+                pixels.at(x + 1, y + 1)[i] = input_io.read_u8();
             }
             if (x != pixels.width())
             {
-                pixels.at(x, y)[i] = input_ptr[0];
-                pixels.at(x, y + 1)[i] = input_ptr[1];
-                input_ptr += 4;
+                pixels.at(x, y)[i] = input_io.read_u8();
+                pixels.at(x, y + 1)[i] = input_io.read_u8();
+                input_io.skip(2);
             }
         }
         if (y != pixels.height())
         {
             for (x = 0; x < pixels.width() - 1; x += 2)
             {
-                pixels.at(x, y)[i] = input_ptr[0];
-                pixels.at(x + 1, y)[i] = input_ptr[2];
-                input_ptr += 4;
+                pixels.at(x, y)[i] = input_io.read_u8();
+                input_io.skip(1);
+                pixels.at(x + 1, y)[i] = input_io.read_u8();
+                input_io.skip(1);
             }
             if (x != pixels.width())
             {
-                pixels.at(x, y)[i] = input_ptr[0];
-                input_ptr += 4;
+                pixels.at(x, y)[i] = input_io.read_u8();
+                input_io.skip(3);
             }
         }
     }
@@ -68,25 +65,21 @@ static void deinterleave(pix::Grid &pixels, const bstr &input)
 
 static void apply_differences(pix::Grid &pixels)
 {
-    for (size_t x : util::range(1, pixels.width()))
-        for (size_t c : util::range(3))
-            pixels.at(x, 0)[c] = pixels.at(x - 1, 0)[c] - pixels.at(x, 0)[c];
+    for (auto x : util::range(1, pixels.width()))
+    for (auto c : util::range(3))
+        pixels.at(x, 0)[c] = pixels.at(x - 1, 0)[c] - pixels.at(x, 0)[c];
 
-    for (size_t y : util::range(1, pixels.height()))
-        for (size_t c : util::range(3))
-            pixels.at(0, y)[c] = pixels.at(0, y - 1)[c] - pixels.at(0, y)[c];
+    for (auto y : util::range(1, pixels.height()))
+    for (auto c : util::range(3))
+        pixels.at(0, y)[c] = pixels.at(0, y - 1)[c] - pixels.at(0, y)[c];
 
-    for (size_t y : util::range(1, pixels.height()))
+    for (auto y : util::range(1, pixels.height()))
+    for (auto x : util::range(1, pixels.width()))
+    for (auto c : util::range(3))
     {
-        for (size_t x : util::range(1, pixels.width()))
-        {
-            for (size_t c : util::range(3))
-            {
-                u8 ax = pixels.at(x - 1, y)[c];
-                u8 ay = pixels.at(x, y - 1)[c];
-                pixels.at(x, y)[c] = (ax + ay) / 2 - pixels.at(x, y)[c];
-            }
-        }
+        u8 ax = pixels.at(x - 1, y)[c];
+        u8 ay = pixels.at(x, y - 1)[c];
+        pixels.at(x, y)[c] = (ax + ay) / 2 - pixels.at(x, y)[c];
     }
 }
 
@@ -100,37 +93,25 @@ static void apply_alpha(pix::Grid &pixels, const bstr &input)
         return;
     }
 
-    size_t expected_size = pixels.width() * pixels.height();
-    expected_size += (pixels.width() & 1) * pixels.height();
-    expected_size += (pixels.height() & 1) * pixels.width();
-    expected_size += (pixels.height() & 1) * (pixels.width() & 1);
-    if (input.size() != expected_size)
-        throw err::BadDataSizeError();
+    io::BufferedIO input_io(input);
 
-    const u8 *input_ptr = input.get<const u8>();
-    if (pixels.width() > 1)
-    {
-        pixels.at(0, 0).a = *input_ptr++;
-        for (size_t x : util::range(1, pixels.width()))
-            pixels.at(x, 0).a = pixels.at(x - 1, 0).a - *input_ptr++;
-        input_ptr += pixels.width() & 1;
-    }
-    else
-        input_ptr++;
+    pixels.at(0, 0).a = input_io.read_u8();
+    for (auto x : util::range(1, pixels.width()))
+        pixels.at(x, 0).a = pixels.at(x - 1, 0).a - input_io.read_u8();
+    if (pixels.width() & 1)
+        input_io.skip(1);
 
-    if (pixels.height() > 1)
+    for (auto y : util::range(1, pixels.height()))
     {
-        for (size_t y : util::range(1, pixels.height()))
+        pixels.at(0, y).a = pixels.at(0, y - 1).a - input_io.read_u8();
+        for (auto x : util::range(1, pixels.width()))
         {
-            pixels.at(0, y).a = pixels.at(0, y - 1).a - *input_ptr++;
-            for (size_t x : util::range(1, pixels.width()))
-            {
-                u8 ax = pixels.at(x - 1, y).a;
-                u8 ay = pixels.at(x, y - 1).a;
-                pixels.at(x, y).a = (ax + ay) / 2 - *input_ptr++;
-            }
-            input_ptr += pixels.width() & 1;
+            u8 ax = pixels.at(x - 1, y).a;
+            u8 ay = pixels.at(x, y - 1).a;
+            pixels.at(x, y).a = (ax + ay) / 2 - input_io.read_u8();
         }
+        if (pixels.width() & 1)
+            input_io.skip(1);
     }
 }
 
