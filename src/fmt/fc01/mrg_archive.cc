96c4d626ebce78882c0e7eb80887d3d09fe25786
rr- <mkurczew@gmail.com>
2015-09-23 22:21:35 +0200

    fmt: support filters in F&C01's MRG archives

--- src/fmt/fc01/mrg_archive.cc
+++ src/fmt/fc01/mrg_archive.cc
@@ -1,6 +1,7 @@
 #include "fmt/fc01/mrg_archive.h"
-#include "fmt/fc01/acd_converter.h"
 #include "err.h"
+#include "fmt/fc01/acd_converter.h"
+#include "fmt/fc01/custom_lzss.h"
 #include "io/buffered_io.h"
 #include "util/range.h"
 
@@ -17,7 +18,7 @@ namespace
         u32 offset;
         u32 size_orig;
         u32 size_comp;
-        u8 compression_type;
+        u8 filter;
     };
 
     using Table = std::vector<std::unique_ptr<TableEntry>>;
@@ -48,6 +49,79 @@ static u8 guess_key(const bstr &table_data, size_t file_size)
     return key;
 }
 
+static u16 get_mask(u16 d)
+{
+    d--;
+    d >>= 8;
+    u16 result = 0xFF;
+    while (d)
+    {
+        d >>= 1;
+        result = (result << 1) | 1;
+    }
+    return result;
+}
+
+static bstr mrg_decrypt(const bstr &input)
+{
+    io::BufferedIO input_io(input);
+    input_io.seek(0x104);
+    auto size_orig = input_io.read_u32_le();
+    input_io.seek(0);
+    size_orig ^= input_io.read_u32_le();
+
+    bstr output(size_orig);
+    auto output_ptr = output.get<u8>();
+    auto output_end = output.end<const u8>();
+
+    u16 arr1[0x101] { };
+    u16 arr2[0x101] { };
+    std::vector<u8> arr3;
+
+    for (auto i : util::range(0x100))
+    {
+        arr2[i] = input_io.read_u8();
+        arr1[i + 1] = arr2[i] + arr1[i];
+    }
+
+    for (auto i : util::range(0x100))
+        for (auto j : util::range(arr2[i]))
+            arr3.push_back(i);
+
+    u16 quant = arr1[0x100];
+    if (!quant)
+        throw err::CorruptDataError("Unexpected data");
+    auto mask = get_mask(quant);
+    auto scale = 0x10000 / quant;
+    auto a = input_io.read_u32_be();
+    auto b = 0;
+    auto c = 0xFFFFFFFF;
+    while (output_ptr < output_end)
+    {
+        c = ((c >> 8) * scale) >> 8;
+        auto v = (a - b) / c;
+        v = arr3.at(v);
+        *output_ptr++ = v;
+        b += arr1[v] * c;
+        c *= arr2[v];
+        while (!(((c + b) ^ b) & 0xFF000000))
+        {
+            a <<= 8;
+            b <<= 8;
+            c <<= 8;
+            a |= input_io.read_u8();
+        }
+        while (c <= mask)
+        {
+            c = (~b & mask) << 8;
+            a = (a << 8) | input_io.read_u8();
+            b <<= 8;
+        }
+    }
+
+    return output;
+}
+
 static Table read_table(io::IO &arc_io)
 {
     arc_io.skip(4);
@@ -69,7 +143,7 @@ static Table read_table(io::IO &arc_io)
         std::unique_ptr<TableEntry> entry(new TableEntry);
         entry->name = table_io.read_to_zero(0x0E).str();
         entry->size_orig = table_io.read_u32_le();
-        entry->compression_type = table_io.read_u8();
+        entry->filter = table_io.read_u8();
         table_io.skip(9);
         entry->offset = table_io.read_u32_le();
         table[i] = std::move(entry);
@@ -90,8 +164,13 @@ static std::unique_ptr<File> read_file(io::IO &arc_io, const TableEntry &entry)
     std::unique_ptr<File> file(new File);
     arc_io.seek(entry.offset);
     auto data = arc_io.read(entry.size_comp);
-    if (entry.compression_type != 0)
-        Log.err("Compressed MRG is unsupported yet\n");
+    if (entry.filter)
+    {
+        if (entry.filter >= 2)
+            data = mrg_decrypt(data);
+        if (entry.filter < 3)
+            data = custom_lzss_decompress(data, entry.size_orig);
+    }
     file->io.write(data);
     file->name = entry.name;
     return file;
