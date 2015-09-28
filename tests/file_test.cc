vn-tools/arc_unpacker

commit 4ffeba293748ef5ac17f07ee8804def0d8159c0d

au: implement file extension stripping

src/file.cc
@@ -10,22 +10,21 @@ static void change_extension(
     boost::filesystem::path &path, const std::string &new_extension)
 {
     if (path.filename().empty()
-        || new_extension.empty()
+        || path.stem().empty()
         || path.filename() == "."
-        || path.filename() == ".."
-        || path.stem() == "")
+        || path.filename() == "..")
     {
         return;
     }
 
-    if (new_extension[0] == '.')
-    {
-        path.replace_extension(new_extension);
-    }
-    else
-    {
-        path.replace_extension("." + new_extension);
-    }
+    auto plain_path = path.string();
+    auto extension = new_extension;
+    auto index = plain_path.find_last_of('.');
+    while (extension[0] == '.')
+        extension.erase(0, 1);
+    if (!extension.empty() && extension[0] != '.')
+        extension = "." + extension;
+    path = plain_path.substr(0, index) + extension;
 }
 
 File::File(const boost::filesystem::path &path, const io::FileMode mode)

tests/file_test.cc
@@ -59,3 +59,22 @@ TEST_CASE("Changing File's extension", "[core][file]")
     test_changing_extension("abc/def.",  "xyz",  "abc/def.xyz");
     test_changing_extension("abc/.def.", "xyz",  "abc/.def.xyz");
 }
+
+TEST_CASE("Stripping File's extension", "[core][file]")
+{
+    test_changing_extension("",          "",  "");
+    test_changing_extension(".",         "",  ".");
+    test_changing_extension("..",        "",  "..");
+    test_changing_extension("abc",       "",  "abc");
+    test_changing_extension("abc.de",    "",  "abc");
+    test_changing_extension("abc.de",    ".", "abc");
+    test_changing_extension("abc.",      "",  "abc");
+    test_changing_extension(".abc.",     "",  ".abc");
+    test_changing_extension("abc",       "",  "abc");
+    test_changing_extension("abc/",      "",  "abc/");
+    test_changing_extension("abc/.",     "",  "abc/.");
+    test_changing_extension("abc/..",    "",  "abc/..");
+    test_changing_extension("abc/def",   "",  "abc/def");
+    test_changing_extension("abc/def.",  "",  "abc/def");
+    test_changing_extension("abc/.def.", "",  "abc/.def");
+}

commit 9b697ee877be67d988a514bf94c18588a7b31d18

fmt: fix changing File's extension
Quite severe bug introduced in 4ffeba293748ef5ac17f07ee8804def0d8159c0d.

src/file.cc
@@ -17,14 +17,14 @@ static void change_extension(
         return;
     }
 
-    auto plain_path = path.string();
+    auto filename = path.filename().string();
     auto extension = new_extension;
-    auto index = plain_path.find_last_of('.');
+    auto index = filename.find_last_of('.');
     while (extension[0] == '.')
         extension.erase(0, 1);
     if (!extension.empty() && extension[0] != '.')
         extension = "." + extension;
-    path = plain_path.substr(0, index) + extension;
+    path = path.parent_path() / (filename.substr(0, index) + extension);
 }
 
 File::File(const boost::filesystem::path &path, const io::FileMode mode)

tests/file_test.cc
@@ -43,38 +43,54 @@ TEST_CASE("Checking File's extension", "[core][file]")
 
 TEST_CASE("Changing File's extension", "[core][file]")
 {
-    test_changing_extension("",          "xyz",  "");
-    test_changing_extension(".",         "xyz",  ".");
-    test_changing_extension("..",        "xyz",  "..");
-    test_changing_extension("abc",       "xyz",  "abc.xyz");
-    test_changing_extension("abc.de",    "xyz",  "abc.xyz");
-    test_changing_extension("abc.de",    ".xyz", "abc.xyz");
-    test_changing_extension("abc.",      "xyz",  "abc.xyz");
-    test_changing_extension(".abc.",     "xyz",  ".abc.xyz");
-    test_changing_extension("abc",       "xyz",  "abc.xyz");
-    test_changing_extension("abc/",      "xyz",  "abc/");
-    test_changing_extension("abc/.",     "xyz",  "abc/.");
-    test_changing_extension("abc/..",    "xyz",  "abc/..");
-    test_changing_extension("abc/def",   "xyz",  "abc/def.xyz");
-    test_changing_extension("abc/def.",  "xyz",  "abc/def.xyz");
-    test_changing_extension("abc/.def.", "xyz",  "abc/.def.xyz");
+    test_changing_extension("",              "xyz",  "");
+    test_changing_extension(".",             "xyz",  ".");
+    test_changing_extension("..",            "xyz",  "..");
+    test_changing_extension("abc",           "xyz",  "abc.xyz");
+    test_changing_extension("abc.de",        "xyz",  "abc.xyz");
+    test_changing_extension("abc.de",        ".xyz", "abc.xyz");
+    test_changing_extension("abc.",          "xyz",  "abc.xyz");
+    test_changing_extension(".abc.",         "xyz",  ".abc.xyz");
+    test_changing_extension("abc",           "xyz",  "abc.xyz");
+    test_changing_extension("abc/",          "xyz",  "abc/");
+    test_changing_extension("abc/.",         "xyz",  "abc/.");
+    test_changing_extension("abc/..",        "xyz",  "abc/..");
+    test_changing_extension("abc/def",       "xyz",  "abc/def.xyz");
+    test_changing_extension("abc/def.",      "xyz",  "abc/def.xyz");
+    test_changing_extension("abc/.def.",     "xyz",  "abc/.def.xyz");
+    test_changing_extension("./abc/def.gh",  "xyz",  "./abc/def.xyz");
+    test_changing_extension("../abc/def.gh", "xyz",  "../abc/def.xyz");
+    test_changing_extension("abc/./def.gh",  "xyz",  "abc/./def.xyz");
+    test_changing_extension("abc/../def.gh", "xyz",  "abc/../def.xyz");
+    test_changing_extension("./abc/def",     "xyz",  "./abc/def.xyz");
+    test_changing_extension("../abc/def",    "xyz",  "../abc/def.xyz");
+    test_changing_extension("abc/./def",     "xyz",  "abc/./def.xyz");
+    test_changing_extension("abc/../def",    "xyz",  "abc/../def.xyz");
 }
 
 TEST_CASE("Stripping File's extension", "[core][file]")
 {
-    test_changing_extension("",          "",  "");
-    test_changing_extension(".",         "",  ".");
-    test_changing_extension("..",        "",  "..");
-    test_changing_extension("abc",       "",  "abc");
-    test_changing_extension("abc.de",    "",  "abc");
-    test_changing_extension("abc.de",    ".", "abc");
-    test_changing_extension("abc.",      "",  "abc");
-    test_changing_extension(".abc.",     "",  ".abc");
-    test_changing_extension("abc",       "",  "abc");
-    test_changing_extension("abc/",      "",  "abc/");
-    test_changing_extension("abc/.",     "",  "abc/.");
-    test_changing_extension("abc/..",    "",  "abc/..");
-    test_changing_extension("abc/def",   "",  "abc/def");
-    test_changing_extension("abc/def.",  "",  "abc/def");
-    test_changing_extension("abc/.def.", "",  "abc/.def");
+    test_changing_extension("",              "",  "");
+    test_changing_extension(".",             "",  ".");
+    test_changing_extension("..",            "",  "..");
+    test_changing_extension("abc",           "",  "abc");
+    test_changing_extension("abc.de",        "",  "abc");
+    test_changing_extension("abc.de",        ".", "abc");
+    test_changing_extension("abc.",          "",  "abc");
+    test_changing_extension(".abc.",         "",  ".abc");
+    test_changing_extension("abc",           "",  "abc");
+    test_changing_extension("abc/",          "",  "abc/");
+    test_changing_extension("abc/.",         "",  "abc/.");
+    test_changing_extension("abc/..",        "",  "abc/..");
+    test_changing_extension("abc/def",       "",  "abc/def");
+    test_changing_extension("abc/def.",      "",  "abc/def");
+    test_changing_extension("abc/.def.",     "",  "abc/.def");
+    test_changing_extension("./abc/def.gh",  "",  "./abc/def");
+    test_changing_extension("../abc/def.gh", "",  "../abc/def");
+    test_changing_extension("abc/./def.gh",  "",  "abc/./def");
+    test_changing_extension("abc/../def.gh", "",  "abc/../def");
+    test_changing_extension("./abc/def",     "",  "./abc/def");
+    test_changing_extension("../abc/def",    "",  "../abc/def");
+    test_changing_extension("abc/./def",     "",  "abc/./def");
+    test_changing_extension("abc/../def",    "",  "abc/../def");
 }