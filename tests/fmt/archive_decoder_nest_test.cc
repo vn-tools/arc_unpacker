1ad0735ce39b8e36b6f1e9681c5517198fdb10d1
rr- <mkurczew@gmail.com>
2015-10-05 19:41:30 +0200

    tests: test disabling recursive decoding

--- tests/fmt/archive_decoder_nest_test.cc
+++ tests/fmt/archive_decoder_nest_test.cc
@@ -1,6 +1,7 @@
 #include <boost/filesystem.hpp>
 #include "fmt/archive_decoder.h"
 #include "fmt/file_decoder.h"
+#include "io/buffered_io.h"
 #include "test_support/catch.hh"
 
 using namespace au;
@@ -27,8 +28,7 @@ namespace
         TestArchiveDecoder();
     protected:
         bool is_recognized_internal(File &arc_file) const override;
-        void unpack_internal(
-            File &arc_file, FileSaver &file_saver) const override;
+        void unpack_internal(File &arc_file, FileSaver &saver) const override;
     };
 }
 
@@ -36,25 +36,36 @@ static std::vector<std::shared_ptr<File>> unpack(
     File &file, ArchiveDecoder &decoder)
 {
     std::vector<std::shared_ptr<File>> saved_files;
-    FileSaverCallback file_saver([&](std::shared_ptr<File> saved_file)
+    FileSaverCallback saver([&](std::shared_ptr<File> saved_file)
     {
         saved_file->io.seek(0);
         saved_files.push_back(saved_file);
     });
-    decoder.unpack(file, file_saver);
+    decoder.unpack(file, saver);
     return saved_files;
+}
+
+static bstr serialize_file(const std::string &name, const bstr &content)
+{
+    io::BufferedIO io;
+    io.write(name);
+    io.write_u8(0);
+    io.write_u32_le(content.size());
+    io.write(content);
+    io.seek(0);
+    return io.read_to_eof();
 }
 
 bool TestFileDecoder::is_recognized_internal(File &file) const
 {
-    if (recognition_callback != nullptr)
+    if (recognition_callback)
         recognition_callback(file);
     return file.has_extension("image");
 }
 
 std::unique_ptr<File> TestFileDecoder::decode_internal(File &file) const
 {
-    if (conversion_callback != nullptr)
+    if (conversion_callback)
         conversion_callback(file);
     std::unique_ptr<File> output_file(new File);
     output_file->io.write("image"_b);
@@ -74,8 +85,7 @@ bool TestArchiveDecoder::is_recognized_internal(File &arc_file) const
     return arc_file.has_extension("archive");
 }
 
-void TestArchiveDecoder::unpack_internal(
-    File &arc_file, FileSaver &file_saver) const
+void TestArchiveDecoder::unpack_internal(File &arc_file, FileSaver &saver) const
 {
     while (!arc_file.io.eof())
     {
@@ -83,91 +93,96 @@ void TestArchiveDecoder::unpack_internal(
         output_file->name = arc_file.io.read_to_zero().str();
         size_t output_file_size = arc_file.io.read_u32_le();
         output_file->io.write(arc_file.io.read(output_file_size));
-        file_saver.save(std::move(output_file));
+        saver.save(std::move(output_file));
     }
 }
 
 TEST_CASE("Nested files unpack correctly", "[fmt_core]")
 {
     TestArchiveDecoder test_archive_decoder;
-    File dummy_file;
-    dummy_file.name = "test.archive";
-    dummy_file.io.write("deeply/nested/test.image"_b);
-    dummy_file.io.write_u8(0);
-    dummy_file.io.write_u32_le(0);
+    File test_archive_file;
+    test_archive_file.name = "test.archive";
+    test_archive_file.io.write(serialize_file("test.image", ""_b));
 
-    auto saved_files = unpack(dummy_file, test_archive_decoder);
-
+    auto saved_files = unpack(test_archive_file, test_archive_decoder);
     REQUIRE(saved_files.size() == 1);
-    REQUIRE(path(saved_files[0]->name) == path("deeply/nested/test.png"));
+    REQUIRE(path(saved_files[0]->name) == path("test.png"));
     REQUIRE(saved_files[0]->io.read_to_eof() == "image"_b);
 }
 
-TEST_CASE("Deeply nested archives unpack correctly", "[fmt_core]")
+TEST_CASE("Recursively nested files unpack correctly", "[fmt_core]")
 {
     TestArchiveDecoder test_archive_decoder;
-    File dummy_file;
-    dummy_file.name = "test.archive";
-    dummy_file.io.write("deeply/nested/test.archive"_b);
-    dummy_file.io.write_u8(0);
-    dummy_file.io.write_u32_le(0);
-    size_t pos = dummy_file.io.tell();
-    dummy_file.io.write("further/nested/file.txt"_b);
-    dummy_file.io.write_u8(0);
-    dummy_file.io.write_u32_le(0);
-    dummy_file.io.write("further/nested/test.image"_b);
-    dummy_file.io.write_u8(0);
-    dummy_file.io.write_u32_le(0);
-    size_t sub_archive_size = dummy_file.io.tell() - pos;
-    dummy_file.io.seek(pos - 4);
-    dummy_file.io.write_u32_le(sub_archive_size);
 
-    auto saved_files = unpack(dummy_file, test_archive_decoder);
+    io::BufferedIO nested_archive_io;
+    nested_archive_io.write(serialize_file("nested/file.txt", ""_b));
+    nested_archive_io.write(serialize_file("nested/test.image", ""_b));
+    nested_archive_io.seek(0);
+    auto nested_archive_content = nested_archive_io.read_to_eof();
 
+    File test_archive_file;
+    test_archive_file.name = "test.archive";
+    test_archive_file.io.write(
+        serialize_file("test.archive", nested_archive_content));
+
+    auto saved_files = unpack(test_archive_file, test_archive_decoder);
     REQUIRE(saved_files.size() == 2);
-    REQUIRE(path(saved_files[0]->name)
-        == path("deeply/nested/test.archive/further/nested/file.txt"));
+    REQUIRE(path(saved_files[0]->name) == path("test.archive/nested/file.txt"));
+    REQUIRE(path(saved_files[1]->name) == path("test.archive/nested/test.png"));
     REQUIRE(saved_files[0]->io.read_to_eof() == ""_b);
-    REQUIRE(path(saved_files[1]->name)
-        == path("deeply/nested/test.archive/further/nested/test.png"));
     REQUIRE(saved_files[1]->io.read_to_eof() == "image"_b);
 }
 
-TEST_CASE("Decoder receives full path", "[fmt_core]")
+TEST_CASE("Disabling recursive decoding", "[fmt_core]")
 {
     TestArchiveDecoder test_archive_decoder;
-    File dummy_file;
-    dummy_file.name = "test.archive";
-    dummy_file.io.write("deeply/nested/test.archive"_b);
-    dummy_file.io.write_u8(0);
-    dummy_file.io.write_u32_le(0);
-    size_t pos = dummy_file.io.tell();
-    dummy_file.io.write("further/nested/test.image"_b);
-    dummy_file.io.write_u8(0);
-    dummy_file.io.write_u32_le(0);
-    size_t sub_archive_size = dummy_file.io.tell() - pos;
-    dummy_file.io.seek(pos - 4);
-    dummy_file.io.write_u32_le(sub_archive_size);
+    test_archive_decoder.disable_nested_decoding();
+
+    io::BufferedIO nested_archive_io;
+    nested_archive_io.write(serialize_file("nested/file.txt", ""_b));
+    nested_archive_io.write(serialize_file("nested/test.image", ""_b));
+    nested_archive_io.seek(0);
+    auto nested_archive_content = nested_archive_io.read_to_eof();
+
+    File test_archive_file;
+    test_archive_file.name = "test.archive";
+    test_archive_file.io.write(
+        serialize_file("test.archive", nested_archive_content));
+
+    auto saved_files = unpack(test_archive_file, test_archive_decoder);
+    REQUIRE(saved_files.size() == 1);
+    REQUIRE(path(saved_files[0]->name) == path("test.archive"));
+    REQUIRE(saved_files[0]->io.read_to_eof() == nested_archive_content);
+}
+
+TEST_CASE("Internal decoders receive full paths", "[fmt_core]")
+{
+    TestArchiveDecoder test_archive_decoder;
+
+    io::BufferedIO nested_archive_io;
+    nested_archive_io.write(serialize_file("nested/test.image", ""_b));
+    nested_archive_io.seek(0);
+    auto nested_archive_content = nested_archive_io.read_to_eof();
+
+    File test_archive_file;
+    test_archive_file.name = "test.archive";
+    test_archive_file.io.write(
+        serialize_file("test.archive", nested_archive_content));
 
     std::vector<path> names_for_recognition;
     std::vector<path> names_for_conversion;
     test_archive_decoder.test_file_decoder.recognition_callback = [&](File &f)
-    {
-        names_for_recognition.push_back(path(f.name));
-    };
+        { names_for_recognition.push_back(path(f.name)); };
     test_archive_decoder.test_file_decoder.conversion_callback = [&](File &f)
-    {
-        names_for_conversion.push_back(path(f.name));
-    };
-    unpack(dummy_file, test_archive_decoder);
+        { names_for_conversion.push_back(path(f.name)); };
+    unpack(test_archive_file, test_archive_decoder);
 
     REQUIRE(names_for_recognition.size() == 4);
-    REQUIRE(names_for_recognition[0] == path("deeply/nested/test.archive"));
-    REQUIRE(names_for_recognition[1] == path("further/nested/test.image"));
-    REQUIRE(names_for_recognition[2] == path("further/nested/test.png"));
+    REQUIRE(names_for_recognition[0] == path("test.archive"));
+    REQUIRE(names_for_recognition[1] == path("nested/test.image"));
+    REQUIRE(names_for_recognition[2] == path("nested/test.png"));
     REQUIRE(names_for_recognition[3]
-        == path("deeply/nested/test.archive/further/nested/test.png"));
-
+        == path("test.archive/nested/test.png"));
     REQUIRE(names_for_conversion.size() == 1);
-    REQUIRE(names_for_conversion[0] == path("further/nested/test.image"));
+    REQUIRE(names_for_conversion[0] == path("nested/test.image"));
 }
