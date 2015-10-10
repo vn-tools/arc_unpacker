fc91fdaf9a53771345d4d457e9ccce9cee647cdd
rr- <mkurczew@gmail.com>
2015-10-10 10:35:41 +0200

    fmt: reduce false positives from EAGLS scripts

--- src/fmt/eagls/pak_script_file_decoder.cc
+++ src/fmt/eagls/pak_script_file_decoder.cc
@@ -11,12 +11,21 @@ bool PakScriptFileDecoder::is_recognized_impl(File &file) const
 {
     if (!file.has_extension("dat") || file.io.size() < 3600)
         return false;
-    // header should consist mostly of zeros
-    auto data = file.io.read(3600);
-    size_t zeros = 0;
-    for (auto i : util::range(data.size()))
-        zeros += !data[i];
-    return zeros > data.size() * 0.8;
+    for (auto i : util::range(100))
+    {
+        auto name = file.io.read(32).str();
+        file.io.skip(4);
+
+        // after first zero, the "name" (whatever it is) should contain only
+        // more zeros
+        auto zero_index = name.find_first_of('\x00');
+        if (zero_index == std::string::npos)
+            return false;
+        for (auto i : util::range(zero_index, name.size()))
+            if (name[i] != '\x00')
+                return false;
+    }
+    return true;
 }
 
 std::unique_ptr<File> PakScriptFileDecoder::decode_impl(File &file) const
@@ -24,17 +33,15 @@ std::unique_ptr<File> PakScriptFileDecoder::decode_impl(File &file) const
     // According to Crass the offset, key and even the presence of LCG
     // vary for other games.
 
-    file.io.skip(3600);
+    file.io.seek(3600);
     auto data = file.io.read(file.io.size() - file.io.tell() - 1);
     s8 seed = file.io.read_u8();
     util::crypt::Lcg lcg(util::crypt::LcgKind::MicrosoftVisualC, seed);
     for (auto i : util::range(0, data.size(), 2))
         data[i] ^= key[lcg.next() % key.size()];
 
-    auto output_file = std::make_unique<File>();
-    output_file->name = file.name;
+    auto output_file = std::make_unique<File>(file.name, data);
     output_file->change_extension("txt");
-    output_file->io.write(data);
     return output_file;
 }
 
