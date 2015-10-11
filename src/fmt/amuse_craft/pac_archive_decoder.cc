f4921235b45bca4a07dfa9f7fbc408f54237e59c
rr- <mkurczew@gmail.com>
2015-10-11 22:56:15 +0200

    fmt: support Amuse Craft (Softpal)'s PAC archives

---
+++ src/fmt/amuse_craft/pac_archive_decoder.cc
@@ -0,0 +1,51 @@
+#include "fmt/amuse_craft/pac_archive_decoder.h"
+#include "util/range.h"
+
+using namespace au;
+using namespace au::fmt::amuse_craft;
+
+static const bstr magic = "PAC\x20\x00\x00\x00\x00"_b;
+
+namespace
+{
+    struct ArchiveEntryImpl final : fmt::ArchiveEntry
+    {
+        size_t offset;
+        size_t size;
+    };
+}
+
+bool PacArchiveDecoder::is_recognized_impl(File &arc_file) const
+{
+    return arc_file.io.read(magic.size()) == magic;
+}
+
+std::unique_ptr<fmt::ArchiveMeta>
+    PacArchiveDecoder::read_meta_impl(File &arc_file) const
+{
+    arc_file.io.seek(magic.size());
+    const auto file_count = arc_file.io.read_u32_le();
+    auto meta = std::make_unique<ArchiveMeta>();
+    arc_file.io.seek(0x804);
+    for (auto i : util::range(file_count))
+    {
+        auto entry = std::make_unique<ArchiveEntryImpl>();
+        entry->name = arc_file.io.read_to_zero(32).str();
+        std::replace(entry->name.begin(), entry->name.end(), '_', '/');
+        entry->size = arc_file.io.read_u32_le();
+        entry->offset = arc_file.io.read_u32_le();
+        meta->entries.push_back(std::move(entry));
+    }
+    return meta;
+}
+
+std::unique_ptr<File> PacArchiveDecoder::read_file_impl(
+    File &arc_file, const ArchiveMeta &m, const ArchiveEntry &e) const
+{
+    auto entry = static_cast<const ArchiveEntryImpl*>(&e);
+    arc_file.io.seek(entry->offset);
+    auto data = arc_file.io.read(entry->size);
+    return std::make_unique<File>(entry->name, data);
+}
+
+static auto dummy = fmt::register_fmt<PacArchiveDecoder>("amuse-craft/pac");
---
+++ tests/fmt/amuse_craft/pac_archive_decoder_test.cc
@@ -0,0 +1,22 @@
+#include "fmt/amuse_craft/pac_archive_decoder.h"
+#include "test_support/catch.hh"
+#include "test_support/decoder_support.h"
+#include "test_support/file_support.h"
+
+using namespace au;
+using namespace au::fmt::amuse_craft;
+
+TEST_CASE("Amuse Craft PAC archives", "[fmt]")
+{
+    const std::vector<std::shared_ptr<File>> expected_files
+    {
+        tests::stub_file("dir/123.txt", "1234567890"_b),
+        tests::stub_file("dir/abc.xyz", "abcdefghijklmnopqrstuvwxyz"_b),
+    };
+
+    PacArchiveDecoder decoder;
+    auto input_file = tests::file_from_path(
+        "tests/fmt/amuse_craft/files/pac/test.pac");
+    auto actual_files = tests::unpack(decoder, *input_file);
+    tests::compare_files(expected_files, actual_files, true);
+}
