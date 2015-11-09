7af3a530ac79020d981138b952374ae24d542961
rr- <mkurczew@gmail.com>
2015-11-09 21:40:46 +0100

    fmt: support Kiss ARC archives

---
+++ src/fmt/kiss/arc_archive_decoder.cc
@@ -0,0 +1,67 @@
+#include "fmt/kiss/arc_archive_decoder.h"
+#include "err.h"
+#include "util/encoding.h"
+#include "util/range.h"
+
+using namespace au;
+using namespace au::fmt::kiss;
+
+namespace
+{
+    struct ArchiveEntryImpl final : fmt::ArchiveEntry
+    {
+        size_t offset;
+        size_t size;
+    };
+}
+
+bool ArcArchiveDecoder::is_recognized_impl(File &arc_file) const
+{
+    if (!arc_file.has_extension("arc"))
+        return false;
+    const auto meta = read_meta_impl(arc_file);
+    if (meta->entries.empty())
+        return false;
+    const auto last_entry
+        = dynamic_cast<ArchiveEntryImpl*>(meta->entries.back().get());
+    return last_entry->size + last_entry->offset == arc_file.io.size();
+}
+
+std::unique_ptr<fmt::ArchiveMeta>
+    ArcArchiveDecoder::read_meta_impl(File &arc_file) const
+{
+    arc_file.io.seek(0);
+    const auto file_count = arc_file.io.read_u32_le();
+    auto meta = std::make_unique<ArchiveMeta>();
+    ArchiveEntryImpl *last_entry = nullptr;
+    for (const size_t i : util::range(file_count))
+    {
+        auto entry = std::make_unique<ArchiveEntryImpl>();
+        entry->name = util::sjis_to_utf8(arc_file.io.read_to_zero()).str();
+        entry->offset = arc_file.io.read_u32_le();
+        if (arc_file.io.read_u32_le() != 0)
+            throw err::CorruptDataError("Expected '0'");
+        if (last_entry)
+            last_entry->size = entry->offset - last_entry->offset;
+        last_entry = entry.get();
+        meta->entries.push_back(std::move(entry));
+    }
+    if (last_entry)
+        last_entry->size = arc_file.io.size() - last_entry->offset;
+    return meta;
+}
+
+std::unique_ptr<File> ArcArchiveDecoder::read_file_impl(
+    File &arc_file, const ArchiveMeta &m, const ArchiveEntry &e) const
+{
+    const auto entry = static_cast<const ArchiveEntryImpl*>(&e);
+    arc_file.io.seek(entry->offset);
+    return std::make_unique<File>(entry->name, arc_file.io.read(entry->size));
+}
+
+std::vector<std::string> ArcArchiveDecoder::get_linked_formats() const
+{
+    return { "kiss/plg" };
+}
+
+static auto dummy = fmt::register_fmt<ArcArchiveDecoder>("kiss/arc");
---
+++ tests/fmt/kiss/arc_archive_decoder_test.cc
@@ -0,0 +1,26 @@
+#include "fmt/kiss/arc_archive_decoder.h"
+#include "test_support/catch.hh"
+#include "test_support/decoder_support.h"
+#include "test_support/file_support.h"
+
+using namespace au;
+using namespace au::fmt::kiss;
+
+static void do_test(const std::string &input_path)
+{
+    std::vector<std::shared_ptr<File>> expected_files
+    {
+        tests::stub_file("123.txt", "1234567890"_b),
+        tests::stub_file("abc.xyz", "abcdefghijklmnopqrstuvwxyz"_b),
+    };
+
+    ArcArchiveDecoder decoder;
+    auto input_file = tests::file_from_path(input_path);
+    auto actual_files = tests::unpack(decoder, *input_file);
+    tests::compare_files(expected_files, actual_files, true);
+}
+
+TEST_CASE("Kiss ARC archives", "[fmt]")
+{
+    do_test("tests/fmt/kiss/files/arc/test.arc");
+}
