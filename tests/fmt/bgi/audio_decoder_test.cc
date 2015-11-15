b7aa560633660ee8509cda8be6a742bb51af53d4
rr- <rr-@sakuya.pl>
2015-11-15 11:10:50 +0100

    tests: test BGI audio decoder

---
+++ tests/fmt/bgi/audio_decoder_test.cc
@@ -0,0 +1,24 @@
+#include "fmt/bgi/audio_decoder.h"
+#include "test_support/catch.hh"
+#include "test_support/decoder_support.h"
+#include "test_support/file_support.h"
+
+using namespace au;
+using namespace au::fmt::bgi;
+
+static void do_test(
+    const std::string &input_path, const std::string &expected_path)
+{
+    AudioDecoder decoder;
+    auto input_file = tests::file_from_path(input_path);
+    auto expected_file = tests::file_from_path(expected_path);
+    auto actual_file = tests::decode(decoder, *input_file);
+    tests::compare_files(*expected_file, *actual_file, false);
+}
+
+TEST_CASE("BGI audio", "[fmt]")
+{
+    do_test(
+        "tests/fmt/bgi/files/audio/wa_038.dat",
+        "tests/fmt/bgi/files/audio/wa_038-out.ogg");
+}
