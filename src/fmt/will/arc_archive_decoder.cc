bce175bbc00fa7c49ab151bd67e78bab833bd1a2
rr- <rr-@sakuya.pl>
2015-11-29 16:19:32 +0100

    fmt: fix Will ARC preprocessing

--- src/fmt/will/arc_archive_decoder.cc
+++ src/fmt/will/arc_archive_decoder.cc
@@ -29,17 +29,17 @@ static std::unique_ptr<fmt::ArchiveMeta> read_meta(
     io::File &input_file, const std::vector<Directory> &dirs, size_t name_size)
 {
     auto min_offset = 4 + dirs.size() * 12;
-    for (auto &dir : dirs)
+    for (const auto &dir : dirs)
         min_offset += dir.file_count * (name_size + 8);
 
     auto meta = std::make_unique<fmt::ArchiveMeta>();
-    for (auto &dir : dirs)
+    for (const auto &dir : dirs)
     {
         input_file.stream.seek(dir.offset);
-        for (auto i : util::range(dir.file_count))
+        for (const auto i : util::range(dir.file_count))
         {
             auto entry = std::make_unique<ArchiveEntryImpl>();
-            auto name = input_file.stream.read_to_zero(name_size).str();
+            const auto name = input_file.stream.read_to_zero(name_size).str();
             entry->already_unpacked = false;
             entry->path = name + "." + dir.extension;
             entry->size = input_file.stream.read_u32_le();
@@ -68,9 +68,9 @@ void ArcArchiveDecoder::preprocess(
 {
     // apply image masks to original sprites
     std::map<std::string, ArchiveEntryImpl*> mask_entries, sprite_entries;
-    for (auto &entry : meta.entries)
+    for (const auto &entry : meta.entries)
     {
-        auto fn = entry->path.stem();
+        const auto fn = entry->path.stem();
         if (entry->path.has_extension("MSK"))
             mask_entries[fn] = static_cast<ArchiveEntryImpl*>(entry.get());
         else if (entry->path.has_extension("WIP"))
@@ -86,13 +86,13 @@ void ArcArchiveDecoder::preprocess(
             auto mask_entry = mask_entries.at(it.first);
             auto sprites = wipf_archive_decoder.unpack_to_images(
                 *read_file(input_file, meta, *sprite_entry));
-            auto masks = wipf_archive_decoder.unpack_to_images(
+            const auto masks = wipf_archive_decoder.unpack_to_images(
                 *read_file(input_file, meta, *mask_entry));
-            for (auto i : util::range(sprites.size()))
+            for (const auto i : util::range(sprites.size()))
                 sprites[i]->apply_mask(*masks.at(i));
             sprite_entry->already_unpacked = true;
             mask_entry->already_unpacked = true;
-            for (auto &sprite : sprites)
+            for (const auto &sprite : sprites)
             {
                 saver.save(util::file_from_image(
                     *sprite, sprite_entry->path));
@@ -108,18 +108,18 @@ void ArcArchiveDecoder::preprocess(
 std::unique_ptr<fmt::ArchiveMeta>
     ArcArchiveDecoder::read_meta_impl(io::File &input_file) const
 {
-    auto dir_count = input_file.stream.read_u32_le();
+    const auto dir_count = input_file.stream.read_u32_le();
     if (dir_count > 100)
         throw err::BadDataSizeError();
     std::vector<Directory> dirs(dir_count);
-    for (auto i : util::range(dirs.size()))
+    for (const auto i : util::range(dirs.size()))
     {
         dirs[i].extension = input_file.stream.read_to_zero(4).str();
         dirs[i].file_count = input_file.stream.read_u32_le();
         dirs[i].offset = input_file.stream.read_u32_le();
     }
 
-    for (auto name_size : {9, 13})
+    for (const auto name_size : {9, 13})
     {
         try
         {
@@ -137,22 +137,18 @@ std::unique_ptr<fmt::ArchiveMeta>
 std::unique_ptr<io::File> ArcArchiveDecoder::read_file_impl(
     io::File &input_file, const ArchiveMeta &m, const ArchiveEntry &e) const
 {
-    auto entry = static_cast<const ArchiveEntryImpl*>(&e);
-    input_file.stream.seek(entry->offset);
-    auto data = input_file.stream.read(entry->size);
+    const auto entry = static_cast<const ArchiveEntryImpl*>(&e);
+    if (entry->already_unpacked)
+        return nullptr;
 
-    auto output_file = std::make_unique<io::File>();
-    output_file->path = entry->path;
-
-    if (output_file->path.has_extension("wsc")
-        || output_file->path.has_extension("scr"))
+    auto data = input_file.stream.seek(entry->offset).read(entry->size);
+    if (entry->path.has_extension("wsc") || entry->path.has_extension("scr"))
     {
         for (auto &c : data)
             c = (c >> 2) | (c << 6);
     }
 
-    output_file->stream.write(data);
-    return output_file;
+    return std::make_unique<io::File>(entry->path, data);
 }
 
 std::vector<std::string> ArcArchiveDecoder::get_linked_formats() const
