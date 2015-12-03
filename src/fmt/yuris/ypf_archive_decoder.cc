f7a8e1ec1385d88cf28c4352ad1f0acd8aabbac4
rr- <rr-@sakuya.pl>
2015-12-03 22:29:32 +0100

    fmt: add YPF name size crypt auto guessing
    Inspecting 2010 release of Whirlpool's Magus Tale has proved that the
    permutation used to derive file name size has nothing to do with the
    archive version - cgsys.ypf was packed as 290, but used permutation that
    conflicted with another game packed as 290.

--- src/fmt/yuris/ypf_archive_decoder.cc
+++ src/fmt/yuris/ypf_archive_decoder.cc
@@ -1,6 +1,7 @@
 #include "fmt/yuris/ypf_archive_decoder.h"
 #include <set>
 #include "err.h"
+#include "io/memory_stream.h"
 #include "util/encoding.h"
 #include "util/pack/zlib.h"
 #include "util/range.h"
@@ -22,7 +23,7 @@ namespace
     };
 }
 
-static u8 read_file_name_size(io::Stream &input_stream, const u32 version)
+static u8 get_name_size(io::Stream &input_stream, size_t initial_pos)
 {
     const auto byte = input_stream.read_u8() ^ 0xFF;
     static const std::vector<u8> table =
@@ -32,18 +33,10 @@ static u8 read_file_name_size(io::Stream &input_stream, const u32 version)
             0x20, 0x23, 0x26, 0x29, 0x2C, 0x2F, 0x2E, 0x32,
         };
 
-    int pos = 4;
-    if (version >= 256)
-    {
-        if (version >= 300 && version < 406)
-            pos = 10;
-        else
-            pos = 0;
-    }
-    const auto it = std::find(table.begin() + pos, table.end(), byte);
+    const auto it = std::find(table.begin() + initial_pos, table.end(), byte);
     if (it == table.end())
         return byte;
-    pos = it - table.begin();
+    const auto pos = it - table.begin();
     return table[pos + ((pos & 1) ? -1 : 1)];
 }
 
@@ -53,6 +46,35 @@ static bstr unxor(const bstr &input, const u8 key)
     for (auto &c : output)
         c ^= key;
     return output;
+}
+
+static size_t guess_name_crypt_pos(
+    io::Stream &table_stream,
+    const size_t version,
+    const size_t file_count)
+{
+    for (const auto initial_pos : {4, 0, 10})
+    {
+        table_stream.seek(0);
+        try
+        {
+            for (const auto i : util::range(file_count))
+            {
+                table_stream.skip(4);
+                const auto name_size = get_name_size(table_stream, initial_pos);
+                table_stream.skip(name_size);
+                table_stream.skip(14);
+                table_stream.skip(version == 0xDE ? 12 : 4);
+            }
+            table_stream.seek(0);
+            return initial_pos;
+        }
+        catch (...)
+        {
+            continue;
+        }
+    }
+    throw err::NotSupportedError("Failed to guess the name crypt position");
 }
 
 static u8 guess_key(const std::vector<bstr> &names)
@@ -86,24 +108,29 @@ std::unique_ptr<fmt::ArchiveMeta>
     const auto file_count = input_file.stream.read_u32_le();
     const auto table_size = input_file.stream.read_u32_le();
 
-    std::vector<bstr> names;
+    io::MemoryStream table_stream(
+        input_file.stream.seek(0x20).read(table_size));
 
+    const auto name_crypt_pos
+        = guess_name_crypt_pos(table_stream, version, file_count);
+
+    std::vector<bstr> names;
     auto meta = std::make_unique<ArchiveMeta>();
-    input_file.stream.seek(0x20);
     for (const auto i : util::range(file_count))
     {
-        input_file.stream.skip(4);
+        table_stream.skip(4);
 
-        const auto name_size = read_file_name_size(input_file.stream, version);
-        const auto name = input_file.stream.read(name_size);
+        const auto name_size = get_name_size(table_stream, name_crypt_pos);
+        const auto name = table_stream.read(name_size);
         names.push_back(name);
+
         auto entry = std::make_unique<ArchiveEntryImpl>();
-        entry->type = input_file.stream.read_u8();
-        entry->compressed = input_file.stream.read_u8() == 1;
-        entry->size_orig = input_file.stream.read_u32_le();
-        entry->size_comp = input_file.stream.read_u32_le();
-        entry->offset = input_file.stream.read_u32_le();
-        input_file.stream.skip(version == 0xDE ? 12 : 4);
+        entry->type = table_stream.read_u8();
+        entry->compressed = table_stream.read_u8() == 1;
+        entry->size_orig = table_stream.read_u32_le();
+        entry->size_comp = table_stream.read_u32_le();
+        entry->offset = table_stream.read_u32_le();
+        table_stream.skip(version == 0xDE ? 12 : 4);
         meta->entries.push_back(std::move(entry));
     }
 
