3a9dddf86cbc255e13731b0894a39d0f28891f30
rr- <rr-@sakuya.pl>
2015-12-05 00:03:25 +0100

    fmt: fix NWA audio decoder

--- src/fmt/real_live/nwa_audio_decoder.cc
+++ src/fmt/real_live/nwa_audio_decoder.cc
@@ -10,16 +10,17 @@ namespace
 {
     struct NwaHeader final
     {
-        u16 channel_count;
-        u16 bits_per_sample;
-        u32 sample_rate;
-        s32 compression_level;
-        u32 block_count;
-        u32 uncompressed_size;
-        u32 compressed_size;
-        u32 sample_count;
-        u32 block_size;
-        u32 rest_size;
+        size_t channel_count;
+        size_t bits_per_sample;
+        size_t sample_rate;
+        int compression_level;
+        bool use_run_length;
+        size_t block_count;
+        size_t uncompressed_size;
+        size_t compressed_size;
+        size_t sample_count;
+        size_t block_size;
+        size_t rest_size;
     };
 
     class CustomBitReader final
@@ -36,68 +37,62 @@ namespace
     };
 }
 
-CustomBitReader::CustomBitReader(const bstr &str)
-    : str_ptr(str.get<const u8>()),
-        str_end(str.end<const u8>()),
-        shift(0),
-        offset(0)
+CustomBitReader::CustomBitReader(const bstr &str) :
+    str_ptr(str.get<const u8>()),
+    str_end(str.end<const u8>()),
+    shift(0),
+    offset(0)
 {
 }
 
 u32 CustomBitReader::get(size_t bits)
 {
-    int ret;
     if (shift > 8)
     {
         ++offset;
         shift -= 8;
     }
-    if (str_ptr + offset >= str_end)
+    if (str_ptr + offset + 2 >= str_end)
         throw err::IoError("Reading bits beyond EOF");
-    ret = *reinterpret_cast<const u16*>(&str_ptr[offset]) >> shift;
+    const auto ret = *reinterpret_cast<const u16*>(&str_ptr[offset]) >> shift;
     shift += bits;
     return ret & ((1 << bits) - 1);
-}
-
-static bool use_run_length(const NwaHeader &header)
-{
-    return header.compression_level == 5 && header.channel_count == 2;
 }
 
 static bstr decode_block(
     const NwaHeader &header,
     size_t current_block,
-    io::Stream &stream,
+    io::Stream &input_stream,
     const std::vector<u32> &offsets)
 {
-    size_t bytes_per_sample = header.bits_per_sample >> 3;
-    size_t current_block_size = current_block != header.block_count - 1
+    const auto bytes_per_sample = header.bits_per_sample >> 3;
+    const auto current_block_size = current_block != header.block_count - 1
         ? header.block_size * bytes_per_sample
         : header.rest_size * bytes_per_sample;
-    auto output_size = current_block_size / bytes_per_sample;
-    auto input_size = current_block != offsets.size() - 1
+    const auto output_size = current_block_size / bytes_per_sample;
+    const auto input_size = current_block != offsets.size() - 1
         ? offsets.at(current_block + 1) - offsets.at(current_block)
-        : stream.size() - offsets.at(current_block);
+        : input_stream.size() - offsets.at(current_block);
 
-    bstr output(output_size * 2);
-    s16 *output_ptr = output.get<s16>();
+    io::MemoryStream output_stream(output_size);
 
-    stream.seek(offsets.at(current_block));
+    input_stream.seek(offsets.at(current_block));
     s16 d[2];
-    for (auto i : util::range(header.channel_count))
+    for (const auto i : util::range(header.channel_count))
     {
         if (header.bits_per_sample == 8)
-            d[i] = stream.read_u8();
+            d[i] = input_stream.read_u8();
         else
-            d[i] = stream.read_u16_le();
+            d[i] = input_stream.read_u16_le();
     }
 
-    auto input = stream.read(
+    const auto input = input_stream.read(
         input_size - bytes_per_sample * header.channel_count);
     CustomBitReader bit_reader(input);
+
     auto current_channel = 0;
     auto run_length = 0;
-    for (auto i : util::range(output_size))
+    for (const auto i : util::range(output_size))
     {
         if (run_length)
         {
@@ -115,14 +110,14 @@ static bstr decode_block(
                 else
                 {
                     int bits = 8, shift = 9;
-                    if (header.compression_level >= 3)
+                    if (header.compression_level < 3)
                     {
                         bits -= header.compression_level;
                         shift += header.compression_level;
                     }
-                    const int mask1 = (1 << (bits - 1));
-                    const int mask2 = (1 << (bits - 1)) - 1;
-                    int b = bit_reader.get(bits);
+                    const auto mask1 = (1 << (bits - 1));
+                    const auto mask2 = (1 << (bits - 1)) - 1;
+                    const auto b = bit_reader.get(bits);
                     if (b & mask1)
                         d[current_channel] -= (b & mask2) << shift;
                     else
@@ -142,15 +137,15 @@ static bstr decode_block(
                     bits = 5 - header.compression_level;
                     shift = 2 + type + header.compression_level;
                 }
-                const int mask1 = (1 << (bits - 1));
-                const int mask2 = (1 << (bits - 1)) - 1;
-                int b = bit_reader.get(bits);
+                const auto mask1 = (1 << (bits - 1));
+                const auto mask2 = (1 << (bits - 1)) - 1;
+                const auto b = bit_reader.get(bits);
                 if (b & mask1)
                     d[current_channel] -= (b & mask2) << shift;
                 else
                     d[current_channel] += (b & mask2) << shift;
             }
-            else if (use_run_length(header))
+            else if (header.use_run_length)
             {
                 run_length = bit_reader.get(1);
                 if (run_length == 1)
@@ -162,18 +157,20 @@ static bstr decode_block(
             }
         }
 
-        output_ptr[i] = header.bits_per_sample == 8
-            ? d[current_channel] * 0x100
-            : d[current_channel];
+        if (header.bits_per_sample == 8)
+            output_stream.write_u8(d[current_channel]);
+        else
+            output_stream.write_u16_le(d[current_channel]);
 
         if (header.channel_count == 2)
             current_channel ^= 1;
     }
 
-    return output;
+    return output_stream.seek(0).read_to_eof();
 }
 
-static bstr nwa_read_compressed(io::Stream &stream, const NwaHeader &header)
+static bstr read_compressed_samples(
+    io::Stream &input_stream, const NwaHeader &header)
 {
     if (header.compression_level < 0 || header.compression_level > 5)
         throw err::NotSupportedError("Unsupported compression level");
@@ -202,20 +199,21 @@ static bstr nwa_read_compressed(io::Stream &stream, const NwaHeader &header)
         throw err::CorruptDataError("Bad sample count");
     }
 
-    stream.skip(4);
+    input_stream.skip(4);
     std::vector<u32> offsets;
-    for (auto i : util::range(header.block_count))
-        offsets.push_back(stream.read_u32_le());
+    for (const auto i : util::range(header.block_count))
+        offsets.push_back(input_stream.read_u32_le());
 
     bstr output;
-    for (auto i : util::range(header.block_count))
-        output += decode_block(header, i, stream, offsets);
+    for (const auto i : util::range(header.block_count))
+        output += decode_block(header, i, input_stream, offsets);
     return output;
 }
 
-static bstr nwa_read_uncompressed(io::Stream &stream, const NwaHeader &header)
+static bstr read_uncompressed_samples(
+    io::Stream &input_stream, const NwaHeader &header)
 {
-    return stream.read(header.uncompressed_size);
+    return input_stream.read(header.uncompressed_size);
 }
 
 bool NwaAudioDecoder::is_recognized_impl(io::File &input_file) const
@@ -226,24 +224,24 @@ bool NwaAudioDecoder::is_recognized_impl(io::File &input_file) const
 res::Audio NwaAudioDecoder::decode_impl(io::File &input_file) const
 {
     // buffer the file in memory for performance
-    io::MemoryStream stream(input_file.stream.read_to_eof());
+    io::MemoryStream input_stream(input_file.stream.seek(0).read_to_eof());
 
     NwaHeader header;
-    header.channel_count = stream.read_u16_le();
-    header.bits_per_sample = stream.read_u16_le();
-    header.sample_rate = stream.read_u32_le();
-    header.compression_level = static_cast<s32>(stream.read_u32_le());
-    stream.skip(4);
-    header.block_count = stream.read_u32_le();
-    header.uncompressed_size = stream.read_u32_le();
-    header.compressed_size = stream.read_u32_le();
-    header.sample_count = stream.read_u32_le();
-    header.block_size = stream.read_u32_le();
-    header.rest_size = stream.read_u32_le();
+    header.channel_count = input_stream.read_u16_le();
+    header.bits_per_sample = input_stream.read_u16_le();
+    header.sample_rate = input_stream.read_u32_le();
+    header.compression_level = static_cast<s32>(input_stream.read_u32_le());
+    header.use_run_length = input_stream.read_u32_le();
+    header.block_count = input_stream.read_u32_le();
+    header.uncompressed_size = input_stream.read_u32_le();
+    header.compressed_size = input_stream.read_u32_le();
+    header.sample_count = input_stream.read_u32_le();
+    header.block_size = input_stream.read_u32_le();
+    header.rest_size = input_stream.read_u32_le();
 
-    bstr samples = header.compression_level == -1
-        ? nwa_read_uncompressed(stream, header)
-        : nwa_read_compressed(stream, header);
+    const auto samples = header.compression_level == -1
+        ? read_uncompressed_samples(input_stream, header)
+        : read_compressed_samples(input_stream, header);
 
     res::Audio audio;
     audio.channel_count = header.channel_count;
