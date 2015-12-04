03b929c47b1a3beae368226c88b9b00930d2fde3
rr- <rr-@sakuya.pl>
2015-12-04 21:56:28 +0100

    tests: improve File tests

--- tests/io/file_test.cc
+++ tests/io/file_test.cc
@@ -2,17 +2,50 @@
 #include "test_support/catch.hh"
 
 using namespace au;
-using namespace au::io;
 
-TEST_CASE("Empty file creation", "[core][file]")
+static void test_guessing_extension(
+    const bstr &magic, const std::string &expected_extension)
 {
-    File file;
-    REQUIRE(file.path == "");
+    io::File file("test", magic);
+    REQUIRE(!file.path.has_extension(expected_extension));
+    file.guess_extension();
+    INFO("Extension guess test failed for ." << expected_extension);
+    REQUIRE(file.path.has_extension(expected_extension));
 }
 
-TEST_CASE("Setting File's name", "[core][file]")
+TEST_CASE("File", "[io]")
 {
-    File file;
-    file.path = "abc";
-    REQUIRE(file.path == "abc");
+    SECTION("Virtual files")
+    {
+        SECTION("Via properties")
+        {
+            io::File file;
+            REQUIRE(file.path == "");
+            REQUIRE(file.stream.size() == 0);
+        }
+        SECTION("Via constructor")
+        {
+            io::File file("test.path", "test.content"_b);
+            REQUIRE(file.path == "test.path");
+            REQUIRE(file.stream.read_to_eof() == "test.content"_b);
+        }
+    }
+
+    SECTION("Physical files")
+    {
+        io::File file("tests/io/file_test.cc", io::FileMode::Read);
+        REQUIRE(file.path == "tests/io/file_test.cc");
+        REQUIRE(file.stream.read_to_eof().find("TEST_CASE"_b) != bstr::npos);
+    }
+
+    SECTION("Guessing extension")
+    {
+        test_guessing_extension("abmp"_b, "b");
+        test_guessing_extension("IMOAVI"_b, "imoavi");
+        test_guessing_extension("\x89PNG"_b, "png");
+        test_guessing_extension("BM"_b, "bmp");
+        test_guessing_extension("RIFF"_b, "wav");
+        test_guessing_extension("OggS"_b, "ogg");
+        test_guessing_extension("\xFF\xD8\xFF"_b, "jpeg");
+    }
 }
