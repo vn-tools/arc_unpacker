fe6af8789261add79e2046084924901d8cc3d092
rr- <rr-@sakuya.pl>
2015-12-05 16:43:29 +0100

    fmt: support AVGEngine's GXP archives

---
+++ tests/fmt/avg/gxp_archive_decoder_test.cc
@@ -0,0 +1,27 @@
+#include "fmt/avg/gxp_archive_decoder.h"
+#include "test_support/decoder_support.h"
+#include "test_support/catch.hh"
+#include "test_support/file_support.h"
+
+using namespace au;
+using namespace au::fmt::avg;
+
+static const std::string dir = "tests/fmt/avg/files/gxp/";
+
+static void do_test(const std::string &input_path)
+{
+    const std::vector<std::shared_ptr<io::File>> expected_files
+    {
+        tests::stub_file("123.txt", "1234567890"_b),
+        tests::stub_file("abc.xyz", "abcdefghijklmnopqrstuvwxyz"_b),
+    };
+    const GxpArchiveDecoder decoder;
+    const auto input_file = tests::file_from_path(dir + input_path);
+    const auto actual_files = tests::unpack(decoder, *input_file);
+    tests::compare_files(expected_files, actual_files, true);
+}
+
+TEST_CASE("AVG GXP archives", "[fmt]")
+{
+    do_test("test.gxp");
+}
