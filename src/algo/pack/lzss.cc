26e5a078bdafb96162aef9ce73eb563c90925c36
rr- <rr-@sakuya.pl>
2015-12-12 00:31:55 +0100

    algo: modernize LZSS code

--- src/algo/pack/lzss.cc
+++ src/algo/pack/lzss.cc
@@ -1,5 +1,6 @@
 #include "algo/pack/lzss.h"
 #include "algo/range.h"
+#include "ptr.h"
 #include "util/cyclic_buffer.h"
 
 using namespace au;
@@ -18,80 +19,70 @@ bstr algo::pack::lzss_decompress_bitwise(
     const size_t output_size,
     const LzssSettings &settings)
 {
-    bstr output;
-    output.reserve(output_size);
-    const size_t dictionary_size = 1 << settings.position_bits;
-    size_t dictionary_pos = settings.initial_dictionary_pos;
-    auto dictionary = std::make_unique<u8[]>(dictionary_size);
-
-    u8 *dictionary_ptr = dictionary.get();
-
-    while (output.size() < output_size)
+    bstr output(output_size);
+    const auto dict_size = 1 << settings.position_bits;
+    auto dict_pos = settings.initial_dictionary_pos;
+    auto dict = std::make_unique<u8[]>(dict_size);
+    auto output_ptr = make_ptr(output);
+    auto dict_ptr = dict.get();
+    while (output_ptr < output.end())
     {
         if (bit_reader.get(1) > 0)
         {
-            u8 byte = bit_reader.get(8);
-            output += byte;
-            dictionary_ptr[dictionary_pos] = byte;
-            dictionary_pos++;
-            dictionary_pos %= dictionary_size;
+            *output_ptr++ = bit_reader.get(8);
+            dict_ptr[dict_pos] = output_ptr[-1];
+            dict_pos++;
+            dict_pos %= dict_size;
         }
         else
         {
-            unsigned int pos = bit_reader.get(settings.position_bits);
-            unsigned int size = bit_reader.get(settings.size_bits);
-            size += settings.min_match_size;
-            for (const auto i : algo::range(size))
+            auto look_behind_pos = bit_reader.get(settings.position_bits);
+            auto repetitions = bit_reader.get(settings.size_bits)
+                + settings.min_match_size;
+            while (repetitions-- && output_ptr < output.end())
             {
-                u8 byte = dictionary_ptr[pos];
-                pos += 1;
-                pos %= dictionary_size;
-                dictionary_ptr[dictionary_pos] = byte;
-                dictionary_pos++;
-                dictionary_pos %= dictionary_size;
-                output += byte;
-                if (output.size() >= output_size)
-                    break;
+                *output_ptr++ = dict_ptr[look_behind_pos];
+                look_behind_pos++;
+                look_behind_pos %= dict_size;
+                dict_ptr[dict_pos] = output_ptr[-1];
+                dict_pos++;
+                dict_pos %= dict_size;
             }
         }
     }
-
     return output;
 }
 
 bstr algo::pack::lzss_decompress_bytewise(
     const bstr &input, const size_t output_size)
 {
-    bstr output(output_size);
     util::CyclicBuffer<0x1000> dict(0xFEE);
-
-    auto input_ptr = input.get<const u8>();
-    auto output_ptr = output.get<u8>();
-    const auto input_end = input.end<const u8>();
-    const auto output_end = output.end<const u8>();
-
+    bstr output(output_size);
+    auto output_ptr = make_ptr(output);
+    auto input_ptr = make_ptr(input);
     u16 control = 0;
-    while (output_ptr < output_end && input_ptr < input_end)
+    while (output_ptr < output_ptr.end())
     {
         control >>= 1;
         if (!(control & 0x100))
+        {
+            if (input_ptr >= input_ptr.end()) break;
             control = *input_ptr++ | 0xFF00;
-
+        }
         if (control & 1)
         {
+            if (input_ptr >= input_ptr.end()) break;
             *output_ptr++ = *input_ptr++;
             dict << output_ptr[-1];
         }
         else
         {
-            u8 tmp1 = *input_ptr++;
-            if (input_ptr >= input_end)
-                break;
-            u8 tmp2 = *input_ptr++;
-
-            u16 look_behind_pos = ((tmp2 & 0xF0) << 4) | tmp1;
-            u16 repetitions = (tmp2 & 0xF) + 3;
-            while (repetitions-- && output_ptr < output_end)
+            if (input_ptr + 2 > input_ptr.end()) break;
+            const auto lo = *input_ptr++;
+            const auto hi = *input_ptr++;
+            auto look_behind_pos = lo | ((hi & 0xF0) << 4);
+            auto repetitions = (hi & 0xF) + 3;
+            while (repetitions-- && output_ptr < output_ptr.end())
             {
                 *output_ptr++ = dict[look_behind_pos++];
                 dict << output_ptr[-1];
