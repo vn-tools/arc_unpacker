bb0b683dbc27737c5438a23c695aadc78f0fea22
rr- <rr-@sakuya.pl>
2015-12-11 10:30:53 +0100

    au: add ptr class
    Its purpose is to add syntactic sugar around plain pointers, and keep
    track of underlying memory chunk size. This should simplify pointer
    management in some cases, and reduce number of parameters in function
    declarations.

---
+++ src/ptr.h
@@ -0,0 +1,83 @@
+#pragma once
+
+#include "types.h"
+
+namespace au {
+
+    template<typename T> class ptr final
+    {
+    public:
+        constexpr ptr(T *data, const size_t size) :
+            start_ptr(data),
+            cur_ptr(data),
+            end_ptr(data + size)
+        {
+        }
+
+        constexpr ptr &operator--()
+        {
+            cur_ptr -= sizeof(T);
+            return *this;
+        }
+
+        constexpr ptr &operator++()
+        {
+            cur_ptr += sizeof(T);
+            return *this;
+        }
+
+        constexpr ptr operator--(int)
+        {
+            auto p = *this;
+            cur_ptr -= sizeof(T);
+            return p;
+        }
+
+        constexpr ptr operator++(int)
+        {
+            auto p = *this;
+            cur_ptr += sizeof(T);
+            return p;
+        }
+
+        constexpr size_t size() const { return end_ptr - start_ptr; }
+        constexpr void operator +=(const size_t n) const { cur_ptr += n; }
+        constexpr void operator -=(const size_t n) const { cur_ptr -= n; }
+        constexpr T *operator +(const size_t n) const { return cur_ptr + n; }
+        constexpr T *operator -(const size_t n) const { return cur_ptr - n; }
+        constexpr T &operator *() const { return *cur_ptr; }
+        constexpr T &operator[](const size_t n) const { return cur_ptr[n]; }
+        constexpr T *start() const { return start_ptr; }
+        constexpr T *end() const { return end_ptr; }
+
+        void operator +=(const size_t n) { cur_ptr += n; }
+        void operator -=(const size_t n) { cur_ptr -= n; }
+        T *operator +(const size_t n) { return cur_ptr + n; }
+        T *operator -(const size_t n) { return cur_ptr - n; }
+        T &operator *() { return *cur_ptr; }
+        T &operator[](const size_t n) { return cur_ptr[n]; }
+        T *start() { return start_ptr; }
+        T *end() { return end_ptr; }
+
+    private:
+        const T *start_ptr;
+        T *cur_ptr;
+        const T *end_ptr;
+    };
+
+    template<typename T> inline ptr<T> make_ptr(T *data, const size_t size)
+    {
+        return ptr<T>(data, size);
+    }
+
+    inline ptr<u8> make_ptr(bstr &data)
+    {
+        return ptr<u8>(data.get<u8>(), data.size());
+    }
+
+    inline const ptr<const u8> make_ptr(const bstr &data)
+    {
+        return ptr<const u8>(data.get<const u8>(), data.size());
+    }
+
+}
---
+++ tests/ptr_test.cc
@@ -0,0 +1,69 @@
+#include "ptr.h"
+#include "test_support/catch.hh"
+#include "types.h"
+
+using namespace au;
+
+TEST_CASE("ptr", "[core]")
+{
+    SECTION("Constructor")
+    {
+        std::vector<u32> data = {1, 2, 3};
+        auto p = ptr<u32>(&data[0], data.size());
+        REQUIRE(p.size() == 3);
+    }
+
+    SECTION("make_ptr")
+    {
+        const bstr data = "herp"_b;
+        const auto p = make_ptr(data);
+        REQUIRE(p.size() == 4);
+
+        const auto p2 = make_ptr(data.get<const u8>(), 3);
+        REQUIRE(p2.size() == 3);
+    }
+
+    SECTION("Accessing the elements")
+    {
+        const bstr data = "herp"_b;
+        auto p = make_ptr(data);
+        REQUIRE(p[0] == 'h');
+        REQUIRE(p[1] == 'e');
+        REQUIRE(*p.start() == 'h');
+        REQUIRE(*p == 'h');
+        REQUIRE(p.end()[-1] == 'p');
+    }
+
+    SECTION("Navigating")
+    {
+        const bstr data = "herp"_b;
+        auto p = make_ptr(data);
+        p++;
+        REQUIRE(p[0] == 'e');
+        p--;
+        REQUIRE(p[0] == 'h');
+        p += 2;
+        REQUIRE(p[0] == 'r');
+        p -= 2;
+        REQUIRE(p[0] == 'h');
+    }
+
+    SECTION("Modifying and navigating at once")
+    {
+        bstr data = "herp"_b;
+        auto p = make_ptr(data);
+        {
+            *p++ = 'H';
+            *p++ = 'E';
+        }
+        REQUIRE(data == "HErp"_b);
+    }
+
+    SECTION("No problems with strict aliasing")
+    {
+        bstr data = "herp"_b;
+        auto p = make_ptr(data);
+        *reinterpret_cast<int*>(&p[0]) = 0x12345678;
+        REQUIRE(data == "\x78\x56\x34\x12"_b);
+    }
+}
