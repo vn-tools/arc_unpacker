9b1c4b8206e0ea773347520940ea12eeed220cd6
rr- <rr-@sakuya.pl>
2015-12-25 00:14:55 +0100

    au: add TaskScheduler

---
+++ src/task_scheduler.cc
@@ -0,0 +1,86 @@
+#include "task_scheduler.h"
+#include <chrono>
+#include <deque>
+#include <thread>
+#include <vector>
+#include "algo/range.h"
+
+using namespace au;
+
+struct TaskScheduler::Priv final
+{
+    std::deque<std::unique_ptr<ITask>> tasks;
+    std::vector<std::unique_ptr<std::thread>> threads;
+};
+
+TaskScheduler::TaskScheduler() : p(new Priv())
+{
+}
+
+TaskScheduler::~TaskScheduler()
+{
+}
+
+void TaskScheduler::push_front(std::unique_ptr<ITask> task)
+{
+    std::unique_lock<std::mutex> lock(mutex);
+    p->tasks.push_front(std::move(task));
+}
+
+void TaskScheduler::push_back(std::unique_ptr<ITask> task)
+{
+    std::unique_lock<std::mutex> lock(mutex);
+    p->tasks.push_back(std::move(task));
+}
+
+bool TaskScheduler::run(size_t number_of_threads)
+{
+    if (!number_of_threads)
+        number_of_threads = std::thread::hardware_concurrency();
+    if (!number_of_threads)
+        number_of_threads = 1;
+
+    bool success = true;
+    bool still_running = true;
+
+    for (const auto i : algo::range(number_of_threads))
+    {
+        p->threads.push_back(std::make_unique<std::thread>([&]()
+        {
+            while (true)
+            {
+                std::unique_ptr<ITask> task;
+
+                {
+                    std::unique_lock<std::mutex> lock(mutex);
+                    if (p->tasks.empty())
+                    {
+                        if (still_running && number_of_threads > 1)
+                        {
+                            lock.unlock();
+                            std::this_thread::sleep_for(
+                                std::chrono::milliseconds(10));
+                            continue;
+                        }
+                        break;
+                    }
+                    task = std::move(p->tasks.front());
+                    p->tasks.pop_front();
+                }
+
+                const auto local_success = task->work();
+
+                {
+                    std::unique_lock<std::mutex> lock(mutex);
+                    success &= local_success;
+                    still_running = !p->tasks.empty();
+                }
+            }
+        }));
+    }
+
+    for (auto &t : p->threads)
+        t->join();
+
+    return success;
+}
---
+++ src/task_scheduler.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <memory>
+#include <mutex>
+
+namespace au {
+
+    class ITask
+    {
+    public:
+        virtual ~ITask() { }
+        virtual bool work() const = 0;
+    };
+
+    class TaskScheduler final
+    {
+    public:
+        TaskScheduler();
+        ~TaskScheduler();
+        bool run(const size_t number_of_threads = 0);
+        void push_front(std::unique_ptr<ITask> task);
+        void push_back(std::unique_ptr<ITask> task);
+        void join();
+        std::mutex mutex;
+    private:
+        struct Priv;
+        std::unique_ptr<Priv> p;
+    };
+
+}
