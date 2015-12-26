14ff03ac1f353d7f831f883743378fa461ca01ee
rr- <rr-@sakuya.pl>
2015-12-26 20:13:32 +0100

    io: add path::is_absolute()

--- src/io/path.cc
+++ src/io/path.cc
@@ -95,6 +95,11 @@ void path::operator /=(const path &other)
     p = (*this / other).p;
 }
 
+bool path::is_absolute() const
+{
+    return boost::filesystem::path(p).is_absolute();
+}
+
 bool path::is_root() const
 {
     return boost::filesystem::path(p) == boost::filesystem::path(p).root_path();
