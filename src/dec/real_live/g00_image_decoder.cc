d990f08e1500648ad29ccbe1ed079852aac36e56
rr- <rr-@sakuya.pl>
2016-01-10 22:46:31 +0100

    dec/real-live/g00: use ptr

--- src/dec/real_live/g00_image_decoder.cc
+++ src/dec/real_live/g00_image_decoder.cc
@@ -1,7 +1,7 @@
 #include "dec/real_live/g00_image_decoder.h"
 #include "algo/range.h"
-#include "err.h"
 #include "io/memory_stream.h"
+#include "ptr.h"
 
 using namespace au;
 using namespace au::dec::real_live;
@@ -18,17 +18,18 @@ namespace
 }
 
 static bstr decompress(
-    const bstr &input, size_t output_size, size_t byte_count, size_t size_delta)
+    const bstr &input,
+    const size_t output_size,
+    const size_t byte_count,
+    const size_t size_delta)
 {
     bstr output(output_size);
 
-    u8 *output_ptr = output.get<u8>();
-    const u8 *output_end = output.end<const u8>();
-    const u8 *input_ptr = input.get<const u8>();
-    const u8 *input_end = input.end<const u8>();
+    auto output_ptr = make_ptr(output);
+    auto input_ptr = make_ptr(input);
 
     u16 control = 1;
-    while (output_ptr < output_end && input_ptr < input_end)
+    while (output_ptr < output_ptr.end() && input_ptr < input_ptr.end())
     {
         control >>= 1;
         if (!(control & 0x100))
@@ -36,30 +37,25 @@ static bstr decompress(
 
         if (control & 1)
         {
-            auto size = byte_count;
-            while (size-- && output_ptr < output_end && input_ptr < input_end)
-                *output_ptr++ = *input_ptr++;
+            output_ptr.append_from(input_ptr, byte_count);
         }
         else
         {
-            u16 tmp = *input_ptr++;
-            if (input_ptr >= input_end)
-                break;
+            if (input_ptr.left()
+                < 2) break;
+            size_t tmp = *input_ptr++;
             tmp |= *input_ptr++ << 8;
 
-            int look_behind = (tmp >> 4) * byte_count;
-            size_t size = ((tmp & 0x0F) + size_delta) * byte_count;
-            u8 *source_ptr = &output_ptr[-look_behind];
-            if (source_ptr < output.get<const u8>())
-                break;
-            while (size-- && output_ptr < output_end && source_ptr < output_end)
-                *output_ptr++ = *source_ptr++;
+            const auto look_behind = (tmp >> 4) * byte_count;
+            const auto size = ((tmp & 0x0F) + size_delta) * byte_count;
+            output_ptr.append_from(-look_behind, size);
         }
     }
     return output;
 }
 
-static res::Image decode_v0(io::File &input_file, size_t width, size_t height)
+static res::Image decode_v0(
+    io::File &input_file, const size_t width, const size_t height)
 {
     const auto size_comp = input_file.stream.read_le<u32>() - 8;
     const auto size_orig = input_file.stream.read_le<u32>();
@@ -68,7 +64,8 @@ static res::Image decode_v0(io::File &input_file, size_t width, size_t height)
     return res::Image(width, height, data, res::PixelFormat::BGR888);
 }
 
-static res::Image decode_v1(io::File &input_file, size_t width, size_t height)
+static res::Image decode_v1(
+    io::File &input_file, const size_t width, const size_t height)
 {
     const auto size_comp = input_file.stream.read_le<u32>() - 8;
     const auto size_orig = input_file.stream.read_le<u32>();
@@ -81,7 +78,8 @@ static res::Image decode_v1(io::File &input_file, size_t width, size_t height)
     return res::Image(width, height, pix_data, palette);
 }
 
-static res::Image decode_v2(io::File &input_file, size_t width, size_t height)
+static res::Image decode_v2(
+    io::File &input_file, const size_t width, const size_t height)
 {
     std::vector<std::unique_ptr<Region>> regions;
     const auto region_count = input_file.stream.read_le<u32>();
@@ -100,16 +98,17 @@ static res::Image decode_v2(io::File &input_file, size_t width, size_t height)
     const auto size_comp = input_file.stream.read_le<u32>() - 8;
     const auto size_orig = input_file.stream.read_le<u32>();
 
-    io::MemoryStream input(
+    io::MemoryStream input_stream(
         decompress(input_file.stream.read(size_comp), size_orig, 1, 2));
-    if (input.read_le<u32>() != regions.size())
+    input_stream.seek(0);
+    if (input_stream.read_le<u32>() != regions.size())
         throw err::CorruptDataError("Region count mismatch");
 
     res::Image image(width, height);
     for (const auto &region : regions)
     {
-        region->block_offset = input.read_le<u32>();
-        region->block_size = input.read_le<u32>();
+        region->block_offset = input_stream.read_le<u32>();
+        region->block_size = input_stream.read_le<u32>();
     }
 
     for (const auto &region : regions)
@@ -117,26 +116,26 @@ static res::Image decode_v2(io::File &input_file, size_t width, size_t height)
         if (region->block_size <= 0)
             continue;
 
-        input.seek(region->block_offset);
-        u16 block_type = input.read_le<u16>();
-        u16 part_count = input.read_le<u16>();
+        input_stream.seek(region->block_offset);
+        u16 block_type = input_stream.read_le<u16>();
+        u16 part_count = input_stream.read_le<u16>();
         if (block_type != 1)
             throw err::NotSupportedError("Unexpected block type");
 
-        input.skip(0x70);
+        input_stream.skip(0x70);
         for (const auto j : algo::range(part_count))
         {
-            u16 part_x = input.read_le<u16>();
-            u16 part_y = input.read_le<u16>();
-            input.skip(2);
-            u16 part_width = input.read_le<u16>();
-            u16 part_height = input.read_le<u16>();
-            input.skip(0x52);
+            const auto part_x = input_stream.read_le<u16>();
+            const auto part_y = input_stream.read_le<u16>();
+            input_stream.skip(2);
+            const auto part_width = input_stream.read_le<u16>();
+            const auto part_height = input_stream.read_le<u16>();
+            input_stream.skip(0x52);
 
             res::Image part(
                 part_width,
                 part_height,
-                input,
+                input_stream,
                 res::PixelFormat::BGRA8888);
 
             const size_t target_x = region->x1 + part_x;
@@ -165,21 +164,18 @@ bool G00ImageDecoder::is_recognized_impl(io::File &input_file) const
 res::Image G00ImageDecoder::decode_impl(
     const Logger &logger, io::File &input_file) const
 {
-    u8 version = input_file.stream.read<u8>();
-    u16 width = input_file.stream.read_le<u16>();
-    u16 height = input_file.stream.read_le<u16>();
+    const auto version = input_file.stream.read<u8>();
+    const auto width = input_file.stream.read_le<u16>();
+    const auto height = input_file.stream.read_le<u16>();
 
-    switch (version)
-    {
-        case 0:
-            return decode_v0(input_file, width, height);
+    if (version == 0)
+        return decode_v0(input_file, width, height);
 
-        case 1:
-            return decode_v1(input_file, width, height);
+    if (version == 1)
+        return decode_v1(input_file, width, height);
 
-        case 2:
-            return decode_v2(input_file, width, height);
-    }
+    if (version == 2)
+        return decode_v2(input_file, width, height);
 
     throw err::UnsupportedVersionError(version);
 }
