a83766f3afdcb6f38998bd7dd212d501dae65f8d
rr- <rr-@sakuya.pl>
2016-01-15 23:00:19 +0100

    tests/dec/entis/noa: test ErisaN compression

--- tests/dec/entis/noa_archive_decoder_test.cc
+++ tests/dec/entis/noa_archive_decoder_test.cc
@@ -8,13 +8,10 @@ using namespace au::dec::entis;
 
 static const std::string dir = "tests/dec/entis/files/noa/";
 
-static void do_test(const std::string &input_path)
+static void do_test(
+    const std::string &input_path,
+    const std::vector<std::shared_ptr<io::File>> expected_files)
 {
-    const std::vector<std::shared_ptr<io::File>> expected_files
-    {
-        tests::stub_file("123.txt", "1234567890"_b),
-        tests::stub_file("abc.txt", "abcdefghijklmnopqrstuvwxyz"_b),
-    };
     const auto decoder = NoaArchiveDecoder();
     const auto input_file = tests::file_from_path(dir + input_path);
     const auto actual_files = tests::unpack(decoder, *input_file);
@@ -25,6 +22,19 @@ TEST_CASE("Entis NOA archives", "[dec]")
 {
     SECTION("Unencrypted")
     {
-        do_test("unencrypted.noa");
+        do_test("unencrypted.noa",
+            {
+                tests::stub_file("123.txt", "1234567890"_b),
+                tests::stub_file("abc.txt", "abcdefghijklmnopqrstuvwxyz"_b),
+            });
+    }
+
+    SECTION("Compressed with ErisaN")
+    {
+        do_test(
+            "encrypted-erisan.noa",
+            {
+                tests::file_from_path(dir + "system.inf", "system.inf"),
+            });
     }
 }
