f8b09e1d020aafc293694e42180b93b1a8fbfe87
rr- <rr-@sakuya.pl>
2016-01-18 14:12:05 +0100

    dec/system_epsilon/packdat: initial support

---
+++ src/dec/system_epsilon/packdat_archive_decoder.cc
@@ -0,0 +1,72 @@
+#include "dec/system_epsilon/packdat_archive_decoder.h"
+#include "algo/binary.h"
+#include "algo/ptr.h"
+#include "algo/range.h"
+
+using namespace au;
+using namespace au::dec::system_epsilon;
+
+static const auto magic = "PACKDAT."_b;
+
+namespace
+{
+    struct ArchiveEntryImpl final : dec::ArchiveEntry
+    {
+        size_t offset;
+        size_t size_comp;
+        size_t size_orig;
+        u32 flags;
+    };
+}
+
+bool PackdatArchiveDecoder::is_recognized_impl(io::File &input_file) const
+{
+    return input_file.stream.read(magic.size()) == magic;
+}
+
+std::unique_ptr<dec::ArchiveMeta> PackdatArchiveDecoder::read_meta_impl(
+    const Logger &logger, io::File &input_file) const
+{
+    input_file.stream.seek(magic.size());
+    const auto file_count = input_file.stream.read_le<u32>();
+    input_file.stream.skip(4);
+    auto meta = std::make_unique<ArchiveMeta>();
+    for (const auto i : algo::range(file_count))
+    {
+        auto entry = std::make_unique<ArchiveEntryImpl>();
+        entry->path = input_file.stream.read_to_zero(32).str();
+        entry->offset = input_file.stream.read_le<u32>();
+        entry->flags = input_file.stream.read_le<u32>();
+        entry->size_orig = input_file.stream.read_le<u32>();
+        entry->size_comp = input_file.stream.read_le<u32>(); // or vice versa
+        meta->entries.push_back(std::move(entry));
+    }
+    return std::move(meta);
+}
+
+std::unique_ptr<io::File> PackdatArchiveDecoder::read_file_impl(
+    const Logger &logger,
+    io::File &input_file,
+    const dec::ArchiveMeta &m,
+    const dec::ArchiveEntry &e) const
+{
+    const auto entry = static_cast<const ArchiveEntryImpl*>(&e);
+    if (entry->size_orig != entry->size_comp)
+        throw err::NotSupportedError("Compressed entries are not supported");
+    auto data = input_file.stream.seek(entry->offset).read(entry->size_orig);
+    if (entry->flags & 0x10000)
+    {
+        u32 key = data.size() >> 2;
+        key = (key << ((key & 7) + 8)) ^ key;
+        auto data_ptr = algo::make_ptr(data.get<u32>(), data.size() / 4);
+        while (data_ptr.left())
+        {
+            *data_ptr ^= key;
+            key = algo::rotl<u32>(key, *data_ptr++ % 24);
+        }
+    }
+    return std::make_unique<io::File>(entry->path, data);
+}
+
+static auto _
+    = dec::register_decoder<PackdatArchiveDecoder>("system-epsilon/packdat");
---
+++ tests/dec/system_epsilon/packdat_archive_decoder_test.cc
@@ -0,0 +1,75 @@
+﻿#include "dec/system_epsilon/packdat_archive_decoder.h"
+#include "algo/binary.h"
+#include "algo/ptr.h"
+#include "test_support/catch.h"
+#include "test_support/decoder_support.h"
+#include "test_support/file_support.h"
+
+using namespace au;
+using namespace au::dec::system_epsilon;
+
+static void encrypt(bstr &data)
+{
+    u32 key = data.size() >> 2;
+    key = (key << ((key & 7) + 8)) ^ key;
+    auto data_ptr = algo::make_ptr(data.get<u32>(), data.size() / 4);
+    while (data_ptr.left())
+    {
+        const auto old_key = key;
+        key = algo::rotl<u32>(key, *data_ptr % 24);
+        *data_ptr++ ^= old_key;
+    }
+}
+
+static void do_test(
+    const std::vector<std::shared_ptr<io::File>> &expected_files,
+    const bool encrypted)
+{
+    const auto decoder = PackdatArchiveDecoder();
+    io::File input_file;
+    input_file.stream.write("PACKDAT."_b);
+    input_file.stream.write_le<u32>(expected_files.size());
+    input_file.stream.write("JUNK"_b);
+    auto offset = 16 + expected_files.size() * 48;
+    for (const auto &file : expected_files)
+    {
+        input_file.stream.write_zero_padded(file->path.str(), 32);
+        input_file.stream.write_le<u32>(offset);
+        input_file.stream.write_le<u32>(encrypted ? 0x10000 : 0);
+        input_file.stream.write_le<u32>(file->stream.size());
+        input_file.stream.write_le<u32>(file->stream.size());
+        offset += file->stream.size();
+    }
+    for (const auto &file : expected_files)
+    {
+        auto data = file->stream.seek(0).read_to_eof();
+        if (encrypted)
+            encrypt(data);
+        input_file.stream.write(data);
+    }
+    const auto actual_files = tests::unpack(decoder, input_file);
+    tests::compare_files(expected_files, actual_files, true);
+}
+
+TEST_CASE("SYSTEM-ε PACKDAT archives", "[dec]")
+{
+    SECTION("Unencrypted")
+    {
+        do_test(
+            {
+                tests::stub_file("00000.dat", "1234567890"_b),
+                tests::stub_file("00001.dat", "abcdefghijklmnopqrstuvwxyz"_b),
+            },
+            false);
+    }
+
+    SECTION("Encrypted")
+    {
+        do_test(
+            {
+                tests::stub_file("00000.dat", "1234567890"_b),
+                tests::stub_file("00001.dat", "abcdefghijklmnopqrstuvwxyz"_b),
+            },
+            true);
+    }
+}
