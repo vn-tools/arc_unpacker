7fd874a57e6ebc2344c83ef2aeadb2f0c475d6a2
rr- <rr-@sakuya.pl>
2016-01-23 13:08:11 +0100

    dec/complets/gd: initial support (GD3)

---
+++ src/dec/complets/gd_image_decoder.cc
@@ -0,0 +1,191 @@
+#include "dec/complets/gd_image_decoder.h"
+#include "algo/format.h"
+#include "algo/pack/lzss.h"
+#include "algo/ptr.h"
+#include "algo/range.h"
+#include "err.h"
+#include "io/msb_bit_stream.h"
+
+using namespace au;
+using namespace au::dec::complets;
+
+static const bstr magic = "GD3"_b;
+
+namespace
+{
+    enum class CompressionType : u8
+    {
+        Uncompressed = 0x62,
+        Lzss         = 0x6C,
+        Custom       = 0x70,
+    };
+}
+
+static bstr decompress_lzss(
+    io::BaseBitStream &bit_stream,
+    const size_t width,
+    const size_t height,
+    const size_t channels)
+{
+    std::vector<u8> dict(1 << 16, 0);
+    auto dict_ptr = algo::make_cyclic_ptr(dict.data(), dict.size()) + 1;
+
+    bstr output(width * height * channels);
+    auto output_ptr = algo::make_ptr(output);
+    while (output_ptr.left() && bit_stream.left())
+    {
+        if (bit_stream.read(1))
+        {
+            if (bit_stream.left() < 8)
+                break;
+            const auto b = bit_stream.read(8);
+            *output_ptr++ = b;
+            *dict_ptr++ = b;
+        }
+        else
+        {
+            if (bit_stream.left() < 20)
+                break;
+            auto look_behind_pos = bit_stream.read(16);
+            auto repetitions = bit_stream.read(4) + 3;
+            auto source_ptr
+                = algo::make_cyclic_ptr(dict.data(), dict.size())
+                + look_behind_pos;
+            while (repetitions-- && output_ptr.left())
+            {
+                const auto b = *source_ptr++;
+                *output_ptr++ = b;
+                *dict_ptr++ = b;
+            }
+        }
+    }
+    return output;
+}
+
+static bstr decompress_custom(
+    io::BaseBitStream &bit_stream,
+    const size_t width,
+    const size_t height,
+    const size_t channels)
+{
+    bstr output(width * height * channels, 0xFF);
+    auto ptr1 = algo::make_ptr(output);
+
+    while (true)
+    {
+        auto skip = bit_stream.read(2);
+        if (skip == 2)
+        {
+            skip = bit_stream.read(2) + 2;
+        }
+        else if (skip == 3)
+        {
+            int j = 3;
+            while (bit_stream.read(1) && bit_stream.left())
+                ++j;
+            if (j >= 0x18)
+                break;
+            skip = ((1 << j) | bit_stream.read(j)) - 2;
+        }
+
+        ptr1 += 3 * skip;
+        if (ptr1.left() < 3)
+            throw err::BadDataOffsetError();
+        ptr1[0] = bit_stream.read(8);
+        ptr1[1] = bit_stream.read(8);
+        ptr1[2] = bit_stream.read(8);
+        if (bit_stream.read(1))
+        {
+            auto ptr2 = algo::make_ptr(output) + ptr1.pos();
+            while (true)
+            {
+                const auto strategy = bit_stream.read(2);
+                if (strategy == 0)
+                {
+                    if (!bit_stream.read(1))
+                        break;
+                    ptr2 += (width + (bit_stream.read(1) ? 2 : -2)) * channels;
+                }
+                else if (strategy == 1)
+                    ptr2 += (width - 1) * channels;
+                else if (strategy == 2)
+                    ptr2 += width * channels;
+                else if (strategy == 3)
+                    ptr2 += (width + 1) * channels;
+                if (ptr2.left() < 3)
+                    throw err::BadDataOffsetError();
+                ptr2[0] = ptr1[0];
+                ptr2[1] = ptr1[1];
+                ptr2[2] = ptr1[2];
+            }
+        }
+        ptr1 += 3;
+    }
+
+    auto output_ptr = output.get<u8>();
+    int buf[3] = {0, 0, 0};
+    for (const auto y : algo::range(height))
+    for (const auto x : algo::range(width))
+    {
+        const auto diff
+            = output_ptr[0] != 0xFF
+            || output_ptr[1] != 0xFF
+            || output_ptr[2] != 0xFF;
+        for (const auto c : algo::range(channels))
+        {
+            if (diff)
+                buf[c] = *output_ptr++;
+            else
+                *output_ptr++ = buf[c];
+        }
+    }
+    return output;
+}
+
+bool GdImageDecoder::is_recognized_impl(io::File &input_file) const
+{
+    return input_file.stream.read(magic.size()) == magic;
+}
+
+res::Image GdImageDecoder::decode_impl(
+    const Logger &logger, io::File &input_file) const
+{
+    const auto fmt = res::PixelFormat::BGR888;
+    const auto channels = 3;
+    const auto width = 800;
+    const auto height = 600;
+    const auto thumb_width = width / 10;
+    const auto thumb_height = (height / 10) - 1;
+    const auto compression_type = input_file.stream
+        .seek(4 + channels * thumb_width * thumb_height)
+        .read<CompressionType>();
+    input_file.stream.skip(1);
+
+    bstr output;
+
+    if (compression_type == CompressionType::Uncompressed)
+    {
+        output = input_file.stream.read_to_eof();
+    }
+    else if (compression_type == CompressionType::Lzss)
+    {
+        io::MsbBitStream bit_stream(input_file.stream.read_to_eof());
+        output = decompress_lzss(
+            bit_stream, width, height, channels);
+    }
+    else if (compression_type == CompressionType::Custom)
+    {
+        io::MsbBitStream bit_stream(input_file.stream.read_to_eof());
+        output = decompress_custom(
+            bit_stream, width, height, channels);
+    }
+    else
+    {
+        throw err::NotSupportedError(
+            algo::format("Unknown compression type: %x", compression_type));
+    }
+
+    return res::Image(width, height, output, fmt).flip_vertically();
+}
+
+static auto _ = dec::register_decoder<GdImageDecoder>("complets/gd");
---
+++ tests/dec/complets/gd_image_decoder_test.cc
@@ -0,0 +1,206 @@
+#include "dec/complets/gd_image_decoder.h"
+#include "algo/pack/lzss.h"
+#include "algo/ptr.h"
+#include "algo/range.h"
+#include "io/memory_stream.h"
+#include "io/msb_bit_stream.h"
+#include "test_support/catch.h"
+#include "test_support/decoder_support.h"
+#include "test_support/image_support.h"
+
+using namespace au;
+using namespace au::dec::complets;
+
+static bstr compress_custom(const res::Image &input_image)
+{
+    const auto width = input_image.width();
+    std::vector<res::Pixel> pixels;
+    for (const auto y : algo::range(input_image.height()))
+    for (const auto x : algo::range(width))
+        pixels.push_back(input_image.at(x, y));
+    std::vector<bool> known(pixels.size(), false);
+    auto pixels_ptr = algo::make_ptr(pixels.data(), pixels.size());
+
+    io::MemoryStream output_stream;
+    {
+        io::MsbBitStream bit_stream(output_stream);
+        while (pixels_ptr.left())
+        {
+            size_t match_size = 0;
+            while (pixels_ptr.left() > 1 && known[pixels_ptr.pos()])
+            {
+                match_size++;
+                pixels_ptr++;
+            }
+
+            if (match_size < 2)
+                bit_stream.write(2, match_size);
+            else if (match_size < 6)
+            {
+                bit_stream.write(2, 2);
+                bit_stream.write(2, match_size - 2);
+            }
+            else
+            {
+                bit_stream.write(2, 3);
+                auto match_size_bits = 0;
+                auto match_size_copy = match_size + 2;
+                while (match_size_copy >>= 1)
+                    match_size_bits++;
+                for (const auto j : algo::range(3, match_size_bits))
+                    bit_stream.write(1, 1);
+                bit_stream.write(1, 0);
+                bit_stream.write(match_size_bits, match_size + 2);
+            }
+
+            bit_stream.write(8, pixels_ptr->b);
+            bit_stream.write(8, pixels_ptr->g);
+            bit_stream.write(8, pixels_ptr->r);
+
+            if (pixels_ptr.left() > width + 2
+                && (pixels_ptr[width - 2] == *pixels_ptr
+                || pixels_ptr[width - 1] == *pixels_ptr
+                || pixels_ptr[width] == *pixels_ptr
+                || pixels_ptr[width + 1] == *pixels_ptr
+                || pixels_ptr[width + 2] == *pixels_ptr))
+            {
+                bit_stream.write(1, 1);
+                auto lookahead_ptr
+                    = algo::make_ptr(pixels.data(), pixels.size())
+                    + pixels_ptr.pos();
+                while (lookahead_ptr.left() > width + 2)
+                {
+                    if (lookahead_ptr[width] == *pixels_ptr)
+                    {
+                        bit_stream.write(2, 2);
+                        lookahead_ptr += width;
+                    }
+                    else if (lookahead_ptr[width - 1] == *pixels_ptr)
+                    {
+                        bit_stream.write(2, 1);
+                        lookahead_ptr += width - 1;
+                    }
+                    else if (lookahead_ptr[width + 1] == *pixels_ptr)
+                    {
+                        bit_stream.write(2, 3);
+                        lookahead_ptr += width + 1;
+                    }
+                    else if (lookahead_ptr[width - 2] == *pixels_ptr)
+                    {
+                        bit_stream.write(2, 0);
+                        bit_stream.write(1, 1);
+                        bit_stream.write(1, 0);
+                        lookahead_ptr += width - 2;
+                    }
+                    else if (lookahead_ptr[width + 2] == *pixels_ptr)
+                    {
+                        bit_stream.write(2, 0);
+                        bit_stream.write(1, 1);
+                        bit_stream.write(1, 1);
+                        lookahead_ptr += width + 2;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                    known[lookahead_ptr.pos()] = true;
+                }
+                bit_stream.write(2, 0);
+                bit_stream.write(1, 0);
+            }
+            else
+            {
+                bit_stream.write(1, 0);
+            }
+
+            pixels_ptr++;
+
+            match_size = 0;
+            while (pixels_ptr.left() > match_size)
+            {
+                if (pixels_ptr[-1] != pixels_ptr[match_size])
+                    break;
+                known[pixels_ptr.pos() + match_size] = true;
+                match_size++;
+            }
+        }
+        bit_stream.write(2, 3);
+        bit_stream.write(32, 0xFFFFFFFF);
+    }
+    return output_stream.seek(0).read_to_eof();
+}
+
+TEST_CASE("Complet's GD images", "[dec]")
+{
+    const auto decoder = GdImageDecoder();
+    res::Image expected_image(800, 600);
+    expected_image.overlay(
+        tests::get_opaque_test_image(), res::Image::OverlayKind::OverwriteAll);
+    for (auto &c : expected_image)
+    {
+        c.a = 0xFF;
+        if (c.r == 0xFF && c.g == 0xFF && c.b == 0xFF)
+            c.b--;
+    }
+
+    res::Image input_image = expected_image;
+    input_image.flip_vertically();
+
+    io::File input_file;
+    input_file.stream.write("GD3?"_b);
+    for (const auto y : algo::range((input_image.height() - 1) / 10))
+    for (const auto x : algo::range(input_image.width() / 10))
+    {
+        input_file.stream.write<u8>(input_image.at(x * 10, y * 10).b);
+        input_file.stream.write<u8>(input_image.at(x * 10, y * 10).g);
+        input_file.stream.write<u8>(input_image.at(x * 10, y * 10).r);
+    }
+
+    SECTION("Uncompressed")
+    {
+        input_file.stream.write<u8>(0x62);
+        input_file.stream.write("?");
+        for (const auto y : algo::range(input_image.height()))
+        for (const auto x : algo::range(input_image.width()))
+        {
+            input_file.stream.write<u8>(input_image.at(x, y).b);
+            input_file.stream.write<u8>(input_image.at(x, y).g);
+            input_file.stream.write<u8>(input_image.at(x, y).r);
+        }
+        const auto actual_image = tests::decode(decoder, input_file);
+        tests::compare_images(expected_image, actual_image);
+    }
+
+    SECTION("LZSS compressed")
+    {
+        input_file.stream.write<u8>(0x6C);
+        input_file.stream.write("?");
+        io::MemoryStream tmp_stream;
+        for (const auto y : algo::range(input_image.height()))
+        for (const auto x : algo::range(input_image.width()))
+        {
+            tmp_stream.write<u8>(input_image.at(x, y).b);
+            tmp_stream.write<u8>(input_image.at(x, y).g);
+            tmp_stream.write<u8>(input_image.at(x, y).r);
+        }
+        algo::pack::BitwiseLzssSettings settings;
+        settings.position_bits = 16;
+        settings.size_bits = 4;
+        settings.initial_dictionary_pos = 1;
+        settings.min_match_size = 3;
+        input_file.stream.write(
+            algo::pack::lzss_compress(
+                tmp_stream.seek(0).read_to_eof(), settings));
+        const auto actual_image = tests::decode(decoder, input_file);
+        tests::compare_images(expected_image, actual_image);
+    }
+
+    SECTION("Custom compressed")
+    {
+        input_file.stream.write<u8>(0x70);
+        input_file.stream.write("?");
+        input_file.stream.write(compress_custom(input_image));
+        const auto actual_image = tests::decode(decoder, input_file);
+        tests::compare_images(expected_image, actual_image);
+    }
+}
