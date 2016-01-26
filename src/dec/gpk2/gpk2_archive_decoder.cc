f77e8d1d82eec87b15e541a87a4c1ff343cb1cd1
rr- <rr-@sakuya.pl>
2016-01-26 22:13:54 +0100

    dec/gpk2/gpk2: initial support

---
+++ src/dec/gpk2/gpk2_archive_decoder.cc
@@ -0,0 +1,55 @@
+#include "dec/gpk2/gpk2_archive_decoder.h"
+#include "algo/range.h"
+
+using namespace au;
+using namespace au::dec::gpk2;
+
+static const bstr magic = "GPK2"_b;
+
+namespace
+{
+    struct ArchiveEntryImpl final : dec::ArchiveEntry
+    {
+        size_t offset;
+        size_t size;
+    };
+}
+
+bool Gpk2ArchiveDecoder::is_recognized_impl(io::File &input_file) const
+{
+    return input_file.stream.read(magic.size()) == magic;
+}
+
+std::unique_ptr<dec::ArchiveMeta> Gpk2ArchiveDecoder::read_meta_impl(
+    const Logger &logger, io::File &input_file) const
+{
+    input_file.stream.seek(magic.size());
+    const auto table_offset = input_file.stream.read_le<u32>();
+    input_file.stream.seek(table_offset);
+    const auto file_count = input_file.stream.read_le<u32>();
+
+    auto meta = std::make_unique<ArchiveMeta>();
+    for (const auto i : algo::range(file_count))
+    {
+        auto entry = std::make_unique<ArchiveEntryImpl>();
+        entry->offset = input_file.stream.read_le<u32>();
+        entry->size = input_file.stream.read_le<u32>();
+        entry->path = input_file.stream.read_to_zero(0x80).str();
+        meta->entries.push_back(std::move(entry));
+    }
+    return meta;
+}
+
+std::unique_ptr<io::File> Gpk2ArchiveDecoder::read_file_impl(
+    const Logger &logger,
+    io::File &input_file,
+    const dec::ArchiveMeta &m,
+    const dec::ArchiveEntry &e) const
+{
+    const auto entry = static_cast<const ArchiveEntryImpl*>(&e);
+    return std::make_unique<io::File>(
+        entry->path,
+        input_file.stream.seek(entry->offset).read(entry->size));
+}
+
+static auto _ = dec::register_decoder<Gpk2ArchiveDecoder>("gpk2/gpk2");
---
+++ tests/dec/gpk2/gpk2_archive_decoder_test.cc
@@ -0,0 +1,41 @@
+#include "dec/gpk2/gpk2_archive_decoder.h"
+#include "io/memory_stream.h"
+#include "test_support/catch.h"
+#include "test_support/decoder_support.h"
+#include "test_support/file_support.h"
+
+using namespace au;
+using namespace au::dec::gpk2;
+
+static void do_test(
+    const std::vector<std::shared_ptr<io::File>> &expected_files)
+{
+    io::File input_file;
+    input_file.stream.write("GPK2"_b);
+    input_file.stream.write_le<u32>(0);
+    io::MemoryStream table_stream;
+    table_stream.write_le<u32>(expected_files.size());
+    for (const auto &file : expected_files)
+    {
+        table_stream.write_le<u32>(input_file.stream.tell());
+        table_stream.write_le<u32>(file->stream.size());
+        table_stream.write_zero_padded(file->path.str(), 0x80);
+        input_file.stream.write(file->stream.seek(0).read_to_eof());
+    }
+    const auto offset_to_table = input_file.stream.tell();
+    input_file.stream.write(table_stream.seek(0).read_to_eof());
+    input_file.stream.seek(4).write_le<u32>(offset_to_table);
+
+    const auto decoder = Gpk2ArchiveDecoder();
+    const auto actual_files = tests::unpack(decoder, input_file);
+    tests::compare_files(actual_files, expected_files, true);
+}
+
+TEST_CASE("GPK2 GPK2 archives", "[dec]")
+{
+    do_test(
+        {
+            tests::stub_file("00000.dat", "1234567890"_b),
+            tests::stub_file("00001.dat", "abcdefghijklmnopqrstuvwxyz"_b),
+        });
+}
