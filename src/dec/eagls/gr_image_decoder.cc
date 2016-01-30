e164fbe3d1e4d6683951afa1caa1465ea304a87c
rr- <rr-@sakuya.pl>
2016-01-30 16:03:47 +0100

    dec/eagls/gr: use const where possible

--- src/dec/eagls/gr_image_decoder.cc
+++ src/dec/eagls/gr_image_decoder.cc
@@ -13,16 +13,16 @@ static const u32 xor_value = 0x75BD924;
 
 static size_t guess_output_size(const bstr &data)
 {
-    io::MemoryStream tmp_stream(algo::pack::lzss_decompress(data, 30));
-    tmp_stream.skip(10);
-    auto pixels_start = tmp_stream.read_le<u32>();
-    tmp_stream.skip(4);
-    auto width = tmp_stream.read_le<u32>();
-    auto height = tmp_stream.read_le<u32>();
-    tmp_stream.skip(2);
-    auto bpp = tmp_stream.read_le<u16>();
-    auto stride = ((width + 3) / 4) * 4;
-    return pixels_start + stride * height * (bpp >> 3);
+    io::MemoryStream bmp_stream(algo::pack::lzss_decompress(data, 30));
+    bmp_stream.skip(10);
+    const auto pixels_start = bmp_stream.read_le<u32>();
+    bmp_stream.skip(4);
+    const auto width = bmp_stream.read_le<u32>();
+    const auto height = bmp_stream.read_le<u32>();
+    bmp_stream.skip(2);
+    const auto depth = bmp_stream.read_le<u16>();
+    const auto stride = ((width + 3) / 4) * 4;
+    return pixels_start + stride * height * (depth >> 3);
 }
 
 bool GrImageDecoder::is_recognized_impl(io::File &input_file) const
@@ -36,17 +36,17 @@ res::Image GrImageDecoder::decode_impl(
     // According to Crass the offset, key and LCG kind vary for other games.
 
     auto data = input_file.stream.read(input_file.stream.size() - 1);
-    auto seed = input_file.stream.read<u8>() ^ xor_value;
+    const auto seed = input_file.stream.read<u8>() ^ xor_value;
 
     algo::crypt::Lcg lcg(algo::crypt::LcgKind::ParkMillerRevised, seed);
-    for (auto i : algo::range(0, std::min<size_t>(0x174B, data.size())))
+    for (const auto i : algo::range(0, std::min<size_t>(0x174B, data.size())))
         data[i] ^= key[lcg.next() % key.size()];
 
-    auto output_size = guess_output_size(data);
+    const auto output_size = guess_output_size(data);
     data = algo::pack::lzss_decompress(data, output_size);
 
-    io::File bmp_file(input_file.path, data);
     const auto bmp_file_decoder = dec::microsoft::BmpImageDecoder();
+    io::File bmp_file(input_file.path, data);
     return bmp_file_decoder.decode(logger, bmp_file);
 }
 
