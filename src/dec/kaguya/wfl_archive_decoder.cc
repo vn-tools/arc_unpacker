71b16fe02c96ea0220430468f7ff740e86730c4a
rr- <rr-@sakuya.pl>
2016-02-01 19:35:35 +0100

    dec/kaguya/wfl: support compression

--- src/dec/kaguya/wfl_archive_decoder.cc
+++ src/dec/kaguya/wfl_archive_decoder.cc
@@ -1,6 +1,7 @@
 #include "dec/kaguya/wfl_archive_decoder.h"
 #include "algo/binary.h"
 #include "algo/locale.h"
+#include "algo/pack/lzss.h"
 #include "algo/range.h"
 #include "err.h"
 
@@ -13,10 +14,21 @@ namespace
 {
     struct ArchiveEntryImpl final : dec::ArchiveEntry
     {
+        u16 type;
         size_t offset;
         size_t size_comp;
         size_t size_orig;
     };
+}
+
+static bstr decompress(const bstr &input, const size_t size_orig)
+{
+    algo::pack::BitwiseLzssSettings settings;
+    settings.min_match_size = 2;
+    settings.position_bits = 12;
+    settings.size_bits = 4;
+    settings.initial_dictionary_pos = 1;
+    return algo::pack::lzss_decompress(input, size_orig, settings);
 }
 
 bool WflArchiveDecoder::is_recognized_impl(io::File &input_file) const
@@ -37,12 +49,12 @@ std::unique_ptr<dec::ArchiveMeta> WflArchiveDecoder::read_meta_impl(
         auto entry = std::make_unique<ArchiveEntryImpl>();
         entry->path = algo::sjis_to_utf8(algo::unxor(
             input_file.stream.read(name_size), 0xFF).str()).str(true);
-        const auto type = input_file.stream.read_le<u16>();
+        entry->type = input_file.stream.read_le<u16>();
         entry->size_comp = input_file.stream.read_le<u32>();
-        entry->offset = input_file.stream.tell();
-        entry->size_orig = type == 1
+        entry->size_orig = entry->type == 1
             ? input_file.stream.read_le<u32>()
             : entry->size_comp;
+        entry->offset = input_file.stream.tell();
         input_file.stream.skip(entry->size_comp);
         meta->entries.push_back(std::move(entry));
     }
@@ -56,11 +68,12 @@ std::unique_ptr<io::File> WflArchiveDecoder::read_file_impl(
     const dec::ArchiveEntry &e) const
 {
     const auto entry = static_cast<const ArchiveEntryImpl*>(&e);
-    auto output_file = std::make_unique<io::File>(
-        entry->path,
-        input_file.stream.seek(entry->offset).read(entry->size_comp));
-    output_file->guess_extension();
-    return output_file;
+    const auto data_comp
+        = input_file.stream.seek(entry->offset).read(entry->size_comp);
+    const auto data_orig = entry->type == 1
+        ? decompress(data_comp, entry->size_orig)
+        : data_comp;
+    return std::make_unique<io::File>(entry->path, data_orig);
 }
 
 static auto _ = dec::register_decoder<WflArchiveDecoder>("kaguya/wfl");
--- tests/dec/kaguya/wfl_archive_decoder_test.cc
+++ tests/dec/kaguya/wfl_archive_decoder_test.cc
@@ -1,12 +1,23 @@
 ﻿#include "dec/kaguya/wfl_archive_decoder.h"
 #include "algo/binary.h"
 #include "algo/locale.h"
+#include "algo/pack/lzss.h"
 #include "test_support/catch.h"
 #include "test_support/decoder_support.h"
 #include "test_support/file_support.h"
 
 using namespace au;
 using namespace au::dec::kaguya;
+
+static bstr compress(const bstr &input)
+{
+    algo::pack::BitwiseLzssSettings settings;
+    settings.min_match_size = 2;
+    settings.position_bits = 12;
+    settings.size_bits = 4;
+    settings.initial_dictionary_pos = 1;
+    return algo::pack::lzss_compress(input, settings);
+}
 
 TEST_CASE("Atelier Kaguya WFL archives", "[dec]")
 {
@@ -19,15 +30,36 @@ TEST_CASE("Atelier Kaguya WFL archives", "[dec]")
 
     io::File input_file;
     input_file.stream.write("WFL1"_b);
-    for (const auto &file : expected_files)
+
+    SECTION("Uncompressed")
     {
-        const auto name = algo::unxor(
-            algo::utf8_to_sjis(file->path.str()) + "\x00"_b, 0xFF);
-        input_file.stream.write_le<u32>(name.size());
-        input_file.stream.write(name);
-        input_file.stream.write_le<u16>(0);
-        input_file.stream.write_le<u32>(file->stream.size());
-        input_file.stream.write(file->stream.seek(0).read_to_eof());
+        for (const auto &file : expected_files)
+        {
+            const auto name = algo::unxor(
+                algo::utf8_to_sjis(file->path.str()) + "\x00"_b, 0xFF);
+            input_file.stream.write_le<u32>(name.size());
+            input_file.stream.write(name);
+            input_file.stream.write_le<u16>(0);
+            input_file.stream.write_le<u32>(file->stream.size());
+            input_file.stream.write(file->stream.seek(0).read_to_eof());
+        }
+    }
+
+    SECTION("Compressed")
+    {
+        for (const auto &file : expected_files)
+        {
+            const auto name = algo::unxor(
+                algo::utf8_to_sjis(file->path.str()) + "\x00"_b, 0xFF);
+            input_file.stream.write_le<u32>(name.size());
+            input_file.stream.write(name);
+            const auto data_orig = file->stream.seek(0).read_to_eof();
+            const auto data_comp = compress(data_orig);
+            input_file.stream.write_le<u16>(1);
+            input_file.stream.write_le<u32>(data_comp.size());
+            input_file.stream.write_le<u32>(data_orig.size());
+            input_file.stream.write(data_comp);
+        }
     }
 
     const auto decoder = WflArchiveDecoder();
