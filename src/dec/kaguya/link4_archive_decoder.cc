5490e76d946d6428d21363524ce8b2d4ae6e7b0a
rr- <rr-@sakuya.pl>
2016-02-09 22:18:46 +0100

    dec/kaguya/link4: initial support

---
+++ src/dec/kaguya/link4_archive_decoder.cc
@@ -0,0 +1,81 @@
+#include "dec/kaguya/link4_archive_decoder.h"
+#include "algo/locale.h"
+#include "algo/range.h"
+
+using namespace au;
+using namespace au::dec::kaguya;
+
+static const auto magic = "LINK4"_b;
+
+namespace
+{
+    struct ArchiveEntryImpl final : dec::ArchiveEntry
+    {
+        size_t offset;
+        size_t size;
+    };
+}
+
+bool Link4ArchiveDecoder::is_recognized_impl(io::File &input_file) const
+{
+    return input_file.stream.seek(0).read(magic.size()) == magic;
+}
+
+std::unique_ptr<dec::ArchiveMeta> Link4ArchiveDecoder::read_meta_impl(
+    const Logger &logger, io::File &input_file) const
+{
+    input_file.stream.seek(8);
+    auto meta = std::make_unique<ArchiveMeta>();
+        input_file.stream.skip(2);
+    while (true)
+    {
+        const auto entry_offset = input_file.stream.tell();
+        const auto entry_size = input_file.stream.read_le<u32>();
+        if (!entry_size)
+            break;
+
+        input_file.stream.skip(9);
+
+        const auto entry_name_size = input_file.stream.read<u8>();
+        input_file.stream.skip(2);
+        const auto entry_name = algo::sjis_to_utf8(
+            input_file.stream.read(entry_name_size)).str();
+
+        auto entry = std::make_unique<ArchiveEntryImpl>();
+        entry->path = entry_name;
+        entry->offset = input_file.stream.tell();
+        entry->size = entry_size - (entry->offset - entry_offset);
+        meta->entries.push_back(std::move(entry));
+
+        input_file.stream.seek(entry_offset + entry_size);
+    }
+
+    return meta;
+}
+
+std::unique_ptr<io::File> Link4ArchiveDecoder::read_file_impl(
+    const Logger &logger,
+    io::File &input_file,
+    const dec::ArchiveMeta &m,
+    const dec::ArchiveEntry &e) const
+{
+    const auto entry = static_cast<const ArchiveEntryImpl*>(&e);
+    return std::make_unique<io::File>(
+        entry->path,
+        input_file.stream.seek(entry->offset).read(entry->size));
+}
+
+std::vector<std::string> Link4ArchiveDecoder::get_linked_formats() const
+{
+    return
+    {
+        "kaguya/ap",
+        "kaguya/ap0",
+        "kaguya/ap2",
+        "kaguya/anm",
+        "kaguya/bmr",
+        "microsoft/bmp"
+    };
+}
+
+static auto _ = dec::register_decoder<Link4ArchiveDecoder>("kaguya/link4");
---
+++ tests/dec/kaguya/link4_archive_decoder_test.cc
@@ -0,0 +1,41 @@
+﻿#include "dec/kaguya/link4_archive_decoder.h"
+#include "algo/locale.h"
+#include "test_support/catch.h"
+#include "test_support/decoder_support.h"
+#include "test_support/file_support.h"
+
+using namespace au;
+using namespace au::dec::kaguya;
+
+TEST_CASE("Atelier Kaguya LINK4 archives", "[dec]")
+{
+    const std::vector<std::shared_ptr<io::File>> &expected_files =
+    {
+        tests::stub_file("123.txt", "1234567890"_b),
+        tests::stub_file("abc.txt", "abcdefghijklmnopqrstuvwxyz"_b),
+        tests::stub_file("魔法.txt", "expecto patronum"_b),
+    };
+
+    const auto arc_name = "tst"_b;
+
+    io::File input_file;
+    input_file.stream.write("LINK4"_b);
+    input_file.stream.write(arc_name);
+    input_file.stream.write("??"_b);
+    for (const auto &file : expected_files)
+    {
+        const auto data = file->stream.seek(0).read_to_eof();
+        const auto name = algo::utf8_to_sjis(file->path.str());
+        input_file.stream.write_le<u32>(data.size() + 16 + name.size());
+        input_file.stream.write("?????????"_b);
+        input_file.stream.write<u8>(name.size());
+        input_file.stream.write("??"_b);
+        input_file.stream.write(name);
+        input_file.stream.write(data);
+    }
+    input_file.stream.write_le<u32>(0);
+
+    const auto decoder = Link4ArchiveDecoder();
+    const auto actual_files = tests::unpack(decoder, input_file);
+    tests::compare_files(actual_files, expected_files, true);
+}
