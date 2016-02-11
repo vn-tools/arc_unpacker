739e29a9f8e44352d39caa4fe13c00bf5c46fc71
rr- <rr-@sakuya.pl>
2016-02-11 12:03:36 +0100

    dec/malie/libu: further reduce memory usage (#34)

--- src/dec/malie/libu_archive_decoder.cc
+++ src/dec/malie/libu_archive_decoder.cc
@@ -1,4 +1,5 @@
 #include "dec/malie/libu_archive_decoder.h"
+#include <cstring>
 #include "algo/crypt/camellia.h"
 #include "algo/locale.h"
 #include "algo/range.h"
@@ -21,70 +22,140 @@ namespace
         size_t offset;
         size_t size;
     };
+
+    // Rather than decrypting to bstr, the decryption is implemented as stream,
+    // so that huge files occupy as little memory as possible
+    class CamelliaStream final : public io::BaseByteStream
+    {
+    public:
+        CamelliaStream(
+            io::BaseByteStream &parent_stream, const LibPlugin &key);
+        CamelliaStream(
+            io::BaseByteStream &parent_stream,
+            const LibPlugin &key,
+            const size_t offset,
+            const size_t size);
+        ~CamelliaStream();
+
+        size_t size() const override;
+        size_t tell() const override;
+        std::unique_ptr<BaseByteStream> clone() const override;
+
+    protected:
+        void read_impl(void *destination, const size_t size) override;
+        void write_impl(const void *source, const size_t size) override;
+        void seek_impl(const size_t offset) override;
+        void resize_impl(const size_t new_size) override;
+
+    private:
+        const LibPlugin key;
+        std::unique_ptr<algo::crypt::Camellia> camellia;
+        std::unique_ptr<io::BaseByteStream> parent_stream;
+        const size_t parent_stream_offset;
+        const size_t parent_stream_size;
+    };
 }
 
-static void decrypt(
-    const LibPlugin &key,
-    io::BaseByteStream &input_stream,
-    io::BaseByteStream &output_stream,
-    const size_t size)
+CamelliaStream::CamelliaStream(
+    io::BaseByteStream &parent_stream, const LibPlugin &key)
+        : CamelliaStream(parent_stream, key, 0, parent_stream.size())
 {
-    if (key.empty())
+}
+
+CamelliaStream::CamelliaStream(
+    io::BaseByteStream &parent_stream,
+    const LibPlugin &key,
+    const size_t offset,
+    const size_t size) :
+        key(key),
+        parent_stream(parent_stream.clone()),
+        parent_stream_offset(offset),
+        parent_stream_size(size)
+{
+    if (key.size())
+        camellia = std::make_unique<algo::crypt::Camellia>(key);
+}
+
+CamelliaStream::~CamelliaStream()
+{
+}
+
+void CamelliaStream::seek_impl(const size_t offset)
+{
+    parent_stream->seek(parent_stream_offset + offset);
+}
+
+void CamelliaStream::read_impl(void *destination, const size_t size)
+{
+    if (!camellia)
     {
-        output_stream.write(input_stream, size);
+        const auto chunk = parent_stream->read(size);
+        std::memcpy(destination, chunk.get<u8>(), size);
         return;
     }
 
-    algo::crypt::Camellia camellia(key);
-
-    const auto offset_pad  = input_stream.tell() & 0xF;
-    const auto offset_start = input_stream.tell() & ~0xF;
+    const auto old_pos = parent_stream->tell();
+    const auto offset_pad  = parent_stream->tell() & 0xF;
+    const auto offset_start = parent_stream->tell() & ~0xF;
     const auto aligned_size = (offset_pad + size + 0xF) & ~0xF;
     const auto block_count = (aligned_size + 0xF) / 0x10;
     if (block_count == 0)
         return;
 
-    input_stream.seek(input_stream.tell() - offset_pad);
+    parent_stream->seek(parent_stream_offset
+        + ((parent_stream->tell() - parent_stream_offset) - offset_pad));
+
+    io::MemoryStream output_stream;
+    output_stream.resize(block_count * 16);
+    output_stream.seek(0);
     for (const auto i : algo::range(block_count))
     {
         u32 input_block[4];
         u32 output_block[4];
         for (const auto j : algo::range(4))
-            input_block[j] = input_stream.read_le<u32>();
-        camellia.decrypt_block_128(
-            offset_start + i * 0x10,
-            input_block,
-            output_block);
+            input_block[j] = parent_stream->read_le<u32>();
+        camellia->decrypt_block_128(
+            offset_start + i * 0x10, input_block, output_block);
         for (const auto j : algo::range(4))
             output_stream.write_be<u32>(output_block[j]);
-
-        if (i == 0)
-        {
-            const auto first_block = output_stream.seek(0).read_to_eof();
-            const auto block_part = first_block.substr(offset_pad);
-            output_stream.resize(0);
-            output_stream.write(block_part);
-            output_stream.resize(size);
-        }
     }
+    const auto chunk = output_stream.seek(offset_pad).read(size);
+    std::memcpy(destination, chunk.get<u8>(), size);
+    parent_stream->seek(old_pos + size);
 }
 
-static bstr decrypt(
-    const LibPlugin &key,
-    io::BaseByteStream &input_stream,
-    const size_t size)
+void CamelliaStream::write_impl(const void *source, const size_t size)
 {
-    io::MemoryStream output_stream;
-    decrypt(key, input_stream, output_stream, size);
-    return output_stream.seek(0).read(size);
+    throw err::NotSupportedError("Not implemented");
+}
+
+size_t CamelliaStream::tell() const
+{
+    return parent_stream->tell() - parent_stream_offset;
+}
+
+size_t CamelliaStream::size() const
+{
+    return parent_stream_size;
+}
+
+void CamelliaStream::resize_impl(const size_t new_size)
+{
+    parent_stream->resize(new_size);
+}
+
+std::unique_ptr<io::BaseByteStream> CamelliaStream::clone() const
+{
+    return std::make_unique<CamelliaStream>(
+        *parent_stream, key, parent_stream_offset, parent_stream_size);
 }
 
 bool LibuArchiveDecoder::is_recognized_impl(io::File &input_file) const
 {
     for (const auto &plugin : plugin_manager.get_all())
     {
-        input_file.stream.seek(0);
-        const auto maybe_magic = decrypt(plugin, input_file.stream, 4);
+        CamelliaStream camellia_stream(input_file.stream, plugin);
+        const auto maybe_magic = camellia_stream.seek(0).read(4);
         if (maybe_magic == magic)
             return true;
     }
@@ -100,25 +171,19 @@ std::unique_ptr<dec::ArchiveMeta> LibuArchiveDecoder::read_meta_impl(
     meta->plugin = maybe_magic == magic
         ? plugin_manager.get("noop")
         : plugin_manager.get();
-    input_file.stream.seek(0);
 
-    io::MemoryStream header_stream(
-        decrypt(meta->plugin, input_file.stream, 0x10));
-    header_stream.seek(magic.size());
-    const auto version = header_stream.read_le<u32>();
-    const auto file_count = header_stream.read_le<u32>();
-    const auto table_size = file_count * 80;
-
-    io::MemoryStream table_stream(
-        decrypt(meta->plugin, input_file.stream, table_size));
-
+    CamelliaStream camellia_stream(input_file.stream, meta->plugin);
+    camellia_stream.seek(magic.size());
+    const auto version = camellia_stream.read_le<u32>();
+    const auto file_count = camellia_stream.read_le<u32>();
+    camellia_stream.skip(4);
     for (const auto i : algo::range(file_count))
     {
         auto entry = std::make_unique<ArchiveEntryImpl>();
-        entry->path = algo::utf16_to_utf8(table_stream.read(68)).str(true);
-        entry->size = table_stream.read_le<u32>();
-        entry->offset = table_stream.read_le<u32>();
-        table_stream.skip(4);
+        entry->path = algo::utf16_to_utf8(camellia_stream.read(68)).str(true);
+        entry->size = camellia_stream.read_le<u32>();
+        entry->offset = camellia_stream.read_le<u32>();
+        camellia_stream.skip(4);
         meta->entries.push_back(std::move(entry));
     }
     return std::move(meta);
@@ -132,10 +197,13 @@ std::unique_ptr<io::File> LibuArchiveDecoder::read_file_impl(
 {
     const auto meta = static_cast<const ArchiveMetaImpl*>(&m);
     const auto entry = static_cast<const ArchiveEntryImpl*>(&e);
-    input_file.stream.seek(entry->offset);
-    auto output_file = std::make_unique<io::File>(entry->path, ""_b);
-    decrypt(meta->plugin, input_file.stream, output_file->stream, entry->size);
-    return output_file;
+    return std::make_unique<io::File>(
+        entry->path,
+        std::make_unique<CamelliaStream>(
+            input_file.stream,
+            meta->plugin,
+            entry->offset,
+            entry->size));
 }
 
 std::vector<std::string> LibuArchiveDecoder::get_linked_formats() const
