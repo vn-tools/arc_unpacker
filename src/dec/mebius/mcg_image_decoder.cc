9252ac511033c1f9b1e11460e9f05b7e06dca0cb
rr- <rr-@sakuya.pl>
2016-02-21 12:50:57 +0100

    dec/mebius/mcg: initial support

---
+++ src/dec/mebius/mcg_image_decoder.cc
@@ -0,0 +1,130 @@
+#include "dec/mebius/mcg_image_decoder.h"
+#include "algo/ptr.h"
+#include "algo/range.h"
+#include "io/memory_stream.h"
+
+using namespace au;
+using namespace au::dec::mebius;
+
+static const bstr magic = "MCG"_b;
+
+namespace
+{
+    struct FormatInfo final
+    {
+        res::PixelFormat fmt;
+        bool compressed;
+        bool deinterleaved;
+    };
+}
+
+static FormatInfo get_format_info(const u8 type)
+{
+    if (type == 0)      return {res::PixelFormat::BGR888,   false,  true};
+    else if (type == 1) return {res::PixelFormat::BGRA8888, false,  false};
+    else if (type == 2) return {res::PixelFormat::BGR888,   true,   true};
+    else if (type == 3) return {res::PixelFormat::BGRA8888, true,   true};
+    else if (type == 4) return {res::PixelFormat::Gray8,    false,  false};
+    else if (type == 5) return {res::PixelFormat::Gray8,    true,   false};
+    else throw std::logic_error("Invalid type");
+}
+
+static bstr rle_decompress(
+    io::BaseByteStream &input_stream,
+    const size_t width,
+    const size_t height,
+    const size_t channels)
+{
+    bstr output(width * height * channels);
+    auto output_ptr = algo::make_ptr(output);
+    auto marker = input_stream.read<u8>();
+    auto current_channel = 0u;
+
+    while (input_stream.left() && output_ptr.left())
+    {
+        const auto control = input_stream.read<u8>();
+        if (control == marker)
+        {
+            auto repetitions = input_stream.read<u8>();
+            const auto byte = input_stream.read<u8>();
+            while (repetitions-- && output_ptr.left())
+                *output_ptr++ = byte;
+        }
+        else
+        {
+            *output_ptr++ = control;
+        }
+    }
+
+    return output;
+}
+
+static bstr interleave(
+    const bstr &input,
+    const size_t width,
+    const size_t height,
+    const size_t channels)
+{
+    bstr output(width * height * channels);
+    for (const auto c : algo::range(channels))
+    for (const auto y : algo::range(height))
+    for (const auto x : algo::range(width))
+        output[(y * width + x) * channels + c]
+            = input[y * width + x + c * width * height];
+    return output;
+}
+
+bool McgImageDecoder::is_recognized_impl(io::File &input_file) const
+{
+    if (input_file.stream.read(magic.size()) != magic)
+        return false;
+    const auto type = input_file.stream.read<u8>();
+    return type < 8;
+}
+
+res::Image McgImageDecoder::decode_impl(
+    const Logger &logger, io::File &input_file) const
+{
+    input_file.stream.seek(magic.size());
+    const auto type = input_file.stream.read<u8>();
+    const auto total_width = input_file.stream.read_be<u16>();
+    const auto total_height = input_file.stream.read_be<u16>();
+    const auto x = input_file.stream.read_be<u16>();
+    const auto y = input_file.stream.read_be<u16>();
+    const auto width = input_file.stream.read_be<u16>();
+    const auto height = input_file.stream.read_be<u16>();
+
+    // copy content to memory for speed
+    io::MemoryStream input_stream(input_file.stream.read_to_eof());
+
+    if (type == 6 || type == 7)
+    {
+        res::Image output_image(width, height);
+        for (auto &c : output_image)
+        {
+            c.r = c.g = c.b = 0;
+            c.a = 0xFF;
+        }
+        return output_image;
+    }
+
+    const auto output_info = get_format_info(type);
+    const auto channels = res::pixel_format_to_bpp(output_info.fmt);
+
+    bstr data;
+    if (output_info.compressed)
+        data = rle_decompress(input_stream, width, height, channels);
+    else
+        data = input_stream.read(width * height * channels);
+
+    if (output_info.deinterleaved)
+        data = interleave(data, width, height, channels);
+
+    res::Image output_image(width, height, data, output_info.fmt);
+    output_image.offset(x, y);
+    output_image.crop(total_width, total_height);
+    output_image.flip_vertically();
+    return output_image;
+}
+
+static auto _ = dec::register_decoder<McgImageDecoder>("mebius/mcg");
---
+++ tests/dec/mebius/mcg_image_decoder_test.cc
@@ -0,0 +1,237 @@
+#include "dec/mebius/mcg_image_decoder.h"
+#include "algo/range.h"
+#include "test_support/catch.h"
+#include "test_support/decoder_support.h"
+#include "test_support/image_support.h"
+
+using namespace au;
+using namespace au::dec::mebius;
+
+static void compress(
+    const bstr &input, io::BaseByteStream &output_stream, const u8 marker)
+{
+    output_stream.write<u8>(marker);
+    size_t i = 0;
+    while (i < input.size())
+    {
+        size_t repetitions = 1;
+        while (i + repetitions < input.size()
+            && input[i + repetitions] == input[i]
+            && repetitions < 255)
+        {
+            repetitions++;
+        }
+
+        if (repetitions > 1 || input[i] == marker)
+        {
+            output_stream.write<u8>(marker);
+            output_stream.write<u8>(repetitions);
+            output_stream.write<u8>(input[i]);
+            i += repetitions;
+        }
+        else
+        {
+            output_stream.write<u8>(input[i]);
+            i++;
+        }
+    }
+}
+
+TEST_CASE("Studio Mebius MCG images", "[dec]")
+{
+    const auto decoder = McgImageDecoder();
+
+    SECTION("Type 0")
+    {
+        const auto expected_image = tests::get_opaque_test_image();
+
+        io::File test_file;
+        test_file.stream.write("MCG"_b);
+        test_file.stream.write<u8>(0);
+        test_file.stream.write_be<u16>(expected_image.width());
+        test_file.stream.write_be<u16>(expected_image.height());
+        test_file.stream.write_be<u16>(0);
+        test_file.stream.write_be<u16>(0);
+        test_file.stream.write_be<u16>(expected_image.width());
+        test_file.stream.write_be<u16>(expected_image.height());
+        for (const auto c : algo::range(3))
+        for (const auto y : algo::range(expected_image.height() - 1, -1, -1))
+        for (const auto x : algo::range(expected_image.width()))
+            test_file.stream.write<u8>(expected_image.at(x, y)[c]);
+
+        const auto actual_image = tests::decode(decoder, test_file);
+        tests::compare_images(actual_image, expected_image);
+    }
+
+    SECTION("Type 1")
+    {
+        const auto expected_image = tests::get_transparent_test_image();
+
+        io::File test_file;
+        test_file.stream.write("MCG"_b);
+        test_file.stream.write<u8>(1);
+        test_file.stream.write_be<u16>(expected_image.width());
+        test_file.stream.write_be<u16>(expected_image.height());
+        test_file.stream.write_be<u16>(0);
+        test_file.stream.write_be<u16>(0);
+        test_file.stream.write_be<u16>(expected_image.width());
+        test_file.stream.write_be<u16>(expected_image.height());
+        for (const auto y : algo::range(expected_image.height() - 1, -1, -1))
+        for (const auto x : algo::range(expected_image.width()))
+        {
+            const auto &pixel = expected_image.at(x, y);
+            test_file.stream.write<u8>(pixel.b);
+            test_file.stream.write<u8>(pixel.g);
+            test_file.stream.write<u8>(pixel.r);
+            test_file.stream.write<u8>(pixel.a);
+        }
+
+        const auto actual_image = tests::decode(decoder, test_file);
+        tests::compare_images(actual_image, expected_image);
+    }
+
+    SECTION("Type 2")
+    {
+        const auto expected_image = tests::get_opaque_test_image();
+
+        io::File test_file;
+        test_file.stream.write("MCG"_b);
+        test_file.stream.write<u8>(2);
+        test_file.stream.write_be<u16>(expected_image.width());
+        test_file.stream.write_be<u16>(expected_image.height());
+        test_file.stream.write_be<u16>(0);
+        test_file.stream.write_be<u16>(0);
+        test_file.stream.write_be<u16>(expected_image.width());
+        test_file.stream.write_be<u16>(expected_image.height());
+
+        bstr data_orig;
+        for (const auto c : algo::range(3))
+        for (const auto y : algo::range(expected_image.height() - 1, -1, -1))
+        for (const auto x : algo::range(expected_image.width()))
+            data_orig += expected_image.at(x, y)[c];
+
+        compress(data_orig, test_file.stream, 0xFF);
+
+        const auto actual_image = tests::decode(decoder, test_file);
+        tests::compare_images(actual_image, expected_image);
+    }
+
+    SECTION("Type 3")
+    {
+        const auto expected_image = tests::get_transparent_test_image();
+
+        io::File test_file;
+        test_file.stream.write("MCG"_b);
+        test_file.stream.write<u8>(3);
+        test_file.stream.write_be<u16>(expected_image.width());
+        test_file.stream.write_be<u16>(expected_image.height());
+        test_file.stream.write_be<u16>(0);
+        test_file.stream.write_be<u16>(0);
+        test_file.stream.write_be<u16>(expected_image.width());
+        test_file.stream.write_be<u16>(expected_image.height());
+
+        bstr data_orig;
+        for (const auto c : algo::range(4))
+        for (const auto y : algo::range(expected_image.height() - 1, -1, -1))
+        for (const auto x : algo::range(expected_image.width()))
+            data_orig += expected_image.at(x, y)[c];
+
+        compress(data_orig, test_file.stream, 0xFF);
+
+        const auto actual_image = tests::decode(decoder, test_file);
+        tests::compare_images(actual_image, expected_image);
+    }
+
+    SECTION("Type 4")
+    {
+        auto expected_image = tests::get_opaque_test_image();
+        for (auto &c : expected_image)
+            c.g = c.b = c.r;
+
+        io::File test_file;
+        test_file.stream.write("MCG"_b);
+        test_file.stream.write<u8>(4);
+        test_file.stream.write_be<u16>(expected_image.width());
+        test_file.stream.write_be<u16>(expected_image.height());
+        test_file.stream.write_be<u16>(0);
+        test_file.stream.write_be<u16>(0);
+        test_file.stream.write_be<u16>(expected_image.width());
+        test_file.stream.write_be<u16>(expected_image.height());
+        for (const auto y : algo::range(expected_image.height() - 1, -1, -1))
+        for (const auto x : algo::range(expected_image.width()))
+            test_file.stream.write<u8>(expected_image.at(x, y).r);
+
+        const auto actual_image = tests::decode(decoder, test_file);
+        tests::compare_images(actual_image, expected_image);
+    }
+
+    SECTION("Type 5")
+    {
+        auto expected_image = tests::get_opaque_test_image();
+        for (auto &c : expected_image)
+            c.g = c.b = c.r;
+
+        io::File test_file;
+        test_file.stream.write("MCG"_b);
+        test_file.stream.write<u8>(5);
+        test_file.stream.write_be<u16>(expected_image.width());
+        test_file.stream.write_be<u16>(expected_image.height());
+        test_file.stream.write_be<u16>(0);
+        test_file.stream.write_be<u16>(0);
+        test_file.stream.write_be<u16>(expected_image.width());
+        test_file.stream.write_be<u16>(expected_image.height());
+
+        bstr data_orig;
+        for (const auto y : algo::range(expected_image.height() - 1, -1, -1))
+        for (const auto x : algo::range(expected_image.width()))
+            data_orig += expected_image.at(x, y).r;
+
+        compress(data_orig, test_file.stream, 0xFF);
+
+        const auto actual_image = tests::decode(decoder, test_file);
+        tests::compare_images(actual_image, expected_image);
+    }
+
+    SECTION("Type 6")
+    {
+        res::Image expected_image(800, 600);
+        for (auto &c : expected_image)
+        {
+            c.r = c.g = c.b = 0;
+            c.a = 0xFF;
+        }
+
+        io::File test_file;
+        test_file.stream.write("MCG"_b);
+        test_file.stream.write<u8>(6);
+        test_file.stream.write_be<u16>(expected_image.width());
+        test_file.stream.write_be<u16>(expected_image.height());
+        test_file.stream.write_be<u16>(0);
+        test_file.stream.write_be<u16>(0);
+        test_file.stream.write_be<u16>(expected_image.width());
+        test_file.stream.write_be<u16>(expected_image.height());
+        const auto actual_image = tests::decode(decoder, test_file);
+        tests::compare_images(actual_image, expected_image);
+    }
+
+    SECTION("Type 7")
+    {
+        res::Image expected_image(800, 600);
+        for (auto &c : expected_image)
+        {
+            c.r = c.g = c.b = 0;
+            c.a = 0xFF;
+        }
+        io::File test_file;
+        test_file.stream.write("MCG"_b);
+        test_file.stream.write<u8>(6);
+        test_file.stream.write_be<u16>(expected_image.width());
+        test_file.stream.write_be<u16>(expected_image.height());
+        test_file.stream.write_be<u16>(0);
+        test_file.stream.write_be<u16>(0);
+        test_file.stream.write_be<u16>(expected_image.width());
+        test_file.stream.write_be<u16>(expected_image.height());
+        const auto actual_image = tests::decode(decoder, test_file);
+        tests::compare_images(actual_image, expected_image);
+    }
+}
