28002d5b37fe9a6800eb85e5ee6b81d0296a2feb
rr- <rr-@sakuya.pl>
2016-02-22 23:00:08 +0100

    dec/abstraction/wad: initial support

---
+++ src/dec/abstraction/wad_archive_decoder.cc
@@ -0,0 +1,77 @@
+#include "dec/abstraction/wad_archive_decoder.h"
+#include "algo/locale.h"
+#include "algo/range.h"
+
+using namespace au;
+using namespace au::dec::abstraction;
+
+static const bstr magic = "AGAR"_b;
+
+static const std::string read_string(io::BaseByteStream &input_stream)
+{
+    const auto name_size = input_stream.read_le<u32>();
+    return algo::utf8_to_sjis(input_stream.read(name_size)).str();
+}
+
+bool WadArchiveDecoder::is_recognized_impl(io::File &input_file) const
+{
+    return input_file.stream.seek(0).read(magic.size()) == magic;
+}
+
+std::unique_ptr<dec::ArchiveMeta> WadArchiveDecoder::read_meta_impl(
+    const Logger &logger, io::File &input_file) const
+{
+    input_file.stream.seek(magic.size());
+    const auto version_major = input_file.stream.read_le<u32>();
+    const auto version_minor = input_file.stream.read_le<u32>();
+    const auto extra_header_size = input_file.stream.read_le<u32>();
+    input_file.stream.skip(extra_header_size);
+
+    auto meta = std::make_unique<ArchiveMeta>();
+
+    const auto file_count = input_file.stream.read_le<u32>();
+    for (const auto i : algo::range(file_count))
+    {
+        auto entry = std::make_unique<PlainArchiveEntry>();
+        entry->path = read_string(input_file.stream);
+        entry->size = input_file.stream.read_le<u64>();
+        entry->offset = input_file.stream.read_le<u64>();
+        meta->entries.push_back(std::move(entry));
+    }
+
+    const auto dir_count = input_file.stream.read_le<u32>();
+    for (const auto i : algo::range(dir_count))
+    {
+        const auto dir_name = read_string(input_file.stream);
+        const auto dir_entry_count = input_file.stream.read_le<u32>();
+        for (const auto j : algo::range(dir_entry_count))
+        {
+            const auto dir_entry_name = read_string(input_file.stream);
+            const auto dir_entry_type = input_file.stream.read<u8>();
+        }
+    }
+
+    const auto base_offset = input_file.stream.pos();
+    for (auto &entry : meta->entries)
+        static_cast<PlainArchiveEntry*>(entry.get())->offset += base_offset;
+
+    return meta;
+}
+
+std::unique_ptr<io::File> WadArchiveDecoder::read_file_impl(
+    const Logger &logger,
+    io::File &input_file,
+    const dec::ArchiveMeta &m,
+    const dec::ArchiveEntry &e) const
+{
+    const auto entry = static_cast<const PlainArchiveEntry*>(&e);
+    const auto data = input_file.stream.seek(entry->offset).read(entry->size);
+    return std::make_unique<io::File>(entry->path, data);
+}
+
+std::vector<std::string> WadArchiveDecoder::get_linked_formats() const
+{
+    return {"truevision/tga"};
+}
+
+static auto _ = dec::register_decoder<WadArchiveDecoder>("abstraction/wad");
---
+++ tests/dec/abstraction/wad_archive_decoder_test.cc
@@ -0,0 +1,108 @@
+#include "dec/abstraction/wad_archive_decoder.h"
+#include "algo/locale.h"
+#include "test_support/catch.h"
+#include "test_support/decoder_support.h"
+#include "test_support/file_support.h"
+
+using namespace au;
+using namespace au::dec::abstraction;
+
+namespace
+{
+    struct DirEntry final
+    {
+        std::string name;
+        u8 type;
+    };
+
+    struct Dir final
+    {
+        std::string name;
+        std::vector<DirEntry> entries;
+    };
+}
+
+static void write_string(
+    io::BaseByteStream &output_stream, const bstr &input_string)
+{
+    const auto converted = algo::utf8_to_sjis(input_string);
+    output_stream.write_le<u32>(converted.size());
+    output_stream.write(converted);
+}
+
+static std::unique_ptr<io::File> get_wad_file(
+    const std::vector<std::shared_ptr<io::File>> &expected_files,
+    const std::vector<Dir> &dirs)
+{
+    const auto extra_header = "herp derp"_b;
+
+    auto output_file = std::make_unique<io::File>("test.wad", ""_b);
+    output_file->stream.write("AGAR"_b);
+    output_file->stream.write_le<u32>(1);
+    output_file->stream.write_le<u32>(1);
+    output_file->stream.write_le<u32>(extra_header.size());
+    output_file->stream.write(extra_header);
+
+    size_t current_offset = 0;
+    output_file->stream.write_le<u32>(expected_files.size());
+    for (const auto &file : expected_files)
+    {
+        write_string(output_file->stream, file->path.str());
+        output_file->stream.write_le<u64>(file->stream.size());
+        output_file->stream.write_le<u64>(current_offset);
+        current_offset += file->stream.size();
+    }
+
+    output_file->stream.write_le<u32>(dirs.size());
+    for (const auto &dir : dirs)
+    {
+        write_string(output_file->stream, dir.name);
+        output_file->stream.write_le<u32>(dir.entries.size());
+        for (const auto &dir_entry : dir.entries)
+        {
+            write_string(output_file->stream, dir_entry.name);
+            output_file->stream.write<u8>(dir_entry.type);
+        }
+    }
+
+    for (const auto &file : expected_files)
+    {
+        auto data = file->stream.seek(0).read_to_eof();
+        output_file->stream.write(data);
+    }
+
+    return output_file;
+}
+
+TEST_CASE("Abstraction Games WAD archives", "[dec]")
+{
+    const std::vector<std::shared_ptr<io::File>> expected_files =
+    {
+        tests::stub_file("00000.dat", "1234567890"_b),
+        tests::stub_file("00001.dat", "abcdefghijklmnopqrstuvwxyz"_b),
+    };
+
+    std::vector<Dir> dirs =
+    {
+        {
+            "test",
+            {
+                {"test", 1},
+                {"test2", 1},
+            }
+        },
+        {
+            "test2",
+            {
+                {"test", 1},
+                {"test2", 1},
+                {"test3", 1},
+            }
+        },
+    };
+
+    auto input_file = get_wad_file(expected_files, dirs);
+    const auto decoder = WadArchiveDecoder();
+    const auto actual_files = tests::unpack(decoder, *input_file);
+    tests::compare_files(actual_files, expected_files, true);
+}
