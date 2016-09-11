e4795e3e9788d100f491f47872ff0c0c40b75027
rr- <rr-@sakuya.pl>
2016-09-11 12:16:37 +0200

    dec/shiina-rio/warc: add gensou plugin

--- src/dec/shiina_rio/warc_archive_decoder_plugins.cc
+++ src/dec/shiina_rio/warc_archive_decoder_plugins.cc
@@ -52,11 +52,11 @@ WarcArchiveDecoder::WarcArchiveDecoder()
             p->entry_name_size = 0x20;
             p->region_image = read_etc_image("region.png");
             p->logo_data = read_etc_file("logo3.jpg");
-            p->crc_crypt = warc::get_crc_crypt(read_etc_file("table4.bin"));
             p->initial_crypt_base_keys = {0x4B535453, 0xA15FA15F, 0, 0, 0};
             p->flag_pre_crypt
                 = p->flag_post_crypt
                 = warc::get_flag_crypt1(read_etc_file("flag.png"), 0xECB2F5B2);
+            p->crc_crypt = warc::get_crc_crypt(read_etc_file("table4.bin"));
             return p;
         });
 
@@ -109,6 +109,24 @@ WarcArchiveDecoder::WarcArchiveDecoder()
             p->flag_pre_crypt
                 = p->flag_post_crypt
                 = warc::get_flag_crypt1(read_etc_file("flag.png"), 0x90CC9DC2);
+            p->crc_crypt = warc::get_crc_crypt(read_etc_file("table4.bin"));
+            return p;
+        });
+
+    plugin_manager.add(
+        "gensou",
+        "Gensou no Idea ~Oratorio Phantasm Historia~"
+        "Gensou no Idea",
+        []()
+        {
+            auto p = std::make_shared<warc::Plugin>();
+            p->version = 2490;
+            p->entry_name_size = 0x20;
+            p->region_image = read_etc_image("region.png");
+            p->logo_data = read_etc_file("logo7.jpg");
+            p->initial_crypt_base_keys
+                = {0x45BA9DA7, 0x68A8E7A9, 0x6AA84DA8, 0, 0};
+            p->flag_pre_crypt = warc::get_flag_crypt2();
             p->crc_crypt = warc::get_crc_crypt(read_etc_file("table4.bin"));
             return p;
         });
