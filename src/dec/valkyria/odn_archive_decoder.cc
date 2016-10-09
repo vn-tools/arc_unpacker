ed2a50977aa11664bd1aa884db48a13c5ee46d93
rr- <rr-@sakuya.pl>
2016-10-09 15:52:36 +0200

    dec/valkyria/odn: support images

--- src/dec/valkyria/odn_archive_decoder.cc
+++ src/dec/valkyria/odn_archive_decoder.cc
@@ -1,7 +1,9 @@
 #include "dec/valkyria/odn_archive_decoder.h"
 #include <map>
 #include "algo/range.h"
+#include "enc/png/png_image_encoder.h"
 #include "err.h"
+#include "io/memory_byte_stream.h"
 
 using namespace au;
 using namespace au::dec::valkyria;
@@ -15,6 +17,13 @@ namespace
         Variant3,
     };
 }
+
+// this is pathetic but their games actually hardcode this
+static const std::vector<std::tuple<size_t, size_t>> known_image_sizes =
+{
+    {800, 600},
+    {640, 480},
+};
 
 static size_t hex_to_int(const bstr &input)
 {
@@ -128,6 +137,69 @@ static std::unique_ptr<dec::ArchiveMeta> read_meta_v3(
     return meta;
 }
 
+static bstr decompress(const bstr &input, const size_t chunk_size)
+{
+    bstr output;
+    io::MemoryByteStream input_stream(input);
+    while (input_stream.left())
+    {
+        const u8 flags = input_stream.read<u8>();
+        u16 mask = 1;
+        do
+        {
+            if (!input_stream.left())
+                break;
+            const bstr chunk = input_stream.read(chunk_size);
+            const auto repetitions = (flags & mask)
+                ? input_stream.read_be<u16>() + 1
+                : 1;
+            for (const auto i : algo::range(repetitions))
+                output += chunk;
+            mask <<= 1;
+        }
+        while (mask <= 128);
+    }
+    return output;
+}
+
+static bstr decompress_bgr(const Logger &logger, const bstr &input)
+{
+    const auto output = decompress(input, 3);
+    const auto encoder = enc::png::PngImageEncoder();
+    for (const auto &known_image_size : known_image_sizes)
+    {
+        const auto width = std::get<0>(known_image_size);
+        const auto height = std::get<1>(known_image_size);
+        if (output.size() == width * height * 3)
+        {
+            res::Image image(width, height, output, res::PixelFormat::BGR888);
+            image.flip_vertically();
+            return encoder.encode(logger, image, "dummy.png")
+                ->stream.seek(0).read_to_eof();
+        }
+    }
+    return output;
+}
+
+static bstr decompress_bgra(const Logger &logger, const bstr &input)
+{
+    const auto output = decompress(input, 4);
+    const auto encoder = enc::png::PngImageEncoder();
+    for (const auto &known_image_size : known_image_sizes)
+    {
+        const auto width = std::get<0>(known_image_size);
+        const auto height = std::get<1>(known_image_size);
+        if (output.size() == width * height * 4)
+        {
+            res::Image image(width, height, output, res::PixelFormat::BGRA8888);
+            image.flip_vertically();
+            return encoder.encode(logger, image, "dummy.png")
+                ->stream.seek(0).read_to_eof();
+        }
+    }
+    return output;
+}
+
 std::unique_ptr<dec::ArchiveMeta> OdnArchiveDecoder::read_meta_impl(
     const Logger &logger, io::File &input_file) const
 {
@@ -152,7 +224,12 @@ std::unique_ptr<io::File> OdnArchiveDecoder::read_file_impl(
     const dec::ArchiveEntry &e) const
 {
     const auto entry = static_cast<const PlainArchiveEntry*>(&e);
-    const auto data = input_file.stream.seek(entry->offset).read(entry->size);
+    const auto prefix = entry->path.str().substr(0, 4);
+    auto data = input_file.stream.seek(entry->offset).read(entry->size);
+    if (prefix == "back")
+        data = decompress_bgr(logger, data);
+    if (prefix == "codn" || prefix == "cccc")
+        data = decompress_bgra(logger, data);
     auto ret = std::make_unique<io::File>(entry->path, data);
     ret->guess_extension();
     return ret;
