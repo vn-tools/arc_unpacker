1707c864fa4f44632a5dc81c8aa3ad636bd6b092
Throughflow <91414345+Throughflow@users.noreply.github.com>
2021-09-26 04:05:36 -0700

    dec: Add support for XOR-cipher Valkyria ODN archives
    This variant is used for all script archives

---
+++ src/io/xor_byte_stream.h
@@ -0,0 +1,47 @@
+// Copyright (C) 2016 by rr-
+//
+// This file is part of arc_unpacker.
+//
+// arc_unpacker is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or (at
+// your option) any later version.
+//
+// arc_unpacker is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with arc_unpacker. If not, see <http://www.gnu.org/licenses/>.
+
+#pragma once
+
+#include "io/base_byte_stream.h"
+
+namespace au {
+namespace io {
+
+    class XORByteStream final : public BaseByteStream
+    {
+    public:
+        XORByteStream(io::BaseByteStream& parent_stream);
+        ~XORByteStream();
+
+        uoff_t size() const override;
+        uoff_t pos() const override;
+        std::unique_ptr<BaseByteStream> clone() const override;
+
+    protected:
+        void read_impl(void* destination, const size_t size) override;
+        void write_impl(const void* source, const size_t size) override;
+        void seek_impl(const uoff_t offset) override;
+        void resize_impl(const uoff_t new_size) override;
+
+    private:
+        u8 key();
+
+        std::unique_ptr<io::BaseByteStream> parent_stream;
+    };
+
+} }
